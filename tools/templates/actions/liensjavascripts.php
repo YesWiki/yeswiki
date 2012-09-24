4d2288a2c1c29dd7db6227f1cd212ea00d8450b5
mrflos <florian@outils-reseaux.org>
2012-09-24 19:20:24 +0400

    mise à jour jquery tools pour bug bazar

--- tools/templates/actions/liensjavascripts.php
+++ tools/templates/actions/liensjavascripts.php
@@ -4,7 +4,7 @@ if (!defined("WIKINI_VERSION"))
         die ("acc&egrave;s direct interdit");
 }
 //javascripts
-$wikini_javascripts = '<script type="text/javascript" src="tools/templates/libs/jquery.tools.1.2.6-jquery.1.6.4.min.js"></script>';
+$wikini_javascripts = '<script type="text/javascript" src="tools/templates/libs/jquery.tools.1.2.7-jquery.1.7.2.min.js"></script>';
 if (is_dir('themes/'.$this->config['favorite_theme'].'/javascripts')) {
 	$repertoire = 'themes/'.$this->config['favorite_theme'].'/javascripts';
 } else {
