874b36d0873a6d60b7a1507267aa8f26ca139bc5
David Delon <david.delon@clapas.net>
2013-01-11 11:57:29 +0100

    Finition fusion bachibouzouk dans dev

--- tools/templates/actions/liensjavascripts.php
+++ tools/templates/actions/liensjavascripts.php
@@ -4,15 +4,6 @@ if (!defined("WIKINI_VERSION"))
         die ("acc&egrave;s direct interdit");
 }
 
-//javascripts
-$wikini_javascripts = '<script type="text/javascript" src="tools/templates/libs/jquery.tools.1.2.7-jquery.1.7.2.min.js"></script>';
-if (is_dir('themes/'.$this->config['favorite_theme'].'/javascripts')) {
-	$repertoire = 'themes/'.$this->config['favorite_theme'].'/javascripts';
-} else {
-	$repertoire = 'tools/templates/themes/'.$this->config['favorite_theme'].'/javascripts';
-}
-
-
 echo $this->Format('{{linkjavascript}}');
 
 ?>
