01b9f230da2bc7b9c73eb095a53aa98f6a60db8f
David Delon <david.delon@clapas.net>
2013-01-11 12:06:41 +0100

    Correction probleme de fusion

--- tools/templates/actions/liensjavascripts.php
+++ tools/templates/actions/liensjavascripts.php
@@ -3,16 +3,6 @@ if (!defined("WIKINI_VERSION"))
 {
         die ("acc&egrave;s direct interdit");
 }
-<<<<<<< HEAD
-//javascripts
-$wikini_javascripts = '<script type="text/javascript" src="tools/templates/libs/jquery.tools.1.2.7-jquery.1.7.2.min.js"></script>';
-if (is_dir('themes/'.$this->config['favorite_theme'].'/javascripts')) {
-	$repertoire = 'themes/'.$this->config['favorite_theme'].'/javascripts';
-} else {
-	$repertoire = 'tools/templates/themes/'.$this->config['favorite_theme'].'/javascripts';
-}
-=======
->>>>>>> mrflos/bachibouzouk
 
 echo $this->Format('{{linkjavascript}}');
 
