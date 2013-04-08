a240a227334f2cd5378d4268533fefbef9643200
mrflos <florian@outils-reseaux.org>
2013-04-08 20:31:12 +0400

    handler html

---
+++ tools/templates/handlers/page/html.php
@@ -0,0 +1,23 @@
+<?php
+/*
+*/
+
+// Verification de securite
+if (!defined("WIKINI_VERSION")) {
+	die ("acc&egrave;s direct interdit");
+}
+
+
+if ($this->HasAccess("read")) {
+	if (!$this->page) 	{
+		return;
+	}
+	else {
+		// affichage de la page formatee
+		echo "<div class=\"page\">\n".$this->Format($this->page["body"])."\n</div>\n";	
+	}
+}
+else {
+	return;
+}
+?>
