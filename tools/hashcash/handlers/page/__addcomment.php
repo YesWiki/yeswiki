0c807ed4501e47eab9b16ea0506b293e9044f38c
daiko <daiko@daiko.fr>
2013-04-12 12:49:48 +0200

    Correction bug hashcash sur les commentaires

--- tools/hashcash/handlers/page/__addcomment.php
+++ tools/hashcash/handlers/page/__addcomment.php
@@ -10,7 +10,8 @@ if (isset($_POST["action"]) && $_POST["action"] == 'addcomment') {
 	require_once('tools/hashcash/secret/wp-hashcash.lib');
 	if(!isset($_POST["hashcash_value"]) || ($_POST["hashcash_value"] != hashcash_field_value())) {
 		//$_POST['body'] .= " ".$this->href();
-		$this->SetMessage("Votre commentaire n'a pas été enregistré, le wiki pense que vous êtes un robot.");
+		//$this->SetMessage("Votre commentaire n'a pas été enregistré, le wiki pense que vous êtes un robot.");
+		$this->SetMessage("Résultat attendu : ".hashcash_field_value()." Réultat obtenu : ".$_POST["hashcash_value"]);
 		$this->redirect($this->href());
 	}	
 }
--- tools/hashcash/handlers/page/__show.php
+++ tools/hashcash/handlers/page/__show.php
@@ -5,16 +5,19 @@ if (!defined("WIKINI_VERSION"))
             die ("acc&egrave;s direct interdit");
 }
 
-## Ajoute le javascript qui calcule le hashcash
-if ($this->HasAccess("comment") && $this->page && !$this->page['comment_on'])
+
+if ($this->HasAccess("comment") && !$this->page['comment_on'])
 {
+
+	// Ajoute le javascript qui calcule le hashcash
 	$siteurl = str_replace("/wakka.php?wiki=", "", $this->config['base_url']);
 
 	$ChampsHashcash = '<script type="text/javascript" src="' 
 		. $siteurl . '/tools/hashcash/wp-hashcash-js.php?siteurl='
-		.$siteurl.'"></script><span id="hashcash-text" style="display:none" class="pull-right">Protection anti-spam active</span><hr class="hr_clear" />';
+		.$siteurl.'"></script>';
 
 	 $GLOBALS['js'] = ((isset($GLOBALS['js'])) ? $GLOBALS['js'] : '').$ChampsHashcash."\n";
 }
 
 ?>
+
