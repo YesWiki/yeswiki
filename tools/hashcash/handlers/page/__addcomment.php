10ee64045d1760e2896930b807d9463fb4f0bfe9
daiko <daiko@daiko.fr>
2013-04-12 12:57:07 +0200

    Suppression message de debugage

--- tools/hashcash/handlers/page/__addcomment.php
+++ tools/hashcash/handlers/page/__addcomment.php
@@ -10,8 +10,7 @@ if (isset($_POST["action"]) && $_POST["action"] == 'addcomment') {
 	require_once('tools/hashcash/secret/wp-hashcash.lib');
 	if(!isset($_POST["hashcash_value"]) || ($_POST["hashcash_value"] != hashcash_field_value())) {
 		//$_POST['body'] .= " ".$this->href();
-		//$this->SetMessage("Votre commentaire n'a pas été enregistré, le wiki pense que vous êtes un robot.");
-		$this->SetMessage("Résultat attendu : ".hashcash_field_value()." Réultat obtenu : ".$_POST["hashcash_value"]);
+		$this->SetMessage("Votre commentaire n'a pas été enregistré, le wiki pense que vous êtes un robot.");
 		$this->redirect($this->href());
 	}	
 }
