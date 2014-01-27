341eb84b48c1297e560d8061283427b523f3c1b2
Florian <mrflos@gmail.com>
2014-01-27 18:29:05 +0400

    raccourcissment du nom de page pour les donnees

--- tools/attach/actions/pointimage.php
+++ tools/attach/actions/pointimage.php
@@ -77,7 +77,7 @@ if (empty($point_size)) {
 $readonly = $this->GetParameter('readonly');
 
 // get an unique pagename based on the image filename, without extension
-$datapagetag = mysql_real_escape_string($this->GetPageTag().'PointImageDonnees'.preg_replace("/[^A-Za-z0-9 ]/", '', str_replace('.'.$ext, '', $file)));
+$datapagetag = mysql_real_escape_string($this->GetPageTag().'PI'.preg_replace("/[^A-Za-z0-9 ]/", '', str_replace('.'.$ext, '', $file)));
 
 // save the posted data
 if (isset($_POST['title']) && !empty($_POST['title'])
@@ -89,7 +89,7 @@ if (isset($_POST['title']) && !empty($_POST['title'])
 	$pagetag = mysql_real_escape_string(str_replace($this->config['base_url'], '', $_POST['pagetag']));
 	$chaine = "\n\n~~\"\"<!--".$_POST['image_x']."-".$_POST['image_y']."-".$_POST['color']."--><!--title-->".$_POST['title']."<!--/title-->\"\"\n\"\"<!--desc-->\"\"".$_POST['description']."\"\"<!--/desc-->\n\"\"~~";
 	$donneesbody = $this->LoadSingle("SELECT * FROM ".$this->config["table_prefix"]."pages WHERE tag = '".$pagetag."'and latest = 'Y' limit 1");
-	$this->SavePage($pagetag, $donneesbody['body'].$chaine);
+	$this->SavePage($pagetag, $donneesbody['body'].$chaine, "", true);
 	$this->Redirect($this->Href());
 }
 
