7a10df79d62f1b2268e8384b649d7980648602b3
David Delon <david.delon@clapas.net>
2014-09-24 15:19:45 +0200

    Lang : correction bug livraison

--- tools/lang/actions/lang.php
+++ tools/lang/actions/lang.php
@@ -7,17 +7,5 @@ if (!defined("WIKINI_VERSION"))
   die ("acc&egrave;s direct interdit");
 }
 
-$wikireq = $_REQUEST['wiki'];
-// remove leading slash
-$wikireq = preg_replace("/^\//", "", $wikireq);
-// split into page/method, checking wiki name & method name (XSS proof)
-if (preg_match('`^' . '(' . "[A-Za-z0-9]+" . ')/(' . "[A-Za-z0-9_-]" . '*)' . '$`', $wikireq, $matches)) {
-    list(, $PageTag, $method) = $matches;
-}
-elseif (preg_match('`^' . "[A-Za-z0-9]+" . '$`', $wikireq)) {
-    $PageTag = $wikireq;
-}
 
-echo $PageTag;
-exit;
 ?> 
