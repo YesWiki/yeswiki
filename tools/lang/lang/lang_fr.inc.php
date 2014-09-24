9817c4df32201552c7fe97aa5646775c28f22879
David Delon <david.delon@clapas.net>
2014-09-24 15:12:35 +0200

    Extension lang : gestion du changement de langue

---
+++ tools/lang/lang/lang_fr.inc.php
@@ -0,0 +1,20 @@
+<?php
+if (!defined("WIKINI_VERSION"))
+{
+  die ("acc&egrave;s direct interdit");
+}
+
+
+
+$GLOBALS['translations'] = array_merge($GLOBALS['translations'], array(
+
+// Action translation
+'LANG_DESTINATION_REQUIRED' => 'Le param&egrave;tre destination (langue destination), obligatoire, est manquant.',
+'LANG_FLAG_FILE_MISSING' => 'Drapeau absent pour ce pays'
+
+
+
+
+));
+
+?>
