acd4f260c02af9f1f0e0f6e4320ff9f1af6deadc
Florian <mrflos@gmail.com>
2014-10-16 09:22:44 +0400

    tocjs : table des matière dynamique à menu déroulants

--- tools/toc/actions/tocjs.php
+++ tools/toc/actions/tocjs.php
@@ -8,60 +8,62 @@ if (!defined("WIKINI_VERSION"))
 
 $tag = $this->GetPageTag();
 $class = $this->GetParameter("class");
+$offset = $this->GetParameter("offset");
+if (empty($offset)) $offset = '70';
+$align = $this->GetParameter("align");
+if (empty($align) || $align != 'left') $align = 'right';
+$size = $this->GetParameter("size");
+if (empty($size)) $size = '3';
+$contentsize = 12 - intval($size); 
 
-$script = '$(document).ready(function(){
-    var page = $(".page:first");
-    var titles = page.find("h1,h2,h3,h4,h5");
-    var bootstrap3_enabled = (typeof $().emulateTransitionEnd == \'function\');
-    if (bootstrap3_enabled) {var rowclass=\'row\';} else {var rowclass=\'row-fluid\';}
-    page.addClass("span9 col-md-9").wrap( "<div class=\'"+rowclass+"\'></div>" ).parent().append( "<div id=\'tocjs-'.$tag.'\' class=\'span3 col-md-3 no-dblclick\'><div class=\'bs-sidebar hidden-print\' role=\'complementary\'><ul class=\'nav bs-sidenav\'></ul></div></div>" );
-    var toc = $("#tocjs-'.$tag.'");  
-    var title, idtitle, typetitle, h1 = 0, h2 = 0, h3 = 0, h4 = 0, h5 = 0; 
-    titles.each(function() {
-        title = $(this);
-        typetitle = title.prop("tagName");
-        if (typetitle == \'H1\') {
-            h1 += 1;
-            idtitle = "H1-"+h1;
+$script = '
+var align = "'.$align.'";
+var page = $(".page:first");
+var bootstrap3_enabled = (typeof $().emulateTransitionEnd == \'function\');
+if (bootstrap3_enabled) {
+    var rowclass=\'row\';
+    var colclass=\'col-sm-\';
+} else {
+    var rowclass=\'row-fluid\';
+    var colclass=\'span\';
+}
+if (align === "left") {
+    page.addClass(colclass+"'.$contentsize.'").wrap( "<div class=\'"+rowclass+"\'></div>" ).parent().prepend( "<div class=\'"+colclass+"'.$size.' no-dblclick\'><div id=\'tocjs-'.$tag.'\' class=\'bs-sidebar hidden-print\' role=\'complementary\'></div></div>" );
+}
+else {
+    page.addClass(colclass+"'.$contentsize.'").wrap( "<div class=\'"+rowclass+"\'></div>" ).parent().append( "<div class=\'"+colclass+"'.$size.' no-dblclick\'><div id=\'tocjs-'.$tag.'\' class=\'bs-sidebar hidden-print\' role=\'complementary\'></div></div>" );
+}
+$.gajus
+    .contents({
+        where: $(\'#tocjs-'.$tag.'\'),
+        index: $(\'.page h1, .page h2, .page h3, .page h4, .page h5\')
+    }).on(\'change.contents.gajus\', function (event, change) {
+        if (change.previous) {
+            //change.previous.heading.removeClass(\'active\');
+            change.previous.anchor.removeClass(\'active\').parents(\'li\').removeClass(\'active\');
         }
-        else if (typetitle == \'H2\') {
-            h2 += 1;
-            idtitle = "H2-"+h2;
-        }
-        else if (typetitle == \'H3\') {
-            h3 += 1;
-            idtitle = "H3-"+h3;
-        }
-        else if (typetitle == \'H4\') {
-            h4 += 1;
-            idtitle = "H4-"+h4;
-        }
-        else if (typetitle == \'H5\') {
-            h5 += 1;
-            idtitle = "H5-"+h5;
-        }
-        title.attr(\'id\', idtitle);
-        toc.find(".bs-sidenav").append("<li class=\'"+typetitle+"\'><a class=\'toc-link\' href=\'#"+idtitle+"\'>"+title.html()+"</a></li>");
+
+        //change.current.heading.addClass(\'active\');
+        change.current.anchor.addClass(\'active\').parents(\'li\').addClass(\'active\');
     });
 
     var $window = $(window)
     var $body = $(document.body)
     var pagestartHeight = page.offset().top;
-    var $sideBar = $(\'.bs-sidebar\');
-    var offsetnavbar = 70;
+    var $sideBar = $(\'#tocjs-'.$tag.'\');
 
     $body.scrollspy({
-        target: \'.bs-sidebar\',
-        offset: offsetnavbar
+        target: \'#tocjs-'.$tag.'\',
+        offset: '.$offset.'
     });
 
-    $(\'.toc-link\').click(function (e) {
+    $(\'#tocjs-'.$tag.' a\').click(function (e) {
         e.preventDefault();
 
         var link = $(this).attr(\'href\');
         
         $(\'html, body\').animate({
-            scrollTop: $(link).offset().top - offsetnavbar
+            scrollTop: $(link).offset().top - '.$offset.'
         }, 500);
     })
 
@@ -85,7 +87,8 @@ $script = '$(document).ready(function(){
             }
         })
         $sideBar.on(\'affixed.bs.affix\', function (e) {
-          $sideBar.css(\'top\', offsetnavbar);
+            console.log($sideBar.css(\'width\'));
+          //$sideBar.css(\'top\', '.$offset.');
         })
         $sideBar.on(\'affix-top.bs.affix\', function (e) {
           $sideBar.css(\'top\', 0);
@@ -101,74 +104,81 @@ $script = '$(document).ready(function(){
             $sideBar.affix(\'checkPosition\')
         }, 100);
     });
-});'."\n";
+';
+$this->AddJavascriptFile('tools/toc/libs/vendor/contents.min.js');
 $this->AddJavascript($script);
 echo '<style>
-        /* First level of nav */
-         .bs-sidenav {
-            padding-top: 10px;
-            padding-bottom: 10px;
-            background-color: transparent;
-        }
-        /* All levels of nav */
-         .bs-sidebar .nav > li > a {
-            display: block;
-            color: #999999;
-            font-size: 0.9em;
-            font-weight: 500;
-            padding: 4px 20px;
-        }
-        .bs-sidebar .nav > li > a:hover, .bs-sidebar .nav > li > a:focus {
-            text-decoration: none;
-            color: #563d7c;
-            background-color: transparent;
-            border-left: 1px solid #563d7c;
-        }
-        .bs-sidebar .nav > .active > a, .bs-sidebar .nav > .active:hover > a, .bs-sidebar .nav > .active:focus > a {
-            font-weight: bold;
-            color: #563d7c;
-            background-color: transparent;
-            border-left: 2px solid #563d7c;
-        }
-        /* Nav: second level (shown on .active) */
-         .bs-sidebar .nav .nav {
-            display: none;
-            /* Hide by default, but at >768px, show it */
-            margin-bottom: 8px;
-        }
-        .bs-sidebar .nav .nav > li > a {
-            padding-top: 3px;
-            padding-bottom: 3px;
-            padding-left: 30px;
-            font-size: 90%;
-        }
+    #tocjs-'.$tag.'.affix {
+        top : '.$offset.'px
+    }
 
-        /* Show and affix the side nav when space allows it */
-         @media screen and (min-width: 992px) {
-            .bs-sidebar .nav > .active > ul {
-                display: block;
-            }
-            /* Widen the fixed sidebar */
-            .bs-sidebar.affix, .bs-sidebar.affix-bottom {
-                width: 213px;
-            }
-            .bs-sidebar.affix {
-                position: fixed;
-            }
-            .bs-sidebar.affix-bottom {
-                position: absolute;
-                /* Undo the static from mobile first approach */
-            }
-            .bs-sidebar.affix-bottom .bs-sidenav, .bs-sidebar.affix .bs-sidenav {
-                margin-top: 0;
-                margin-bottom: 0;
-            }
+    #tocjs-'.$tag.' ol {
+        list-style:none;
+        padding:0;
+        margin:0;
+    }
+
+    /* All levels of nav */
+     #tocjs-'.$tag.' ol > li > a {
+        display: block;
+        color: #999999;
+        font-size: 0.9em;
+        font-weight: 500;
+        padding: 4px 20px;
+    }
+    #tocjs-'.$tag.' ol > li > a:hover, #tocjs-'.$tag.' ol > li > a:focus {
+        text-decoration: none;
+        color: #444;
+        background-color: transparent;
+        border-left: 1px solid #444;
+    }
+    #tocjs-'.$tag.' ol > .active > a, #tocjs-'.$tag.' ol > .active:hover > a, #tocjs-'.$tag.' ol > .active:focus > a {
+        font-weight: bold;
+        color: #444;
+        background-color: transparent;
+        border-left: 2px solid #444;
+    }
+    /* Nav: second level (shown on .active) */
+    #tocjs-'.$tag.' ol ol {
+        display: none;
+        margin-bottom: 8px;
+    }
+    #tocjs-'.$tag.' .active ol {
+        display: block;
+    }
+    #tocjs-'.$tag.' ol ol > li > a {
+        padding-top: 3px;
+        padding-bottom: 3px;
+        padding-left: 30px;
+        font-size: 90%;
+    }
+
+    /* Show and affix the side nav when space allows it */
+     @media screen and (min-width: 992px) {
+        #tocjs-'.$tag.' ol > .active > ul {
+            display: block;
         }
-        @media screen and (min-width: 1200px) {
-            /* Widen the fixed sidebar again */
-            .bs-sidebar.affix-bottom, .bs-sidebar.affix {
-                width: 263px;
-            }
+        /* Widen the fixed sidebar */
+        #tocjs-'.$tag.'.affix, #tocjs-'.$tag.'.affix-bottom {
+            width: 213px;
         }
+        #tocjs-'.$tag.'.affix {
+            position: fixed;
+        }
+        #tocjs-'.$tag.'.affix-bottom {
+            position: absolute;
+            /* Undo the static from mobile first approach */
+        }
+        #tocjs-'.$tag.'.affix-bottom #tocjs-'.$tag.', #tocjs-'.$tag.'.affix #tocjs-'.$tag.' {
+            margin-top: 0;
+            margin-bottom: 0;
+        }
+    }
+    @media screen and (min-width: 1200px) {
+        /* Widen the fixed sidebar again */
+        #tocjs-'.$tag.'.affix-bottom, #tocjs-'.$tag.'.affix {
+           // width: 263px;
+        }
+    }
 
     </style>'."\n";
