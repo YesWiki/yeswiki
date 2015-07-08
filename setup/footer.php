9f54a6ba4da2648d4e78aac668e25e4731e8e9ff
Florian Schmitt <mrflos@gmail.com>
2015-07-08 11:59:36 +0300

    maj jquery

--- setup/footer.php
+++ setup/footer.php
@@ -1,12 +1,11 @@
 <?php
-if (!defined('WIKINI_VERSION'))
-{
-	die ("acc&egrave;s direct interdit");
+if (!defined('WIKINI_VERSION')) {
+    die("acc&egrave;s direct interdit");
 }
 
 ob_end_flush();
 ?>
-  <script src="tools/templates/libs/vendor/jquery-1.10.2.min.js"></script>
+  <script src="tools/templates/libs/vendor/jquery-1.11.3.min.js"></script>
   <script src="tools/templates/libs/vendor/bootstrap-2.3.2.min.js"></script>
 </body>
 </html>
