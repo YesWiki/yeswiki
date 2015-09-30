9fa8b90e7da844254f80a4bf74d83b06b90353cf
Florian Schmitt <mrflos@gmail.com>
2015-10-01 00:04:15 +0300

    login : bug parenthese mal fermee

--- tools/login/libs/login.functions.php
+++ tools/login/libs/login.functions.php
@@ -4,7 +4,7 @@ function loadUserbyEmail($email, $password = 0)
 {
     global $wiki;
     return $wiki->LoadSingle(
-        "select * from ".$wiki->config["table_prefix"] . "users where email = '".mysqli_real_escape_string($wiki->dblink, ($email).
-        "' " . ($password === 0 ? "" : "and password = '" . mysqli_real_escape_string($wiki->dblink, ($password) . "'") . " limit 1"
+        "select * from ".$wiki->config["table_prefix"] . "users where email = '".mysqli_real_escape_string($wiki->dblink, $email).
+        "' " . ($password === 0 ? "" : "and password = '" . mysqli_real_escape_string($wiki->dblink, $password) . "'") . " limit 1"
     );
 }
