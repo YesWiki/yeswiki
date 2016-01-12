f4a5c51b30daf85b43db8b8ee841d04919478b18
Florian Schmitt <mrflos@gmail.com>
2016-01-12 22:43:02 +0300

    contact : template bs3 et js plus propre

--- tools/contact/libs/contact.js
+++ tools/contact/libs/contact.js
@@ -1,69 +1,68 @@
-( function($) {
-$(document).ready(function(){
+(function($) {
+  $(document).ready(function() {
+    // validation formulaire de contact
+    $('.mail-submit').on('click', function(e) {
+      e.stopPropagation();
+      var form = $(this).parents('.ajax-mail-form');
+      var inputsreq = form.find('input[required], textarea[required]');
 
-	// validation formulaire de contact
-	$(".mail-submit").on("click", function(e) {
-		e.stopPropagation();
-		var form = $(this).parents('.ajax-mail-form');
-		var inputsreq = form.find('input[required], textarea[required]');
+      var atleastonefieldnotvalid = false;
+      var atleastonemailfieldnotvalid = false;
 
-		var atleastonefieldnotvalid = false;
-		var atleastonemailfieldnotvalid = false;
-		
-		// on efface les anciennes erreurs
-		form.find('.help-inline').remove();
+      // on efface les anciennes erreurs
+      form.find('.help-block').remove();
 
-		// il y a des champs requis, on teste la validite champs par champs
-		if (inputsreq.length > 0) {				
-			inputsreq.each(function() {
-				if ( !($(this).val().length === 0 || $(this).val() === '' || $(this).val() === '0')) {
-					$(this).parents('.control-group').removeClass('error');
-				} else {
-					atleastonefieldnotvalid = true;
-					$(this).parents('.control-group').addClass('error');
-					$('<span>').addClass('help-inline').text('La saisie de ce champ est obligatoire.').appendTo($(this).parents('.controls'));
-				}
-			});
-		}
-		
-		// les emails
-		form.find('input[type=email]').each(function() {
-			var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
-			var address = $(this).val();
-			if(reg.test(address) == false && !(address === '' &&  $(this).attr('required') !== 'required')) {
-				atleastonemailfieldnotvalid = true;
-				$(this).parents('.control-group').addClass('error');
-				$('<span>').addClass('help-inline').text('L\'email saisi n\'est pas valide.').appendTo($(this).parents('.controls'));		
-			} else {
-				$(this).parents('.control-group').removeClass('error');
-			}
-		});
+      // il y a des champs requis, on teste la validite champs par champs
+      if (inputsreq.length > 0) {
+        inputsreq.each(function() {
+          if (!($(this).val().length === 0 || $(this).val() === '' || $(this).val() === '0')) {
+            $(this).parents('.control-group').removeClass('has-error');
+          } else {
+            atleastonefieldnotvalid = true;
+            $(this).parents('.control-group').addClass('has-error');
+            $('<span>').addClass('help-block').text('La saisie de ce champ est obligatoire.').appendTo($(this).parents('.controls'));
+          }
+        });
+      }
 
-		if (atleastonefieldnotvalid === true || atleastonemailfieldnotvalid === true) {
-			// on remonte en haut du formulaire
-			$('html, body').animate({scrollTop: form.find(".error:first").offset().top - 80}, 800);
-		}
-		else {
-			// on soumet le formulaire
-			var str = form.serialize();
-			$.ajax({
-				type: "POST",
-				url: form.attr('action'),
-				data: str,
-				success: function(msg) {
-					// si le message a ete envoye, on affiche le message de notification
-					form.find('.alert').remove();
-					form.prepend(msg);
-					msg = '';
-					//  on vide le formulaire si succes
-					if (form.find('.alert-success').length>0) {
-						form[0].reset();
-					}
-				}
-			});
-		}
+      // les emails
+      form.find('input[type=email]').each(function() {
+        var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+        var address = $(this).val();
+        if (reg.test(address) == false && !(address === '' &&  $(this).attr('required') !== 'required')) {
+          atleastonemailfieldnotvalid = true;
+          $(this).parents('.control-group').addClass('has-error');
+          $('<span>').addClass('help-block').text('L\'email saisi n\'est pas valide.').appendTo($(this).parents('.controls'));
+        } else {
+          $(this).parents('.control-group').removeClass('has-error');
+        }
+      });
 
-		return false;
-	});
-});
-} ) ( jQuery );
+      if (atleastonefieldnotvalid === true || atleastonemailfieldnotvalid === true) {
+        // on remonte en haut du formulaire
+        $('html, body').animate({scrollTop: form.find('.has-error:first').offset().top - 80}, 800);
+      } else {
+        // on soumet le formulaire
+        var str = form.serialize();
+        $.ajax({
+          type: 'POST',
+          url: form.attr('action'),
+          data: str,
+          success: function(msg) {
+            // si le message a ete envoye, on affiche le message de notification
+            form.find('.alert').remove();
+            form.prepend(msg);
+            msg = '';
+
+            //  on vide le formulaire si succes
+            if (form.find('.alert-success').length > 0) {
+              form[0].reset();
+            }
+          },
+        });
+      }
+
+      return false;
+    });
+  });
+})(jQuery);
