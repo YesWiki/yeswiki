72f225ebe0acf565c5e48bdded046a9c13e90a14
Florian Schmitt <mrflos@gmail.com>
2016-03-17 15:51:58 +0300

    contact : amélioration de la traduction anglaise

--- tools/contact/lang/contact_en.inc.php
+++ tools/contact/lang/contact_en.inc.php
@@ -19,76 +19,77 @@
 // | License along with this library; if not, write to the Free Software                                  |
 // | Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA                            |
 // +------------------------------------------------------------------------------------------------------+
-// 
+//
 /**
-* Fichier de traduction en francais de l'extension Contact
+* Fichier de traduction en anglais de l'extension Contact
 *
-*@package 		contact
+*@package       contact
 *@author        Florian Schmitt <florian@outils-reseaux.org>
 *@copyright     2012 Outils-Réseaux
 */
 
-$GLOBALS['translations'] = array_merge($GLOBALS['translations'], array(
+$GLOBALS['translations'] = array_merge(
+    $GLOBALS['translations'],
+    array(
+        // actions/abonnement.php
+        'CONTACT_ACTION_ABONNEMENT' => 'Action {{abonnement ...}}',
+        'CONTACT_MAIL_REQUIRED' => 'the email, required, is missing',
 
-// actions/abonnement.php
-'CONTACT_ACTION_ABONNEMENT' => 'Action {{abonnement ...}}',
-'CONTACT_MAIL_REQUIRED' => 'le param&egrave;tre mail, obligatoire, est manquant.',
+        // actions/contact.php
+        'CONTACT_ACTION_CONTACT' => 'Action {{contact ...}}',
 
-// actions/contact.php
-'CONTACT_ACTION_CONTACT' => 'Action {{contact ...}}',
+        // actions/desabonnement.php
+        'CONTACT_ACTION_DESABONNEMENT' => 'Action {{desabonnement ...}}',
 
-// actions/desabonnement.php
-'CONTACT_ACTION_DESABONNEMENT' => 'Action {{desabonnement ...}}',
+        // actions/listsubscription.php
+        'CONTACT_ACTION_LISTSUBSCRIPTION' => 'Action {{listsubscription ...}}',
 
-// actions/listsubscription.php
-'CONTACT_ACTION_LISTSUBSCRIPTION' => 'Action {{listsubscription ...}}',
-
-// actions/mailinglist.php
-'CONTACT_ACTION_MAILINGLIST' => 'Action {{mailinglist ...}}',
-'CONTACT_PARAMETER_LIST_REQUIRED' => 'param&egrave;tre "list" obligatoire (il s\'agit de l\'adresse mail de la liste de diffusion)',
-'CONTACT_MAILS_TO_ADD_OR_REMOVE' => 'Mails &agrave; ajouter ou &agrave; supprimer de la liste',
-'CONTACT_SENT_TO_THE_LIST' => 'Envoi &agrave; la liste',
-'CONTACT_THE_EMAIL' => 'l\'email',
-'CONTACT_SUBMIT_OTHER_EMAILS' => 'Entrer d\'autres mails',
-'CONTACT_OK' => 'OK',
-'CONTACT_BTN_SUBSCRIBE' => 'Abonner',
-'CONTACT_BTN_UNSUBSCRIBE' => 'D&eacute;sabonner',
-'CONTACT_FOR_ALL_THOSE_EMAILS' => 'Pour toutes ces adresses mails',
-'CONTACT_TRY_WITH_OTHER_EMAILS' => 'Essayer avec d\'autres mails dans le texte',
-'CONTACT_NO_EMAILS_FOUND_IN_THIS_TEXT' => 'Pas d\'adresses mails trouv&eacute;es dans le texte fourni',
-'CONTACT_ENTER_TEXT_WITH_EMAILS_INSIDE' => 'Entrez un texte contenant des mails dedans pour les extraire, qu\'importe les s&eacute;parateurs (virgules, point-virgules, deux points, espaces, tabulations, retours &agrave; la ligne) ou le texte entre',
-'CONTACT_YOUR_EMAIL_LIST' => 'Votre liste d\'adresses mails',
-'CONTACT_EXTRACT_EMAILS_FROM_TEXT' => 'Extraire les mails de ce texte',
-'CONTACT_MUST_BE_ADMIN_TO_USE_THIS_ACTION' => 'il faut &ecirc;tre dans le groupe admins pour utiliser cette action',
+        // actions/mailinglist.php
+        'CONTACT_ACTION_MAILINGLIST' => 'Action {{mailinglist ...}}',
+        'CONTACT_PARAMETER_LIST_REQUIRED' => 'parameter list required (list\'s email)',
+        'CONTACT_MAILS_TO_ADD_OR_REMOVE' => 'emails to add or remove of the list',
+        'CONTACT_SENT_TO_THE_LIST' => 'send to the list',
+        'CONTACT_THE_EMAIL' => 'the email',
+        'CONTACT_SUBMIT_OTHER_EMAILS' => 'submit other emails',
+        'CONTACT_OK' => 'OK',
+        'CONTACT_BTN_SUBSCRIBE' => 'Suscribe',
+        'CONTACT_BTN_UNSUBSCRIBE' => 'Unsubscribe',
+        'CONTACT_FOR_ALL_THOSE_EMAILS' => 'For all those emails',
+        'CONTACT_TRY_WITH_OTHER_EMAILS' => 'Try with other emails in the text',
+        'CONTACT_NO_EMAILS_FOUND_IN_THIS_TEXT' => 'No email found in this text',
+        'CONTACT_ENTER_TEXT_WITH_EMAILS_INSIDE' => 'Enter text with emails inside to extract them, no matter the splitters (comma, semi-colon, colon, tab, wordwrap)',
+        'CONTACT_YOUR_EMAIL_LIST' => 'Your emails list',
+        'CONTACT_EXTRACT_EMAILS_FROM_TEXT' => 'Extract emails from the text',
+        'CONTACT_MUST_BE_ADMIN_TO_USE_THIS_ACTION' => 'You have to be in the admins group to use this action',
 
 
-'CONTACT_YOUR_NAME' => 'Votre nom',
-'CONTACT_YOUR_MAIL' => 'Votre adresse mail',
-'CONTACT_SUBJECT' => 'Sujet du message',
-'CONTACT_YOUR_MESSAGE' => 'Votre message',
-'CONTACT_SEND_MESSAGE' => 'Envoyer le message',
-'CONTACT_LISTSUBSCRIBE_REQUIRED' => 'le param&egrave;tre "list", contenant l\'adresse &eacute;lectronique de la liste, du type nomliste@domaine.ext, est obligatoire',
-'CONTACT_USER_NOT_LOGGED_IN' => 'il faut &ecirc;tre identifi&eacute; pour acc&eacute;der aux actions possibles.',
-'CONTACT_USER_NO_EMAIL' => 'il faut &ecirc;tre identifi&eacute; pour acc&eacute;der aux actions possibles.',
+        'CONTACT_YOUR_NAME' => 'Your name',
+        'CONTACT_YOUR_MAIL' => 'Your email',
+        'CONTACT_SUBJECT' => 'message subject',
+        'CONTACT_YOUR_MESSAGE' => 'Your message',
+        'CONTACT_SEND_MESSAGE' => 'Send message',
+        'CONTACT_LISTSUBSCRIBE_REQUIRED' => ' The "list" parameter, which have the list\'s email, like namelist@domain.ext, is required',
+        'CONTACT_USER_NOT_LOGGED_IN' => 'You have to be logged to make any action',
+        'CONTACT_USER_NO_EMAIL' => 'You have to be logged to make any action',
 
-'CONTACT_ENTER_NAME' => 'Vous devez entrer un nom.',
-'CONTACT_ENTER_SENDER_MAIL' => 'Vous devez entrer une adresse mail pour l\'exp&eacute;diteur.',
-'CONTACT_SENDER_MAIL_INVALID' => 'Vous devez entrer une adresse mail valide pour l\'exp&eacute;diteur.',
-'CONTACT_ENTER_RECEIVER_MAIL' => 'Vous devez entrer une adresse mail pour le destinataire.',
-'CONTACT_RECEIVER_MAIL_INVALID' => 'Vous devez entrer une adresse mail valide pour le destinataire.',
-'CONTACT_ENTER_MESSAGE' => 'Veuillez entrer un message. Il doit faire au minimum 10 caract&egrave;res.',
+        'CONTACT_ENTER_NAME' => 'Enter a name',
+        'CONTACT_ENTER_SENDER_MAIL' => 'Your email is needed as sender',
+        'CONTACT_SENDER_MAIL_INVALID' => 'Valid email is needed as sender',
+        'CONTACT_ENTER_RECEIVER_MAIL' => 'Recipient email is needed',
+        'CONTACT_RECEIVER_MAIL_INVALID' => 'Recipient valid email is needed',
+        'CONTACT_ENTER_MESSAGE' => 'Please enter a message, containing at least 10 characters',
 
-'CONTACT_MESSAGE_SUCCESSFULLY_SENT' => 'Votre message a bien &eacute;t&eacute; envoy&eacute;. Merci!',
-'CONTACT_MESSAGE_NOT_SENT' => 'Le message n\'a pas pu &ecirc;tre envoy&eacute;... Probleme du cot&eacute; de la configuration du serveur?',
-'CONTACT_SUBSCRIBE_ORDER_SENT' => 'Votre demande concernant votre abonnement a bien &eacute;t&eacute; prise en compte. Merci!',
-'CONTACT_UNSUBSCRIBE_ORDER_SENT' => 'Votre demande concernant votre d&eacute;sabonnement a bien &eacute;t&eacute; prise en compte. Merci!',
+        'CONTACT_MESSAGE_SUCCESSFULLY_SENT' => 'Your message have been sent. Thank you !',
+        'CONTACT_MESSAGE_NOT_SENT' => 'Your message could not be sent... Server configuration problem ?',
+        'CONTACT_SUBSCRIBE_ORDER_SENT' => 'Subscribe order sent. Thank you!',
+        'CONTACT_UNSUBSCRIBE_ORDER_SENT' => 'Unsubscribe order sent. Thank you!',
 
-'CONTACT_HANDLER_MAIL_FOR_ADMINS' => 'Le handler /mail est r&eacute;serv&eacute; au groupe des administrateurs.',
-'CONTACT_LOGIN_IF_ADMIN' => 'Si vous faites parti ce groupe, veuillez vous identifier.',
+        'CONTACT_HANDLER_MAIL_FOR_ADMINS' => 'Handler mail is only for admins group',
+        'CONTACT_LOGIN_IF_ADMIN' => 'Please login if you are admin',
 
-'CONTACT_SUBSCRIBE' => 'S\'abonner',
-'CONTACT_UNSUBSCRIBE' => 'Se d&eacute;sabonner',
+        'CONTACT_SUBSCRIBE' => 'Subscribe',
+        'CONTACT_UNSUBSCRIBE' => 'Unsubscribe',
 
-'CONTACT_FROM' => 'de'
-
-));
+        'CONTACT_FROM' => 'from'
+    )
+);
