efdfa5c578dfa9188a3f675c90bf0a200340980d
Florian Schmitt <mrflos@gmail.com>
2016-03-25 00:38:28 +0300

    template : correction faille de sécurité sur le handler diaporama

--- tools/templates/handlers/page/diaporama.php
+++ tools/templates/handlers/page/diaporama.php
@@ -1,5 +1,5 @@
 <?php
-    
+
 /**
  *
  * Handler "diaporama" pour YesWiki.
@@ -13,22 +13,22 @@
 if (!defined("WIKINI_VERSION")) {
     die("acc&egrave;s direct interdit");
 }
-    
+
 // on recupere les entetes html mais pas ce qu'il y a dans le body
 $header =  explode('<body', $this->Header());
 echo str_replace('<html', '<html class="slideshow-html"', $header[0])."<body class=\"slideshow-body\">\n";
 
 
 // on regarde si un template est passe en parametre GET, on passe celui par defaut sinon
-if (isset($_GET['template']) && file_exists('tools/templates/presentation/templates/'.$_GET['template'])) {
-    $template = $_GET['template'];
+if (isset($_GET['template']) && file_exists('tools/templates/presentation/templates/'.realpath($_GET['template']))) {
+    $template = realpath($_GET['template']);
 } else {
     $template = 'diaporama_slides.tpl.html';
 }
 
 // fonction de generation du diaporama (teste les droits et l'existence de la page)
 echo print_diaporama($this->tag, $template);
-    
+
 // on recupere juste les javascripts et la fin des balises body et html
 $footer =  preg_replace('/^.+<script/Us', "\n".'<script', $this->Footer());
 
