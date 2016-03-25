7848aa1a046a04dfc0e48a90786ec3473a73ddcc
Florian Schmitt <mrflos@gmail.com>
2016-03-25 11:05:51 +0300

    templates : /diaporama security fix

--- tools/templates/handlers/page/diaporama.php
+++ tools/templates/handlers/page/diaporama.php
@@ -1,7 +1,7 @@
 <?php
-	
+
 /**
- * 
+ *
  * Handler "diaporama" pour YesWiki.
  * Florian Schmitt <florian@outils-reseaux.org>.
  * Licence GPL.
@@ -10,28 +10,26 @@
 **/
 
 // Verification de securite
-if (!defined("WIKINI_VERSION"))
-{
-	die ("acc&egrave;s direct interdit");
+if (!defined("WIKINI_VERSION")) {
+    die("acc&egrave;s direct interdit");
 }
-	
+
 // on recupere les entetes html mais pas ce qu'il y a dans le body
-$header =  explode('<body',$this->Header());
+$header =  explode('<body', $this->Header());
 echo str_replace('<html', '<html class="slideshow-html"', $header[0])."<body class=\"slideshow-body\">\n";
 
 
 // on regarde si un template est passe en parametre GET, on passe celui par defaut sinon
-if (isset($_GET['template']) && file_exists('tools/templates/presentation/templates/'.$_GET['template'])) {
-	$template = $_GET['template'];
+if (isset($_GET['template']) && file_exists('tools/templates/presentation/templates/'.realpath($_GET['template']))) {
+    $template = realpath($_GET['template']);
 } else {
-	$template = 'diaporama_basic.tpl.html';
+    $template = 'diaporama_basic.tpl.html';
 }
 
 // fonction de generation du diaporama (teste les droits et l'existence de la page)
 echo print_diaporama($this->tag, $template);
-	
+
 // on recupere juste les javascripts et la fin des balises body et html
 $footer =  preg_replace('/^.+<script/Us', "\n".'<script', $this->Footer());
 
 echo $footer;
-?>
