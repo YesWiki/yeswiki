bbed2ae0c1998ebe8bad69f3ba6679ef5418dcce
Cyrille Giquello <cyrille@giquello.fr>
2016-07-23 15:53:55 +0200

    retro-compatibilité #200

--- tools/bazar/actions/map.php
+++ tools/bazar/actions/map.php
@@ -4,37 +4,5 @@
  *  Programme gerant les fiches bazar depuis une interface de type geographique.
  **/
 
-// +------------------------------------------------------------------------------------------------------+
-// |                                            ENTETE du PROGRAMME                                       |
-// +------------------------------------------------------------------------------------------------------+
-
-// test de sécurité pour vérifier si on passe par wiki
-if (!defined('WIKINI_VERSION')) {
-    die('acc&egrave;s direct interdit');
-}
-
-// on compte le nombre de fois que l'action bazarliste est appelée afin de différencier les instances
-if (!isset($GLOBALS['nbbazarliste'])) {
-    $GLOBALS['nbbazarliste'] = 0;
-}
-++$GLOBALS['nbbazarliste'];
-
-// Recuperation de tous les parametres
-$params = getAllParameters($this);
-$params['template'] = 'map.tpl.html';
-
-// tableau des fiches correspondantes aux critères
-if (is_array($params['idtypeannonce'])) {
-    $results = array();
-    foreach ($params['idtypeannonce'] as $formid) {
-        $results = array_merge(
-            $results,
-            baz_requete_recherche_fiches($params['query'], 'alphabetique', $formid, '', 1, '', '', true, '')
-        );
-    }
-} else {
-    $results = baz_requete_recherche_fiches($params['query'], 'alphabetique', '', '', 1, '', '', true, '');
-}
-
-// affichage à l'écran
-echo displayResultList($results, $params, false);
+// pour retro-compatibilité
+include( __DIR__.'/bazarcarto.php');
