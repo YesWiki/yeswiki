1ebcb40171c53b5b193dc8e09f1712eba4b5e8ca
sudwebdesign <thomas@sudwebdesign.fr>
2016-11-19 17:59:07 +0100

    Fix foreach Warnings if wiki empty (1st install)
    wakka.php?wiki=BazaR&vue=saisir
    
    Warning: Invalid argument supplied for foreach() in tools/bazar/libs/bazar.fonct.php on line 1039
    
    wakka.php?wiki=BazaR[&vue=formulaire]
    
    Warning: Invalid argument supplied for foreach() in tools/bazar/libs/bazar.fonct.php on line 2333

--- tools/bazar/libs/bazar.fonct.php
+++ tools/bazar/libs/bazar.fonct.php
@@ -1035,10 +1035,10 @@ function baz_formulaire($mode, $url = '', $valeurs = '')
                 $GLOBALS['params']['idtypeannonce'],
                 $GLOBALS['params']['categorienature']
             );
-
-            foreach ($tabform as $key => $value) {
-                $resultat[$value['bn_id_nature']] = $value;
-            }
+            if (is_array($tabform))
+                foreach ($tabform as $key => $value) {
+                    $resultat[$value['bn_id_nature']] = $value;
+                }
             if (count($resultat) == 0) {
                 $res .= '<div class="alert alert-info">'._t('BAZ_NO_FORMS_FOUND').
                 '.</div>'."\n";
@@ -2329,14 +2329,14 @@ function baz_gestion_formulaire()
             '<div class="alert alert-success">'.
             _t('BAZ_FORM_IMPORT_SUCCESSFULL').'.</div>'."\n";
         }
-
-        foreach ($forms as $key => $ligne) {
-            $tab_forms['forms'][$ligne['bn_id_nature']]['title'] = $ligne['bn_label_nature'];
-            $tab_forms['forms'][$ligne['bn_id_nature']]['description'] = $ligne['bn_description'];
-            $tab_forms['forms'][$ligne['bn_id_nature']]['category'] = $ligne['bn_type_fiche'];
-            $tab_forms['forms'][$ligne['bn_id_nature']]['can_edit'] = baz_a_le_droit('saisie_formulaire');
-            $tab_forms['forms'][$ligne['bn_id_nature']]['can_delete'] = $GLOBALS['wiki']->UserIsAdmin();
-        }
+        if (is_array($forms))
+            foreach ($forms as $key => $ligne) {
+                $tab_forms['forms'][$ligne['bn_id_nature']]['title'] = $ligne['bn_label_nature'];
+                $tab_forms['forms'][$ligne['bn_id_nature']]['description'] = $ligne['bn_description'];
+                $tab_forms['forms'][$ligne['bn_id_nature']]['category'] = $ligne['bn_type_fiche'];
+                $tab_forms['forms'][$ligne['bn_id_nature']]['can_edit'] = baz_a_le_droit('saisie_formulaire');
+                $tab_forms['forms'][$ligne['bn_id_nature']]['can_delete'] = $GLOBALS['wiki']->UserIsAdmin();
+            }
 
         // on rajoute les bibliothèques js nécéssaires
         $GLOBALS['wiki']->addJavascriptFile('tools/bazar/libs/bazar.edit_forms.js');
