1419c3b598e4f77c7a46d7062fced215f8f53979
Florian Schmitt <mrflos@gmail.com>
2017-10-06 20:43:34 +0300

    [bazar] mise a jour fullcalendar fixes #301 + corrections chemin css + menage

--- tools/bazar/actions/linkstyle__.php
+++ tools/bazar/actions/linkstyle__.php
@@ -5,5 +5,4 @@ if (!defined("WIKINI_VERSION")) {
 }
 
 //ajout des styles css pour bazar, le calendrier, la google map
-echo '	<link rel="stylesheet" href="tools/bazar/presentation/styles/bazar.css" />'."\n".
-'	<link rel="stylesheet" href="tools/bazar/libs/vendor/fullcalendar/fullcalendar.css" />'."\n";
+echo '	<link rel="stylesheet" href="tools/bazar/presentation/styles/bazar.css" />'."\n";
