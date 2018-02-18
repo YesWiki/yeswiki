60ab5ea15050a4898936a95f06c817fc63e41f82
Florian Schmitt <mrflos@gmail.com>
2018-02-18 03:57:56 +0300

    [autoupdate] real fix for core update and 5min execution time

--- tools/autoupdate/actions/update.php
+++ tools/autoupdate/actions/update.php
@@ -7,6 +7,10 @@ if (!defined("WIKINI_VERSION")) {
     die("acc&egrave;s direct interdit");
 }
 
+// tries to give 5 minutes time for the script to execute
+@ini_set('max_execution_time', 300);
+@set_time_limit(300);
+
 $autoUpdate = new AutoUpdate($this);
 $messages = new Messages();
 $controller = new Controller($autoUpdate, $messages);
