f2e8ce3d7e4f106734ecf84630b9129cafc7f286
Florian Schmitt <mrflos@gmail.com>
2018-12-06 17:41:21 +0100

    commit de test

---
+++ wakka-copie.php
@@ -0,0 +1,44 @@
+<?php
+/*
+ * $Id: wakka.php 864 2007-11-28 12:44:52Z nepote $
+ * Copyright (c) 2002, Hendrik Mans <hendrik@mans.de>
+ * Copyright 2003 Carlo Zottmann
+ * Copyright 2002, 2003, 2005 David DELON
+ * Copyright 2002, 2003, 2004, 2006 Charles NEPOTE
+ * Copyright 2002, 2003 Patrick PAUL
+ * Copyright 2003 Eric DELORD
+ * Copyright 2003 Eric FELDSTEIN
+ * Copyright 2004-2006 Jean-Christophe ANDRE
+ * Copyright 2005-2006 Didier LOISEAU
+ * All rights reserved.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ * derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*
+ * Yes, most of the formatting used in this file is HORRIBLY BAD STYLE. However,
+ * most of the action happens outside of this file, and I really wanted the code
+ * to look as small as what it does. Basically. Oh, I just suck. :)
+ */
+
+ // this file is just to maintain backward compatibility with yeswiki version using wakka.php as main url
+require_once 'index.php';
