baebd2299dbc62763b2b6e67c8afc639e499f32b
Florian Schmitt <mrflos@gmail.com>
2019-02-12 18:55:43 +0300

    [core] regexp compatibility for php7.3

--- includes/constants.php
+++ includes/constants.php
@@ -23,7 +23,7 @@ define("WN_CHAR2", "[A-Za-z0-9_-]"); // \xC0-\xD6\xD8-\xF6\xF8-\xFF]");
 /** the regexp for CamelCase - to use to check the syntax of a word, for example a user name */
 define('WN_CAMEL_CASE', WN_UPPER . WN_LOWER . '+' . WN_UPPER_NUM . WN_CHAR . '*');
 //define('WN_CAMEL_CASE_EVOLVED', '\b[\p{Lu}]+[\p{L}-_0-9]*[\p{Lu}]+[\p{L}-_0-9]*');
-define('WN_CAMEL_CASE_EVOLVED', '[\p{L}-_.0-9]+');
+define('WN_CAMEL_CASE_EVOLVED', '[\p{L}\-_.0-9]+');
 /** the regexp that matches automatic wiki links in a text */
 define('WN_WIKI_LINK', WN_CAMEL_CASE); // this might evolve to handle the page groups and subpages syntax
 /** the regexp that checks if a word is a valid page tag */
