baa291a2c1095d0510040666ff04f8324e2155e4
Sebastian Castro <sebastian.castro@protonmail.com>
2019-10-24 12:32:58 +0200

    [margot] improve buttons, footer

--- themes/margot/javascripts/margot.js
+++ themes/margot/javascripts/margot.js
@@ -1,7 +1,11 @@
-// replace full calendar icons
 $(document).ready(function() {
+  // replace full calendar icons
   $('.fc-prev-button').html('<span class="fa fa-chevron-left"></span>')
                       .prependTo('.fc-toolbar.fc-header-toolbar').removeClass('btn btn-default')
   $('.fc-next-button').html('<span class="fa fa-chevron-right"></span>')
                       .appendTo('.fc-toolbar.fc-header-toolbar').removeClass('btn btn-default')
+
+  // Wave effect on buttons
+  Waves.attach('.btn', ['waves-float', "waves-light"]);
+  Waves.init();
 })
