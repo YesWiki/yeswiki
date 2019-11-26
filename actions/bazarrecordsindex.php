f37048cbb617f3d8f1701f04c7252c0b70b2d077
Florian Schmitt <mrflos@gmail.com>
2019-11-26 03:17:09 +0300

    actions/bazarrecordsindex.php

--- actions/bazarrecordsindex.php
+++ actions/bazarrecordsindex.php
@@ -5,36 +5,36 @@ lists only bazar records.
 
 @licence: AGPL
 */
-if ($pages = $this->LoadAll('SELECT body FROM ' . $this->config["table_prefix"] . 'pages WHERE latest = \'Y\' AND comment_on=\'\' AND body LIKE \'{"%\' AND tag IN (SELECT DISTINCT resource FROM yeswiki_triples WHERE value = "fiche_bazar" AND property = "http://outils-reseaux.org/_vocabulary/type")'))
-{
-	$pagesarray = [];
-	foreach ($pages as $page)
-	{
+
+if ($pages = $this->LoadAll('SELECT body FROM ' . $this->config["table_prefix"] . 'pages WHERE latest = \'Y\' AND comment_on=\'\' AND body LIKE \'{"%\' AND tag IN (SELECT DISTINCT resource FROM ' . $this->config["table_prefix"] . 'triples WHERE value = "fiche_bazar" AND property = "http://outils-reseaux.org/_vocabulary/type")')) {
+    $pagesarray = [];
+    foreach ($pages as $page) {
         $fiche = json_decode($page['body'], true);
         if ($fiche) {
             $pagesarray[$fiche['id_fiche']] = $fiche['bf_titre'];
         }
-	}
-	asort($pagesarray);
-	foreach ($pagesarray as $tag => $page)
-	{
-		// XXX: strtoupper is locale dependent
-		$firstChar = strtoupper($page[0]);
-		if (!preg_match('/'.WN_UPPER.'/', $firstChar)) {
-			$firstChar = '#';
-		}
+    }
+    asort($pagesarray);
+    $buffer = '';
+    foreach ($pagesarray as $tag => $page) {
+        // XXX: strtoupper is locale dependent
+        $firstChar = strtoupper($page[0]);
+        if (!preg_match('/'.WN_UPPER.'/', $firstChar)) {
+            $firstChar = '#';
+        }
 
-		if (empty($curChar) || $firstChar != $curChar) {
-			if (!empty($curChar)) echo "<br />\n" ;
-			echo "<b>$firstChar</b><br />\n" ;
-			$curChar = $firstChar;
-		}
-		echo $this->Format('[['.$tag.' '.$page.']]')."<br />";
-	}
-}
-else
-{
-	echo '<i>'._t('NO_PAGE_FOUND').'.</i>' ;
-}
+        if (empty($curChar) || $firstChar != $curChar) {
+            echo $this->Format($buffer);
+            $buffer = '';
+            if (!empty($curChar)) {
+                echo "<br /><br />\n" ;
+            }
+            echo "<b>$firstChar</b><br />\n" ;
+            $curChar = $firstChar;
 
-?>
+        }
+        $buffer .= '[['.$tag.' '.$page.']]'."\n";
+    }
+} else {
+    echo '<i>'._t('NO_PAGE_FOUND').'.</i>' ;
+}
