38b8ee092dafdcac5d2d6daaefd6ad5e21ffb7c4
acheype <adrien.cheype@gmail.com>
2020-05-28 02:32:35 +0200

    [include] fix the bug of tag name after an include

--- tools/attach/actions/__include.php
+++ tools/attach/actions/__include.php
@@ -4,7 +4,7 @@
 	        die ("acc&egrave;s direct interdit");
 	}
 
-	$oldpage = $this->GetPageTag();
+	$oldpage = $this->page['tag'];
 	$this->tag = trim($this->GetParameter('page'));
 	$this->page = $this->LoadPage($this->tag);
 ?>
