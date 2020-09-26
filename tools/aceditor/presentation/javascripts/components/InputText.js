87e2dbfc4e31503f2cf3d371b2f1b22d75aae228
Sebastian Castro <sebastian.castro@protonmail.com>
2020-09-26 16:03:59 +0200

    actions-builder: color picker

--- tools/aceditor/presentation/javascripts/components/InputText.js
+++ tools/aceditor/presentation/javascripts/components/InputText.js
@@ -1,11 +1,19 @@
 // ext/Number/Color/slider
 export default {
   props: [ 'value', 'config' ],
+  mounted() {
+    if (this.config.type == 'color') {
+      $(this.$refs.input).spectrum({
+        type: "text"
+      });
+    }
+  },
   template: `
     <div class="form-group" :class="config.type" :title="config.hint" >
       <label v-if="config.label" class="control-label">{{ config.label }}</label>
-      <input :type="config.type" :value="value" v-on:input="$emit('input', $event.target.value)" class="form-control"
-             :required="config.required" :min="config.min" :max="config.max"
+      <input :type="config.type == 'color' ? 'text' : config.type" :value="value"
+             v-on:input="$emit('input', $event.target.value)" class="form-control"
+             :required="config.required" :min="config.min" :max="config.max" ref="input"
       />
     </div>
     `
