7d7fc10d51e4e06525881a2177c5f32f9b502393
srosset81 <srosset81@gmail.com>
2020-10-28 01:56:37 +0100

    Merge FormOpen in core YesWiki class

--- includes/YesWiki.php
+++ includes/YesWiki.php
@@ -1113,13 +1113,23 @@ class Wiki
     // FORMS
     public function FormOpen($method = '', $tag = '', $formMethod = 'post', $class = '')
     {
-        $result = "<form action=\"" . $this->href($method, $tag) . "\" method=\"" . $formMethod . "\"";
-        $result .= ((! empty($class)) ? " class=\"" . $class . "\"" : "");
-        $result .= ">\n";
-        if (! $this->config['rewrite_mode']) {
-            $result .= "<input type=\"hidden\" name=\"wiki\" value=\"" . $this->MiniHref($method, $tag) . "\" />\n";
+        if ($method=='edit') {
+            $result  = '<form id="ACEditor" name="ACEditor" enctype="multipart/form-data" action="'.$this->href($method, $tag).'" method="'.$formMethod.'"';
+            $result .= !empty($class) ? ' class="'.$class.'"' : '';
+            $result .= ">\n";
+            if (isset($this->config['password_for_editing']) and !empty($this->config['password_for_editing'])
+                and isset($_POST['password_for_editing'])) {
+                $result .= '<input type="hidden" name="password_for_editing" value="'.$_POST['password_for_editing'].'" />'."\n";
+            }
+        } else {
+            $result = '<form action="'.$this->href($method, $tag).'" method="'.$formMethod.'"';
+            $result .= !empty($class) ? ' class="'.$class.'"' : '';
+            $result .= ">\n";
         }
 
+        if (!$this->config["rewrite_mode"]) {
+            $result .= '<input type="hidden" name="wiki" value="'.$this->MiniHref($method, $tag).'" />'."\n";
+        }
         return $result;
     }
 
