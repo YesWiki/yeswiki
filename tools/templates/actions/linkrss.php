dfff65f1528c6b3bf81014684ba77724cbf4c604
Jérémy Dufraisse <67152407+J9rem@users.noreply.github.com>
2020-11-11 16:19:18 +0100

    manage linkrss in head

--- tools/templates/actions/linkrss.php
+++ tools/templates/actions/linkrss.php
@@ -3,7 +3,17 @@ if (!defined("WIKINI_VERSION")) {
     die("acc&egrave;s direct interdit");
 }
 
-echo "\n".
-'  <!-- RSS links -->'."\n".
-'  <link rel="alternate" type="application/rss+xml" title="'._t('TEMPLATE_RSS_LAST_CHANGES').'" href="'.$this->href('xml', 'DerniersChangementsRSS').'" />'."\n".
-'  <link rel="alternate" type="application/rss+xml" title="'._t('TEMPLATE_RSS_LAST_COMMENTS').'" href="'.$this->href('xml', 'DerniersCommentairesRSS').'" />'."\n";
+$displayLastChanges = $this->HasAccess('read', 'DerniersChangementsRSS') ;
+$displayLastComments = $this->HasAccess('read', 'DerniersCommentairesRSS') ;
+
+if ($displayLastChanges || $displayLastComments) {
+	echo "\n".
+	'  <!-- RSS links -->'."\n" ;
+}
+if ($displayLastChanges) {
+	echo '  <link rel="alternate" type="application/rss+xml" title="'._t('TEMPLATE_RSS_LAST_CHANGES').'" href="'.$this->href('xml', 'DerniersChangementsRSS').'" />'."\n";
+}
+if ($displayLastComments) {
+	echo '  <link rel="alternate" type="application/rss+xml" title="'._t('TEMPLATE_RSS_LAST_COMMENTS').'" href="'.$this->href('xml', 'DerniersCommentairesRSS').'" />'."\n";
+}
+?>
