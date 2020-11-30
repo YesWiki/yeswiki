f5713a2d1630047b158db5d91b98ce3de6248dae
Sebastian Castro <sebastian.castro@protonmail.com>
2020-11-30 16:23:34 +0100

    HelloWorld: Translate greeting action

--- tools/helloworld/lang/helloworld_fr.inc.php
+++ tools/helloworld/lang/helloworld_fr.inc.php
@@ -1,4 +1,9 @@
 <?php
 $GLOBALS['translations'] = array_merge($GLOBALS['translations'], array(
-    'HELLOWORLD_CONFIG' => 'Test config'
+    'HELLOWORLD_MY_MESSAGE' => 'My Message',
+    'HELLOWORLD_CONFIG' => 'Test config',
+    'HELLOWORLD_URL_SHOW_XML' => 'See XML Info Of Current Page',
+    'HELLOWORLD_URL_SHOW_TAG' => 'Show PagePrincipale',
+    'HELLOWORLD_URL_EDIT_TAG' => 'Edit PagePrincipale',
+    'HELLOWORLD_URL_EDIT_WITH_PARAMS' => 'Edit current page with params',
 ));
