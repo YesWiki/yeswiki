46be1220914a476dec67c8321ea614f8f0e19ccf
srosset81 <srosset81@gmail.com>
2020-12-08 09:55:30 +0100

    SelectListField

---
+++ tools/bazar/fields/SelectListField.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace YesWiki\Bazar\Field;
+
+use Psr\Container\ContainerInterface;
+
+/**
+ * @Field({"liste"})
+ */
+class SelectListField extends EnumField
+{
+    public function __construct(array $values, ContainerInterface $services)
+    {
+        parent::__construct($values, $services);
+
+        $this->type = 'select';
+
+        $this->loadOptionsFromList();
+    }
+
+    public function renderInput($entry)
+    {
+        return $this->render('@bazar/inputs/select.twig', [
+            'value' => $this->getValue($entry),
+            'options' => $this->options['label']
+        ]);
+    }
+
+    public function renderStatic($entry)
+    {
+        $value = $this->getValue($entry);
+        $value = $entry !== '' ? $this->options['label'][$value] : '';
+        return $this->render('@bazar/fields/select.twig', [
+            'value' => $value
+        ]);
+    }
+}
