8626cc0bf0bdad5ce06467104acba1c4a5a18a3f
Florian Schmitt <mrflos@gmail.com>
2020-12-10 10:24:12 +0300

    feat(toast) : add configuration of toast_duration and toast_class

--- tools/templates/actions/parambody.php
+++ tools/templates/actions/parambody.php
@@ -3,5 +3,7 @@ if (!defined("WIKINI_VERSION")) {
     die("acc&egrave;s direct interdit");
 }
 //attributs du body
-$body_attr = ($message = $this->GetMessage()) ? "onload=\"toastMessage('".addslashes($message)."');\" " : "";
+$toastDuration = !empty($this->config['toast_duration']) ? $this->config['toast_duration'] : '3000' ;
+$toastClass = !empty($this->config['toast_class']) ? $this->config['toast_class'] : 'alert alert-secondary-1';
+$body_attr = ($message = $this->GetMessage()) ? "onload=\"toastMessage('".addslashes($message)."', ".$toastDuration.", '".$toastClass."');\" " : "";
 echo $body_attr;
