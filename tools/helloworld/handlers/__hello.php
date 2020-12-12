e500101fe561c1347845ab2805692f58ccd7134b
acheype <adrien.cheype@gmail.com>
2020-12-12 16:03:19 +0100

    update hello world examples

--- tools/helloworld/handlers/__hello.php
+++ tools/helloworld/handlers/__hello.php
@@ -3,4 +3,6 @@ if (!defined("WIKINI_VERSION")) {
     die("acc&egrave;s direct interdit");
 }
 
-$msg = "this variable is from <em>__hello.php</em>";
+$this->parameter['msg'] = "this variable is from <em>__hello.php</em>";
+
+$msg2 = "the ugly way of passing then getting variables still works for backwards compatibility but please don't do it :)";
