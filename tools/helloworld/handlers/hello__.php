8b56da46ca699ecc5a583f0a5679a5206a9c5238
acheype <adrien.cheype@gmail.com>
2020-12-12 16:03:19 +0100

    update hello world examples

--- tools/helloworld/handlers/hello__.php
+++ tools/helloworld/handlers/hello__.php
@@ -3,8 +3,11 @@ if (!defined("WIKINI_VERSION")) {
     die("acc&egrave;s direct interdit");
 }
 
-$msg .= ', then display in the <em>hello__.php</em>';
+$this->parameter['msg'] .= ', then display in the <em>hello__.php</em>';
 
-$plugin_output_new = str_replace('MY_MESSAGE', $msg, $plugin_output_new);
-$plugin_output_new = str_replace('CONCLUSION', ' => the new actions/handlers/formatters are compatible ' .
-    'with the old ones', $plugin_output_new);
+$this->output = str_replace('MY_MESSAGE', $this->parameter['msg'], $this->output);
+$this->output = str_replace('MY_SECOND_MESSAGE', $msg2, $this->output);
+// for backwards compatibility, $plugin_output_new is still accessible but depreciated
+$plugin_output_new = str_replace('CONCLUSION', ' => you can mix the new actions/handlers/formatters with '
+    . 'the old ones, even if we encourage to use the new ones', $plugin_output_new);
+
