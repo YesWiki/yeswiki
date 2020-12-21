ecca1b6f101c22f382fb5177dff10258b1966dc3
acheype <adrien.cheype@gmail.com>
2020-12-21 22:20:29 +0100

    fix(button): no-btn have no btn class & modal is now in large display

--- tools/templates/actions/button.php
+++ tools/templates/actions/button.php
@@ -8,8 +8,8 @@ $link = $this->GetParameter('link');
 
 // extration du nom de 'root_page' si nécessaire
 if ($link == "config/root_page") {
-    $link = $this->config['root_page'] ;
-    $this->setParameter('link',$link);
+    $link = $this->config['root_page'];
+    $this->setParameter('link', $link);
 }
 
 if ($this->IsWikiName($link, WN_CAMEL_CASE_EVOLVED)) {
@@ -28,38 +28,46 @@ $icon = trim($this->GetParameter('icon'));
 if (!empty($icon)) {
     // si le parametre contient des espaces, il s'agit d'une icone autre que celles par defaut de bootstrap
     if (preg_match('/\s/', $icon)) {
-        $icon = '<i class="'.$icon.'"></i>';
+        $icon = '<i class="' . $icon . '"></i>';
     } else {
-        $icon = '<i class="icon-'.$icon.' fa fa-'.$icon.'"></i>';
+        $icon = '<i class="icon-' . $icon . ' fa fa-' . $icon . '"></i>';
     }
     if (!empty($text)) {
-        $icon = $icon.' ';
+        $icon = $icon . ' ';
     }
 }
 
 // classe css supplémentaire pour changer le look
 $class = $this->GetParameter('class');
-$class = 'btn '.$class;
+$class = 'btn ' . $class;
 if (!strstr($class, 'btn-')) {
     $class .= ' btn-default';
 }
 
+$datasize = '';
+if (preg_match('/\bmodalbox\b/i', $class)) {
+    // if modalbox, set the big size
+    $datasize .= ' data-size="modal-lg"';
+}
 
 $nobtn = $this->GetParameter('nobtn');
 if (!empty($nobtn) && $nobtn == '1') {
-    $class = str_replace(array('btn ', 'btn-default'), array('',''), $class);
+    // remove all the btn or btn-* css class
+    $class = preg_replace('/\b(?:btn(?!-)|btn-\w+)\b/i', '', $class);
+    // remove unneeded spaces
+    $class = preg_replace('/(^\s*)|(\s*$)/', '', preg_replace('/\s{2,}/', ' ', $class));
 }
 
 $hideIfNoAccess = $this->GetParameter('hideifnoaccess');
 if ($hideIfNoAccess == "true" && isset($linkTag) && !$GLOBALS['wiki']->HasAccess('read', $linkTag)) {
     echo '';
 } elseif (empty($link)) {
-    echo '<div class="alert alert-danger"><strong>'._t('TEMPLATE_ACTION_BUTTON').'</strong> : '._t('TEMPLATE_LINK_PARAMETER_REQUIRED').'.</div>'."\n";
+    echo '<div class="alert alert-danger"><strong>' . _t('TEMPLATE_ACTION_BUTTON') . '</strong> : ' . _t('TEMPLATE_LINK_PARAMETER_REQUIRED') . '.</div>' . "\n";
 } else {
-    $btn = '<a href="'.$link.'" class="'.$class.'"';
+    $btn = '<a href="' . $link . '" class="' . $class . '"' . $datasize;
     if (!empty($title)) {
-        $btn .= ' title="'.htmlentities($title, ENT_COMPAT, YW_CHARSET).'"';
+        $btn .= ' title="' . htmlentities($title, ENT_COMPAT, YW_CHARSET) . '"';
     }
-    $btn .= '>'.$icon.(!empty($text)? htmlentities($text, ENT_COMPAT, YW_CHARSET) : '').'</a>';
+    $btn .= '>' . $icon . (!empty($text) ? htmlentities($text, ENT_COMPAT, YW_CHARSET) : '') . '</a>';
     echo $btn;
 }
