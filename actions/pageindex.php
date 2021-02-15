f15d577f2078786ff7c46d15ef177d0ec1bc3136
Jérémy Dufraisse <67152407+J9rem@users.noreply.github.com>
2021-02-15 15:10:15 +0100

    remove curly brace in actions/PageIndex

--- actions/pageindex.php
+++ actions/pageindex.php
@@ -31,7 +31,7 @@ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 if ($pages = $this->LoadAll('SELECT tag FROM ' . $this->config["table_prefix"] . 'pages WHERE latest = \'Y\' AND comment_on=\'\' ORDER BY tag')) {
     foreach ($pages as $page) {
         // XXX: strtoupper is locale dependent
-        $firstChar = strtoupper($page['tag']{0});
+        $firstChar = strtoupper($page['tag'][0]);
         if (!preg_match('/'.WN_UPPER.'/', $firstChar)) {
             $firstChar = '#';
         }
