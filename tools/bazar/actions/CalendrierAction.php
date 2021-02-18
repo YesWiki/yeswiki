5930a7e24478f4d63456eca6ae56b673e942196f
Jérémy Dufraisse <67152407+J9rem@users.noreply.github.com>
2021-02-18 13:55:59 +0100

    fix minical parameter in calendrieraction

--- tools/bazar/actions/CalendrierAction.php
+++ tools/bazar/actions/CalendrierAction.php
@@ -8,8 +8,19 @@ class CalendrierAction extends YesWikiAction
 {
     public function formatArguments($arg)
     {
+        if (!empty($arg['class'])) {
+            $classes = explode(' ', $arg['class']);
+            $classes = array_combine($classes, $classes);
+        }
+        $minical = (isset($arg['minical']) && $arg['minical']="true") || (isset($classes) && in_array('minical', $classes)) ;
+        if ($minical) {
+            $classes['minical'] = 'minical';
+        }
+        $class = (isset($classes) && count($classes)>0) ? implode(' ', $classes) :null;
+
         return([
-            'minical' => $arg['minical'] ?? null,
+            'minical' => $minical ?? null,
+            'class' => $class,
             //template - default value calendar
             'template' => (isset($arg['template']) &&
                 BazarListeAction::specialActionFromTemplate($arg['template'], 'CALENDRIER_TEMPLATES'))
