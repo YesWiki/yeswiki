e2eb83762034718aeed654ef6b66397e30d4e921
Florian Schmitt <mrflos@gmail.com>
2021-02-24 19:26:48 +0300

    fix(api) : use good array syntax thanks @nils-van-zuijlen

--- includes/ApiResponse.php
+++ includes/ApiResponse.php
@@ -9,12 +9,12 @@ class ApiResponse extends JsonResponse
     public function __construct($data = null, int $status = 200, array $headers = [], bool $json = false)
     {
         $headers = array_merge([
-            ['Access-Control-Allow-Origin: *'],
-            ['Access-Control-Allow-Credentials: true'],
-            ['Access-Control-Allow-Headers: X-Requested-With, Location, Slug, Accept, Content-Type'],
-            ['Access-Control-Expose-Headers: Location, Slug, Accept, Content-Type'],
-            ['Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE, PUT, PATCH'],
-            ['Access-Control-Max-Age: 86400']
+            'Access-Control-Allow-Origin' => '*',
+            'Access-Control-Allow-Credentials' => 'true',
+            'Access-Control-Allow-Headers' => 'X-Requested-With, Location, Slug, Accept, Content-Type',
+            'Access-Control-Expose-Headers' => 'Location, Slug, Accept, Content-Type',
+            'Access-Control-Allow-Methods' => 'POST, GET, OPTIONS, DELETE, PUT, PATCH',
+            'Access-Control-Max-Age' => '86400'
         ], $headers);
 
         parent::__construct($data, $status, $headers, $json);
