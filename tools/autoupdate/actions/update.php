2c4906761d8da1bdeb24627b213752b4ff6bbb53
Jérémy Dufraisse <jeremy.dufraisse-info@orange.fr>
2021-03-06 10:59:25 +0100

    fix(autoupdate): reload page at end of update to be in new wiki context

--- tools/autoupdate/actions/update.php
+++ tools/autoupdate/actions/update.php
@@ -3,27 +3,49 @@ namespace AutoUpdate;
 
 $loader = require __DIR__ . '/../vendor/autoload.php';
 
+// display update's message
+// search data in session message
+$endUpdate = false ;
+if (isset($_SESSION['updateMessage'])) {
+    $message = $_SESSION['updateMessage'];
+    $_SESSION['updateMessage'] = '';
+    if (!empty($message)) {
+        // check integrity of data
+        $data = json_decode($message, true);
+        $endUpdate = is_array($data) && isset($data['messages']) && isset($data['baseURL'])
+            && $this->UserIsAdmin();
+    }
+}
+
 if (!defined("WIKINI_VERSION")) {
     die("acc&egrave;s direct interdit");
 }
 
-$this->addJavascriptFile('tools/templates/libs/vendor/datatables/jquery.dataTables.min.js');
-$this->addJavascriptFile('tools/templates/libs/vendor/datatables/dataTables.bootstrap.min.js');
-$this->addCSSFile('tools/templates/libs/vendor/datatables/dataTables.bootstrap.min.css');
-
-// tries to give 5 minutes time for the script to execute
-@ini_set('max_execution_time', 300);
-@set_time_limit(300);
-
-$autoUpdate = new AutoUpdate($this);
-$messages = new Messages();
-$controller = new Controller($autoUpdate, $messages, $this);
-
-//$controller->run($_GET, $this->getParameter('filter'));
-// Can't see where filter is used => getting rid of it
-$requestedVersion = $this->getParameter('version');
-if (isset($requestedVersion) && $requestedVersion != '') {
-    echo $controller->run($_GET, $requestedVersion);
+if ($endUpdate) {
+    // finished rendering of autoupdate
+    echo $this->render("@autoupdate/update.twig", [
+        'messages' => $data['messages'],
+        'baseUrl' => $data['baseURL'],
+    ]);
 } else {
-    echo $controller->run($_GET);
+    $this->addJavascriptFile('tools/templates/libs/vendor/datatables/jquery.dataTables.min.js');
+    $this->addJavascriptFile('tools/templates/libs/vendor/datatables/dataTables.bootstrap.min.js');
+    $this->addCSSFile('tools/templates/libs/vendor/datatables/dataTables.bootstrap.min.css');
+
+    // tries to give 5 minutes time for the script to execute
+    @ini_set('max_execution_time', 300);
+    @set_time_limit(300);
+
+    $autoUpdate = new AutoUpdate($this);
+    $messages = new Messages();
+    $controller = new Controller($autoUpdate, $messages, $this);
+
+    //$controller->run($_GET, $this->getParameter('filter'));
+    // Can't see where filter is used => getting rid of it
+    $requestedVersion = $this->getParameter('version');
+    if (isset($requestedVersion) && $requestedVersion != '') {
+        echo $controller->run($_GET, $requestedVersion);
+    } else {
+        echo $controller->run($_GET);
+    }
 }
--- tools/autoupdate/app/Controller.php
+++ tools/autoupdate/app/Controller.php
@@ -41,10 +41,23 @@ class Controller
             and $this->autoUpdate->isAdmin()
             ) {
             $this->upgrade($get['upgrade']);
-            return $this->wiki->render("@autoupdate/update.twig", [
-                'messages' => $this->messages,
-                'baseUrl' => $this->autoUpdate->baseUrl(),
-            ]);
+            // reload with handler FinishUpdateHandler to prevent missing files' error due to upgrade.
+            // prepare data
+            $data = [];
+            foreach ($this->messages as $message) {
+                $data_message = [];
+                $data_message['status'] = $message['status'];
+                $data_message['text'] = $message['text'];
+                $data['messages'][] = $data_message;
+            }
+            $data['baseURL'] = $this->autoUpdate->baseUrl();
+            $_SESSION['updateMessage'] = json_encode($data);
+            
+            // call the same href to reload wiki in new doryphore version
+            // give $data by $_SESSION['updateMessage']
+            $newAdress = $this->wiki->Href();
+            header("Location: ".$newAdress);
+            exit();
         }
 
         if (isset($get['delete'])
