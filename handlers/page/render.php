6363d99d2bccb4cea3108ca6929408aadf5000a0
Jérémy Dufraisse <jeremy.dufraisse-info@orange.fr>
2021-03-31 18:18:31 +0200

    fix(renderHandler): add content to body page to render actions (as label) #658

--- handlers/page/render.php
+++ handlers/page/render.php
@@ -6,6 +6,7 @@ $header = explode('<body', $this->Header());
 $output .= $header[0] . '<body class="yeswiki-iframe-body">'."\n"
     .'<div class="container">'."\n"
     .'<div class="yeswiki-page-widget page-widget page" '.$this->Format('{{doubleclic iframe="1"}}').'>'."\n";
+$this->page['body'] = $_GET['content']; // fake Page for actions and handlers
 $output .= $this->Format($_GET['content']);
 $output .= '</div><!-- end .page-widget -->'."\n";
 // on recupere juste les javascripts et la fin des balises body et html
