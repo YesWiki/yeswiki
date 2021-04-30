77ee49ecc0ebde172e67c305c420cbcb41035e8e
Jérémy Dufraisse <jeremy.dufraisse-info@orange.fr>
2021-04-30 14:25:39 +0200

    fix(field-label): display raw html

--- tools/aceditor/presentation/javascripts/components/InputFormField.js
+++ tools/aceditor/presentation/javascripts/components/InputFormField.js
@@ -14,7 +14,7 @@ export default {
       <label v-if="config.label" class="control-label">{{ config.label }}</label>
       <select :value="value" v-on:input="$emit('input', $event.target.value)" class="form-control">
         <option value=""></option>
-        <option v-for="field in fieldOptions" v-if="field.label" :value="field.id">{{ field.label }} - {{ field.id }}</option>
+        <option v-for="field in fieldOptions" v-if="field.label" :value="field.id"><span v-html="field.label"></span> - {{ field.id }}</option>
       </select>
       <input-hint :config="config"></input-hint>
     </div>
