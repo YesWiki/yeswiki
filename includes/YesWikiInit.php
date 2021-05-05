696abad7a759f9eb46db4c992331f85ed0c7fa1c
Jérémy Dufraisse <jeremy.dufraisse-info@orange.fr>
2021-05-05 20:11:45 +0200

    fix(robots.txt): init at creation of wiki

--- includes/YesWikiInit.php
+++ includes/YesWikiInit.php
@@ -204,7 +204,7 @@ class Init
             'default_read_acl' => '*',
             'default_comment_acl' => '@admins',
             'preview_before_save' => 0,
-            'allow_raw_html' => false,
+            'allow_raw_html' => true,
             'disable_wiki_links' => false,
             'timezone'=>'GMT' // Only used if not set in wakka.config.php nor in php.ini
         );
