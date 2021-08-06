b819443a33726a0af2b8bd77c611dcb19e359e92
Jérémy Dufraisse <jeremy.dufraisse-info@orange.fr>
2021-08-06 20:17:09 +0200

    refactor(YesWikiLoader): create loader to factorize code for tests

---
+++ includes/YesWikiLoader.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Yeswiki is a great wiki
+ *
+ * @category Wiki
+ * @package  YesWiki
+ * @license  AGPL version 3
+ * @link     https://yeswiki.net
+ */
+
+namespace YesWiki\Core;
+
+use Doctrine\Common\Annotations\AnnotationRegistry;
+use YesWiki\Wiki;
+
+class YesWikiLoader
+{
+    // singleton
+    private static $wiki;
+
+    protected function __construct()
+    {
+    } // prevent public usage
+    protected function __clone()
+    {
+    } // prevent public usage
+
+    public static function getWiki(): Wiki
+    {
+        if (is_null(self::$wiki)) {
+            require_once 'includes/autoload.inc.php';
+            $loader = require_once 'vendor/autoload.php';
+            if ($loader !== true) { // not true if not already included
+                AnnotationRegistry::registerLoader([$loader, 'loadClass']);
+            }
+
+            $loadedWiki = require_once 'includes/YesWiki.php';
+            if ($loadedWiki !== true || is_null(self::$wiki)) {
+                // params to succeed to instanciate wiki
+                $_SERVER['REQUEST_URI'] = $_SERVER['REQUEST_URI'] ?? '';
+                $_SERVER['HTTP_HOST'] = $_SERVER['HTTP_HOST'] ?? 'localhost';
+                $_SERVER['REMOTE_ADDR'] = $_SERVER['REMOTE_ADDR'] ?? '127.0.0.1';
+                $_SESSION = $_SESSION ?? [];
+
+                self::$wiki = new Wiki();
+            }
+        }
+        return self::$wiki;
+    }
+}
