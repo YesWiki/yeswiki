4b85aef10e6d35ffcc1e11ba018b116da85a1383
Sebastian Castro <sebastian.castro@protonmail.com>
2021-09-09 12:42:46 +0200

    fix(action-builder): input page list not working when select option from autocomplete

--- tools/aceditor/presentation/javascripts/components/InputPageList.js
+++ tools/aceditor/presentation/javascripts/components/InputPageList.js
@@ -20,6 +20,9 @@ export default {
           // remove previous typeahead and refresh source
           $(this.$refs.input).typeahead('destroy');
           $(this.$refs.input).typeahead({ source: pages, items: 5});
+          $(this.$refs.input).on('blur.bootstrap3Typeahead', () => {
+            setTimeout(() => { this.$emit('input',  this.$refs.input.value) }, 200)            
+          })
         }
       })
       return []
@@ -29,8 +32,9 @@ export default {
     <div class="form-group" :class="config.type" :title="config.hint" >
       <label v-if="config.label" class="control-label">{{ config.label }}</label>
       <input type="text" autocomplete="off" :value="value" class="form-control"
-            data-provide="typeahead" data-items="5" :data-source='pageList'
-             v-on:input="$emit('input', $event.target.value)" 
+             data-provide="typeahead" data-items="5" :data-source="pageList"
+             @input="$emit('input', $event.target.value)" 
+             @blur="$emit('input', $event.target.value)" 
              :required="config.required" :min="config.min" :max="config.max" ref="input"
       />
       <input-hint :config="config"></input-hint>
