4ac7038d9203b4a28d113b0dee0f15f1e6485133
Jérémy Dufraisse <jeremy.dufraisse-info@orange.fr>
2021-09-13 17:41:38 +0200

    fix(EntryManager): better catch for filterOnReadACL

--- tools/bazar/services/EntryManager.php
+++ tools/bazar/services/EntryManager.php
@@ -907,6 +907,10 @@ class EntryManager
         // needed ACL
         if (count($neededACL) > 0) {
             $newRequestStart .= ' AND (';
+            if (!empty($user)) {
+                $newRequestStart .= '(';
+                $newRequestEnd = ')'.$newRequestEnd;
+            }
 
             $addOr = false;
             foreach ($neededACL as $acl) {
@@ -923,21 +927,17 @@ class EntryManager
                 $newRequestStart .= ' AND ';
                 $newRequestStart .= ' list NOT LIKE "%!'.$acl.'%"';
             }
+            
+            // add detection of '%'
+            if (!empty($user)) {
+                $newRequestStart .= ') OR (';
+                
+                $newRequestStart .= '(list LIKE "%\\%%" AND list NOT LIKE "%!\\%%")';
+                $newRequestStart .= ' AND owner = _utf8\'' . mysqli_real_escape_string($this->wiki->dblink, $userName) . '\'';
+            }
         }
 
         $request = $newRequestStart.$newRequestEnd;
-
-        // add detection of '%'
-        if (!empty($user)) {
-            $newRequestStart = ' OR (';
-            $newRequestEnd = ')';
-            
-            $newRequestStart .= 'tag in (SELECT DISTINCT page_tag FROM ' . $this->dbService->prefixTable('acls') .
-                'WHERE privilege="read" AND list LIKE "%\\%%" AND list NOT LIKE "%!\\%%"';
-            $newRequestEnd = ') AND owner = _utf8\'' . mysqli_real_escape_string($this->wiki->dblink, $userName) . '\''.$newRequestEnd;
-
-            $request = $request.$newRequestStart.$newRequestEnd;
-        }
 
         // return request to append
         return $request;
