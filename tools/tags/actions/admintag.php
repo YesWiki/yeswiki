de35a0946ce2ad8082e32b21457690a3fb84e6ac
Jérémy Dufraisse <jeremy.dufraisse-info@orange.fr>
2021-11-04 00:23:59 +0100

    fix(admintagAction) : fix display when only one tag + refactor twig

--- tools/tags/actions/admintag.php
+++ tools/tags/actions/admintag.php
@@ -18,74 +18,19 @@ if ($this->UserIsAdmin()) {
     $sql = 'SELECT id, value, resource FROM '.$this->config['table_prefix'].'triples WHERE property="http://outils-reseaux.org/_vocabulary/tag" ORDER BY value ASC, resource ASC';
     $tab_tous_les_tags = $this->LoadAll($sql);
 
-    if (is_array($tab_tous_les_tags) && count($tab_tous_les_tags)>1) {
-        // following line commented to prevent empty tagsname
-        // $this->addJavascriptFile('tools/templates/libs/vendor/datatables/jquery.dataTables.min.js');
-        $this->addCSSFile('tools/templates/libs/vendor/datatables/dataTables.bootstrap.min.css');
-        echo '<table class="table table-striped table-condensed">
-        <thead>
-        <tr>
-          <th>Page</th>
-          <th>'._t('TAGS_PRESENT_IN').'</th>
-          <th>Action</th>
-        </tr>
-        </thead>
-        <tbody>'."\n";
-        $nb_pages = 0;
-        $liste_page = '';
-        $tag_precedent = '';
-        $tab_tous_les_tags[]='fin'; // on ajoute un element au tableau pour boucler une derniere fois
-        $tagsid = '';
-        foreach ($tab_tous_les_tags as $tab_les_tags) {
-            if ($tab_les_tags != 'fin') {
-                $tagstripped = _convert(stripslashes($tab_les_tags['value']), 'ISO-8859-1');
+    if (is_array($tab_tous_les_tags) && count($tab_tous_les_tags)>0) {
+        $tags = [];
+        foreach ($tab_tous_les_tags as $tag) {
+            $tagName = _convert(stripslashes($tag['value']), 'ISO-8859-1');
+            if (empty($tags[$tagName])) {
+                $tags[$tagName] = [$tag];
             } else {
-                $tagstripped = 'fin';
+                $tags[$tagName][] = $tag;
             }
-            if ($tagstripped != 'fin' && ($tagstripped==$tag_precedent || $tag_precedent== '')) {
-                $nb_pages++;
-                if ($tag_precedent=='') {
-                    $tag_precedent = $tagstripped;
-                }
-                if ($nb_pages == 1) {
-                    $liste_page .= '<tr><td>nbpage</td>';
-                } else {
-                    $liste_page .= '<tr><td></td>';
-                }
-                $liste_page .= '<td class="pagewithtag">
-					'._t('TAGS_PRESENT_IN').' : <a class="wikipagelink" href="'.$this->href('', $tab_les_tags['resource']).'">'.$tab_les_tags['resource'].'</a>
-				</td>
-				<td class="delete_tag">
-					<a class="btn btn-xs btn-mini btn-error btn-danger" href="'.$this->href().'&amp;delete_tag='.$tab_les_tags['id'].'"><i class="icon icon-trash icon-white"></i> '._t('TAGS_DELETE_MINUSCULE').' '._t('TAGS_FROM_THIS_PAGE').'.</a>
-				</td>
-				</tr>'."\n";
-                $tagsid .= (($tagsid == '') ? '' : ',').$tab_les_tags['id'];
-            } else {
-                // on affiche les informations pour ce tag
-                $texte_liste  = '<tr>'."\n".'<td class="taglistitem">'."\n".'<span class="tag-label label label-primary">'.$tag_precedent.'</span>'."\n";
-                $liste_page = str_replace('<tr><td>nbpage</td>', $texte_liste, $liste_page);
-                if ($nb_pages>1) {
-                    $liste_page .= '<tr><td></td><td></td><td class="delete_all_tags"><a class="btn btn-xs btn-mini btn-error btn-danger" href="'.$this->href().'&amp;delete_tag='.urlencode($tagsid).'"><strong><i class="icon icon-trash icon-white"></i> '._t('TAGS_DELETE_MINUSCULE').'&nbsp;<span class="tag-label label label-primary">'.$tag_precedent.'</span> '._t('TAGS_FROM_ALL_PAGES').'.</strong></a></td></tr>'."\n";
-                }
-                echo $liste_page;
-
-                // on reinitialise les variables
-                if ($tagstripped != 'fin') {
-                    $nb_pages = 1;
-                    $tagsid = $tab_les_tags['id'];
-                    $liste_page = '<tr><td>nbpage</td>
-					<td class="pagewithtag">
-						'._t('TAGS_PRESENT_IN').' : <a class="wikipagelink" href="'.$this->href('', $tab_les_tags['resource']).'">'.$tab_les_tags['resource'].'</a>
-					</td>
-					<td class="delete_tag">
-						<a href="'.$this->href().'&amp;delete_tag='.urlencode($tab_les_tags['id']).'" class="btn btn-xs btn-mini btn-error btn-danger"><i class="icon icon-trash icon-white"></i> '._t('TAGS_DELETE_MINUSCULE').' '._t('TAGS_FROM_THIS_PAGE').'.</a>
-					</td>
-					</tr>'."\n";
-                }
-            }
-            $tag_precedent = $tagstripped;
         }
-        echo '</tbody></table>'."\n";
+        echo $this->render('@tags/admintag-action.twig', [
+            'tags' => $tags,
+        ]);
     } else {
         echo '<p>Aucun mot clé utilisé pour le moment</p>';
     }
