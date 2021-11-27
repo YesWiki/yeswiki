576fa6a02d844579d91ddd80fe5faecd122a849c
Jérémy Dufraisse <jeremy.dufraisse-info@orange.fr>
2021-11-27 11:55:02 +0100

    fix(translation/action): remove bug for current tag detection

--- tools/lang/actions/translation.php
+++ tools/lang/actions/translation.php
@@ -11,22 +11,40 @@ if (empty($destination)) {
     echo _t(LANG_DESTINATION_REQUIRED);
 }
 
-$wikireq = $_REQUEST['wiki'];
-// remove leading slash
-$wikireq = preg_replace("/^\//", "", $wikireq);
-// split into page/method, checking wiki name & method name (XSS proof)
-if (preg_match('`^' . '(' . "[A-Za-z0-9]+" . ')/(' . "[A-Za-z0-9_-]" . '*)' . '$`', $wikireq, $matches)) {
-    list(, $PageTag, $method) = $matches;
-} elseif (preg_match('`^' . "[A-Za-z0-9]+" . '$`', $wikireq)) {
-    $PageTag = $wikireq;
-}
-// Todo : utiliser template
-
 $flagfile='tools/lang/presentation/images/'.$destination.'.png';
 
 if (file_exists($flagfile)) {
-    echo '<a href="wakka.php?wiki='.$PageTag.'&lang='.$destination.'">
+    $wikireq = isset($_GET['wiki']) ? $_GET['wiki'] : null;
+
+    $currentMethod = empty($this->method) ? '' : '/' . $this->method;
+    $currentTag = (strpos($wikireq, '/') !== false)
+        ? substr($wikireq, 0, -strlen($currentMethod))
+        : $wikireq;
+
+    $queries = [];
+    parse_str($_SERVER['QUERY_STRING'], $queries);
+    unset($queries[$wikireq]);
+    unset($queries['wiki']);
+    $queries['lang'] = $destination;
+    $query = '';
+    foreach ($queries as $key => $value) {
+        $query .= '&'.$key."=".$value ;
+    }
+    $query = substr($query, 1); // remove first '&'
+
+    // remove $_GET['lang'] because it is used by Href
+    if (isset($_GET['lang'])) {
+        $previousLang = $_GET['lang'];
+        unset($_GET['lang']);
+    }
+    // Todo : utiliser template
+    echo '<a href="'.$this->Href($wikireq === $currentTag ? '' : $this->method, $currentTag, $query, false).'">
         <img src="'.$flagfile.'" title="'.$destination.'" alt="Flag'.$destination.'"></img></a>';
+
+    if (isset($previousLang)) {
+        $_GET['lang'] = $previousLang;
+        unset($previousLang);
+    }
 } else {
     echo _t(LANG_FLAG_FILE_MISSING);
 }
