f4f85101d2c264c337587e0f8c2ce7ae9a5e7dca
Jérémy Dufraisse <jeremy.dufraisse-info@orange.fr>
2021-12-23 15:30:40 +0100

    refactor(composer/SimplePie): dot not use gitignore to be able not to depend composer.json from main folder

---
+++ tools/syndication/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-doryphore',
+        'version' => 'dev-doryphore',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '67ebbe8c0f0e02c8ea4270842ee535ed4cff7a19',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-doryphore',
+            'version' => 'dev-doryphore',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '67ebbe8c0f0e02c8ea4270842ee535ed4cff7a19',
+            'dev_requirement' => false,
+        ),
+        'simplepie/simplepie' => array(
+            'pretty_version' => '1.5.7',
+            'version' => '1.5.7.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../simplepie/simplepie',
+            'aliases' => array(),
+            'reference' => '76be63e72ea6867d9982bf9bd97ba706b196d602',
+            'dev_requirement' => false,
+        ),
+    ),
+);
