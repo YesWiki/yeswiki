46c7de25e83a748aa3324797f61df7b511c80b4d
Jérémy Dufraisse <jeremy.dufraisse-info@orange.fr>
2022-01-14 16:28:48 +0100

    feat(syndication/lang): prepare translations

--- tools/syndication/lang/syndication_fr.inc.php
+++ tools/syndication/lang/syndication_fr.inc.php
@@ -28,24 +28,27 @@
 *@copyright     2013 Outils-Réseaux
 */
 
-$GLOBALS['translations'] = array_merge($GLOBALS['translations'], array(
+return [
 
-// actions/syndication.php
-'SYNDICATION_ACTION_SYNDICATION' => 'Action {{syndication ...}}',
-'SYNDICATION_PARAM_URL_REQUIRED' => 'il faut entrer obligatoirement le param&egrave;tre "url" pour syndiquer un flux RSS',
-'SYNDICATION_WRITE_ACCESS_TO_CACHE_FOLDER' => 'le r&eacute;pertoire "cache" n\'a pas les droits d\'acc&egrave;s en &eacute;criture',
-'SYNDICATION_CREATE_CACHE_FOLDER' => 'il faut cr&eacute;er un r&eacute;pertoire "cache" dans le r&eacute;pertoire principal',
-'SYNDICATION_TEMPLATE_NOT_FOUND' => 'n\'existe pas, on utilise le fichier template par d&eacute;faut.',
-'SYNDICATION_READ_MORE' => 'Lire la suite',
+    // actions/syndication.php
+    'SYNDICATION_ACTION_SYNDICATION' => 'Action {{syndication ...}}',
+    'SYNDICATION_PARAM_URL_REQUIRED' => 'il faut entrer obligatoirement le param&egrave;tre "url" pour syndiquer un flux RSS',
+    'SYNDICATION_WRITE_ACCESS_TO_CACHE_FOLDER' => 'le r&eacute;pertoire "cache" n\'a pas les droits d\'acc&egrave;s en &eacute;criture',
+    'SYNDICATION_CREATE_CACHE_FOLDER' => 'il faut cr&eacute;er un r&eacute;pertoire "cache" dans le r&eacute;pertoire principal',
+    'SYNDICATION_TEMPLATE_NOT_FOUND' => 'n\'existe pas, on utilise le fichier template par d&eacute;faut.',
+    'SYNDICATION_READ_MORE' => 'Lire la suite',
 
-// actions/twitter.php
-'SYNDICATION_ACTION_TWITTER' => 'Action {{twitter ...}}',
-'SYNDICATION_PARAM_USER_REQUIRED' => 'param&egrave;tre "user" manquant, il est obligatoire pour sp&eacute;fifier l\'utilisateur twitter choisi',
+    // actions/twitter.php
+    'SYNDICATION_ACTION_TWITTER' => 'Action {{twitter ...}}',
+    'SYNDICATION_PARAM_USER_REQUIRED' => 'param&egrave;tre "user" manquant, il est obligatoire pour sp&eacute;fifier l\'utilisateur twitter choisi',
 
-// actions/twittersearch.php
-'SYNDICATION_ACTION_TWITTERSEARCH' => 'Action {{twittersearch ...}}',
-'SYNDICATION_PARAM_QUERY_REQUIRED' => 'param&egrave;tre "query" manquant, il est obligatoire pour sp&eacute;cifier votre recherche',
-'SYNDICATION_TEMPLATE_DOESNT_EXISTS' => 'Le fichier template',
-'SYNDICATION_USE_OF_DEFAULT_TEMPLATE' => 'n\'existe pas, on utilise le template par d&eacute;faut'
+    // actions/twittersearch.php
+    'SYNDICATION_ACTION_TWITTERSEARCH' => 'Action {{twittersearch ...}}',
+    'SYNDICATION_PARAM_QUERY_REQUIRED' => 'param&egrave;tre "query" manquant, il est obligatoire pour sp&eacute;cifier votre recherche',
+    'SYNDICATION_TEMPLATE_DOESNT_EXISTS' => 'Le fichier template',
+    'SYNDICATION_USE_OF_DEFAULT_TEMPLATE' => 'n\'existe pas, on utilise le template par d&eacute;faut',
 
-));
+    // templates/accordeon.tpl.html
+    'SYNDICATION_SOURCE' => 'source',
+    'SYNDICATION_SEE_ALL_CONTENT' => 'Accéder à l\'intégralité du contenu',
+];
