4963c0889556337e3cb63f5fcd96e696d897bbfe
acheype <adrien.cheype@gmail.com>
2022-02-01 16:38:11 +0100

    feat(bazar): exportTableToCSV function can be called with various selectors

--- tools/bazar/libs/bazar.js
+++ tools/bazar/libs/bazar.js
@@ -851,9 +851,9 @@ $(document).ready(function () {
   });
 })
 
-function exportTableToCSV(filename) {
+function exportTableToCSV(filename, selector = "table tr") {
   var csv = [];
-  var rows = document.querySelectorAll("table tr");
+  var rows = document.querySelectorAll(selector);
   
   for (var i = 0; i < rows.length; i++) {
       var row = [], cols = rows[i].querySelectorAll("td, th");
@@ -874,22 +874,16 @@ function downloadCSV(csv, filename) {
 
   // CSV file
   csvFile = new Blob([csv], {type: "text/csv"});
-
   // Download link
   downloadLink = document.createElement("a");
-
   // File name
   downloadLink.download = filename;
-
   // Create a link to the file
   downloadLink.href = window.URL.createObjectURL(csvFile);
-
   // Hide download link
   downloadLink.style.display = "none";
-
   // Add the link to DOM
   document.body.appendChild(downloadLink);
-
   // Click download link
   downloadLink.click();
 }
