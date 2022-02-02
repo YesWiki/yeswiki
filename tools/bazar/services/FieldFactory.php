cbcc924236e32a6474dae3f29f79c200a805721b
Jérémy Dufraisse <jeremy.dufraisse-info@orange.fr>
2022-02-02 17:24:14 +0100

    fix(FieldFactory): display error to explain cache folder is not writable instead of white page

--- tools/bazar/services/FieldFactory.php
+++ tools/bazar/services/FieldFactory.php
@@ -6,10 +6,13 @@ use Doctrine\Common\Annotations\AnnotationReader;
 use Doctrine\Common\Annotations\AnnotationRegistry;
 use Doctrine\Common\Annotations\CachedReader;
 use Doctrine\Common\Cache\PhpFileCache;
+use Exception;
+use YesWiki\Core\Service\TemplateEngine;
 use YesWiki\Wiki;
 
 class FieldFactory
 {
+    private const CACHE_PATH = '/../../../cache/';
     protected $wiki;
 
     protected $availableFields;
@@ -17,7 +20,25 @@ class FieldFactory
     public function __construct(Wiki $wiki)
     {
         $this->wiki = $wiki;
+        $this->checkCacheFolderExistence();
         $this->loadAvailableField();
+    }
+
+    private function checkCacheFolderExistence()
+    {
+        try {
+            if (!file_exists(__DIR__ .self::CACHE_PATH) || !is_dir(__DIR__ .self::CACHE_PATH)) {
+                throw new Exception("ERROR ! : Folder `cache/` not existing in the root folder on the website host ! Can you create it ? ");
+            }
+            
+            if (!is_writable(__DIR__ .self::CACHE_PATH)) {
+                throw new Exception("ERROR ! : Folder `cache/` is not writable ! Can you give it write acces by ftp for example (code 770) ?");
+            }
+        } catch (Exception $th) {
+            // raw ouput because here TemplateEngine is not ready (services not already compiled and cache folder not available)
+            echo "<div style=\"border:1px red solid;background-color: #FFCCCC;margin:3px;padding:5px;border-radius:5px;\">{$th->getMessage()}</div>";
+            exit();
+        }
     }
 
     private function loadAvailableField()
@@ -26,7 +47,7 @@ class FieldFactory
 
         $reader = new CachedReader(
             new AnnotationReader(),
-            new PhpFileCache(__DIR__ . '/../../../cache/fields'),
+            new PhpFileCache(__DIR__ . self::CACHE_PATH . 'fields'),
             $debug = true
         );
 
