0c1194ca238bf7ede69a90592013bd3509479d5b
Sebastian Castro <sebastian.castro@protonmail.com>
2022-02-11 21:32:32 +0100

    action-builder-checkbox: handle better default value

--- tools/aceditor/presentation/javascripts/components/InputCheckbox.js
+++ tools/aceditor/presentation/javascripts/components/InputCheckbox.js
@@ -1,28 +1,35 @@
 export default {
   props: [ 'value', 'config' ],
-  computed: {
-    customValue: {
-      get() {
-        let result = this.value
-        if (this.config.checkedvalue) {
-          result = this.value == this.config.checkedvalue
-        }
-        if (result == "false") result = false
-        return result
-      },
-      set(newValue) {
-        if (this.config.checkedvalue) {
-          newValue = newValue ? this.config.checkedvalue : this.config.uncheckedvalue
-        }
-        this.$emit('input', newValue)
-      }
+  data() {
+    return {
+      // boolean internal value cause the real value could be a string when using checkedvalue and uncheckedvalue
+      checked: undefined
+    }
+  },
+  mounted() {
+    if (this.value === undefined) {
+      // if no value, we initialize to false, the the param will be correctly set 
+      // i.e. myparam="false" or myparam="0" if uncheckvalue is defined
+      this.checked = false
+    }
+    else {
+      if (this.config.checkedvalue) this.checked = this.value == this.config.checkedvalue
+      else this.checked = this.value
+    }
+  },
+  watch: {
+    checked() {
+      let result
+      if (this.config.checkedvalue) result = this.checked ? this.config.checkedvalue : this.config.uncheckedvalue
+      else result = this.checked
+      this.$emit('input', result)
     }
   },
   template: `
     <div class="form-group input-group checkbox" :title="config.hint" >
       <addon-icon :config="config" v-if="config.icon"></addon-icon>
       <label>
-        <input type="checkbox" v-model="customValue" />
+        <input type="checkbox" v-model="checked" />
         <span>{{ config.label }}</span>
       </label>
       <input-hint :config="config"></input-hint>
