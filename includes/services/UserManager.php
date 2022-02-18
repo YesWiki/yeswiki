51f0ef1a89a700e6536168171bd4e4875da3ff32
Jérémy Dufraisse <jeremy.dufraisse-info@orange.fr>
2022-02-18 13:16:53 +0100

    refactor(UserManager): implements UserProviderInterface, PasswordUpgraderInterface

--- includes/services/UserManager.php
+++ includes/services/UserManager.php
@@ -2,13 +2,23 @@
 
 namespace YesWiki\Core\Service;
 
+use Exception;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
+use Symfony\Component\Security\Core\Exception\UserNotFoundException;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
 use YesWiki\Bazar\Service\EntryManager;
 use YesWiki\Bazar\Service\Guard;
+use YesWiki\Core\Entity\User;
+use YesWiki\Core\Exception\UserEmailAlreadyUsedException;
+use YesWiki\Core\Exception\UserNameAlreadyUsedException;
 use YesWiki\Security\Controller\SecurityController;
 use YesWiki\Wiki;
 
-class UserManager
+class UserManager implements UserProviderInterface, PasswordUpgraderInterface
 {
     protected $wiki;
     protected $dbService;
@@ -25,21 +35,23 @@ class UserManager
         $this->getOneByNameCacheResults = [];
     }
 
-    public function getOneByName($name, $password = 0): ?array
+    private function arrayToUser(?array $userAsArray = null): ?User
     {
-        if ($password === 0 && isset($this->getOneByNameCacheResults[$name])) {
-            return $this->getOneByNameCacheResults[$name];
+        if (empty($userAsArray)) {
+            return null;
         }
-        $result = $this->dbService->loadSingle('select * from' . $this->dbService->prefixTable('users') . "where name = '" . $this->dbService->escape($name) . "' " . ($password === 0 ? "" : "and password = '" . $this->dbService->escape($password) . "'") . ' limit 1');
-        if ($password === 0) {
-            $this->getOneByNameCacheResults[$name] = $result;
-        }
-        return $result;
+        // be carefull the User::__construct is really strict about list of properties that should set
+        return new User($userAsArray);
     }
 
-    public function getOneByEmail($mail, $password = 0): ?array
+    public function getOneByName($name, $password = 0): ?User
     {
-        return $this->dbService->loadSingle('select * from' . $this->dbService->prefixTable('users') . "where email = '" . $this->dbService->escape($mail) . "' " . ($password === 0 ? "" : "and password = '" . $this->dbService->escape($password) . "'") . ' limit 1');
+        return $this->arrayToUser($this->dbService->loadSingle('select * from' . $this->dbService->prefixTable('users') . "where name = '" . $this->dbService->escape($name) . "' " . ($password === 0 ? "" : "and password = '" . $this->dbService->escape($password) . "'") . ' limit 1'));
+    }
+
+    public function getOneByEmail($mail, $password = 0): ?User
+    {
+        return $this->arrayToUser($this->dbService->loadSingle('select * from' . $this->dbService->prefixTable('users') . "where email = '" . $this->dbService->escape($mail) . "' " . ($password === 0 ? "" : "and password = '" . $this->dbService->escape($password) . "'") . ' limit 1'));
     }
 
     public function getAll($dbFields = ['name', 'password', 'email', 'motto', 'revisioncount', 'changescount', 'doubleclickedit', 'signuptime', 'show_comments']): array
@@ -49,16 +61,25 @@ class UserManager
         } else {
             $prefix = $this->params->get('table_prefix');
         }
-        return $this->dbService->loadAll('select '.implode(', ', $dbFields).' from ' . $prefix . 'users order by name');
+        
+        $selectDefinition = empty($dbFields) ? '*' : implode(', ', $dbFields);
+        return array_map(
+            function ($userAsArray) {
+                return $this->arrayToUser($userAsArray);
+            },
+            $this->dbService->loadAll("select $selectDefinition from {$prefix}users order by name")
+        );
     }
 
     public function getLoggedUser()
     {
+        // TODO use AuthController
         return isset($_SESSION['user']) ? $_SESSION['user'] : '';
     }
 
     public function getLoggedUserName()
     {
+        // TODO use AuthController
         if ($user = $this->getLoggedUser()) {
             $name = $user["name"];
         } else {
@@ -69,6 +90,7 @@ class UserManager
 
     public function login($user, $remember = 0)
     {
+        // TODO use AuthController
         $_SESSION['user'] = $user;
         $this->wiki->SetPersistentCookie('name', $user['name'], $remember);
         $this->wiki->SetPersistentCookie('password', $user['password'], $remember);
@@ -77,16 +99,26 @@ class UserManager
 
     public function logout()
     {
+        // TODO use AuthController
         $_SESSION['user'] = '';
         $this->wiki->DeleteCookie('name');
         $this->wiki->DeleteCookie('password');
         $this->wiki->DeleteCookie('remember');
     }
 
+    /**
+     * @throws UserNameAlreadyUsedException|UserEmailAlreadyUsedException|Exception
+     */
     public function create($wikiName, $email, $password)
     {
         if ($this->securityController->isWikiHibernated()) {
-            throw new \Exception(_t('WIKI_IN_HIBERNATION'));
+            throw new Exception(_t('WIKI_IN_HIBERNATION'));
+        }
+        if (!empty($this->getOneByName($wikiName))) {
+            throw new UserNameAlreadyUsedException();
+        }
+        if (!empty($this->getOneByEmail($email))) {
+            throw new UserEmailAlreadyUsedException();
         }
         $this->getOneByNameCacheResults = [];
         return $this->dbService->query(
@@ -99,6 +131,9 @@ class UserManager
         );
     }
 
+    /**
+     * @throws UserEmailAlreadyUsedException
+     */
     public function updateEmail($wikiName, $email)
     {
         if ($this->securityController->isWikiHibernated()) {
@@ -107,6 +142,11 @@ class UserManager
         $this->getOneByNameCacheResults = [];
         $user = $this->getOneByName($wikiName);
         if (!empty($user)) {
+            if ($user->getEmail() !== $email) {
+                if (!empty($this->getOneByEmail($email))) {
+                    throw new UserEmailAlreadyUsedException();
+                };
+            }
             $query =
                 'UPDATE ' . $this->dbService->prefixTable('users') . 'SET ' .
                 'email = "' . $this->dbService->escape($email) . '"'.
@@ -116,5 +156,85 @@ class UserManager
             $this->dbService->query($query);
         }
         $this->getOneByNameCacheResults = [];
+    }
+
+    /* ~~~~~~~~~~~~~~~~~~ implements  PasswordUpgraderInterface ~~~~~~~~~~~~~~~~~~ */
+
+    /**
+     * Upgrades the hashed password of a user, typically for using a better hash algorithm.
+     * This method should persist the new password in the user storage and update the $user object accordingly.
+     * Because you don't want your users not being able to log in, this method should be opportunistic:
+     * it's fine if it does nothing or if it fails without throwing any exception.
+     * @param PasswordAuthenticatedUserInterface|UserInterface $user
+     * @throws UnsupportedUserException if the user is not supported
+     * @param string $newHashedPassword
+     */
+    public function upgradePassword(PasswordAuthenticatedUserInterface|UserInterface $user, string $newHashedPassword)
+    {
+        if (!$this->supportsClass(get_class($user))) {
+            throw new UnsupportedUserException();
+        }
+        $user->setPassword($newHashedPassword);
+    }
+
+    /* ~~~~~~~~~~~~~~~~~~ implements  UserProviderInterface ~~~~~~~~~~~~~~~~~~ */
+    
+    /**
+     * Refreshes the user.
+     *
+     * It is up to the implementation to decide if the user data should be
+     * totally reloaded (e.g. from the database), or if the UserInterface
+     * object can just be merged into some internal array of users / identity
+     * map.
+     *
+     * @return User
+     *
+     * @throws UnsupportedUserException if the user is not supported
+     * @throws UserNotFoundException    if the user is not found
+     */
+    public function refreshUser(UserInterface $user)
+    {
+        if (!$this->supportsClass(get_class($user))) {
+            throw new UnsupportedUserException();
+        }
+        // currently force refresh
+        return $this->getOneByName($user->getName());
+    }
+
+    /**
+     * Whether this provider supports the given user class.
+     *
+     * @return bool
+     */
+    public function supportsClass(string $class)
+    {
+        if (!class_exists($class)) {
+            // prevent calling autoloader via 'is_a'
+            return false;
+        }
+        return is_a($class, User::class, true);
+    }
+
+    /**
+     * @return User
+     *
+     * @throws UserNotFoundException
+     *
+     * @deprecated since Symfony 5.3, use loadUserByIdentifier() instead
+     */
+    public function loadUserByUsername(string $username)
+    {
+        return $this->loadUserByIdentifier($username);
+    }
+
+    /* ~~~~~~~~~~~~~~~~~~ end of implements ~~~~~~~~~~~~~~~~~~ */
+    /**
+     * @return User
+     *
+     * @throws UserNotFoundException
+     */
+    public function loadUserByIdentifier(string $username)
+    {
+        return $this->getOneByName($username);
     }
 }
