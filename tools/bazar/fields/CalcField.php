8c40ef2370cf9a02b5c0751f61161b905f12e81f
Jérémy Dufraisse <jeremy.dufraisse-info@orange.fr>
2022-03-23 15:57:15 +0100

    feat(CalcField): add in formbuilder

--- tools/bazar/fields/CalcField.php
+++ tools/bazar/fields/CalcField.php
@@ -34,7 +34,7 @@ class CalcField extends BazarField
     protected function renderStatic($entry)
     {
         $value = $this->getValue($entry);
-        if ($value !== "0" && empty($value)) {
+        if (!in_array($value, [0,"0"], true) && empty($value)) {
             // 0 should be displayed but not false or null or ""
             return null;
         }
@@ -51,29 +51,33 @@ class CalcField extends BazarField
         $parenthesis = '\)|\('; // Allowed math operators
         $fieldPropertyName = '[A-Za-z_0-9]+'; // Allowed math operators
         $functions = '(?:sinh?|cosh?|tanh?|abs|acosh?|asinh?|atanh?|exp|log10|deg2rad|rad2deg|sqrt|ceil|floor|round)'; // Allowed PHP functions
-        if (!preg_match_all("/($operators|$parenthesis)|($number)|($fieldPropertyName)|($functions)/", $this->calcFormula, $matches)) {
+        $specialtest = '(?:test\(([A-Za-z_0-9]+),([A-Za-z_0-9,]*)\))';
+        if (!preg_match_all("/($operators|$parenthesis)|($number)|($functions)|$specialtest|($fieldPropertyName)/", $this->calcFormula, $matches)) {
             $value = 0;
         } else {
             $formula = "";
             foreach ($matches[0] as $key => $value) {
-                if (!empty($matches[1][$key]) && empty($matches[2][$key]) && empty($matches[3][$key]) && empty($matches[4][$key])) {
+                if (!empty($matches[1][$key])) {
                     // operators or parenthesis
                     $formula .= $matches[1][$key];
-                } elseif (empty($matches[1][$key]) && !empty($matches[2][$key]) && empty($matches[3][$key]) && empty($matches[4][$key])) {
+                } elseif (!empty($matches[2][$key])) {
                     // number
                     $formula .= floatval($matches[2][$key]);
-                } elseif (empty($matches[1][$key]) && empty($matches[2][$key]) && !empty($matches[3][$key]) && empty($matches[4][$key])) {
-                    // field property name
-                    $formula .= $this->getEntryValue($entry, $matches[3][$key]);
-                } elseif (empty($matches[1][$key]) && empty($matches[2][$key]) && empty($matches[3][$key]) && !empty($matches[4][$key])) {
+                } elseif (!empty($matches[3][$key])) {
                     // functions
-                    $formula .= $matches[4][$key];
+                    $formula .= $matches[3][$key];
+                } elseif (!empty($matches[4][$key])) {
+                    // test
+                    $formula .= $this->testEntryValue($entry, $matches[4][$key], $matches[5][$key] ?? null);
+                } elseif (!empty($matches[6][$key])) {
+                    // field property name
+                    $formula .= $this->getEntryValue($entry, $matches[6][$key]);
                 }
             }
             $formula = preg_replace('/\s+/', '', $formula);
             $regexpToCheckIfMathFormula = '/^(('.$number.'|'.$functions.'\s*\((?1)+\)|\((?1)+\))(?:'.$operators.'(?2))?)+$/';
             // Final regexp, heavily using recursive patterns
-            if (preg_match($regexp, $formula)) {
+            if (preg_match($regexpToCheckIfMathFormula, $formula)) {
                 $formula = preg_replace('!pi|π!', 'pi()', $formula);
                 try {
                     eval("\$value = $formula;");
@@ -90,6 +94,20 @@ class CalcField extends BazarField
     private function getEntryValue($entry, $name, $default = 0)
     {
         return (!isset($entry[$name]) || !is_scalar($entry[$name])) ? $default : floatval($entry[$name]);
+    }
+
+    private function testEntryValue($entry, $name, $value)
+    {
+        $result = false ;
+        if (isset($entry[$name]) && is_scalar($entry[$name])) {
+            $fieldValue = $entry[$name];
+            if (empty($value) && !in_array($value, [0,"0"], true)) {
+                $result = empty($fieldValue);
+            } else {
+                $result = ($fieldValue == $value);
+            }
+        }
+        return $result ?  1 : 0;
     }
 
     public function getCalcFormula(): ?string
