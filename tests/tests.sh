c55ce3e5492b3525fbc55bb5c4eef69acd45fe48
Jérémy Dufraisse <jeremy.dufraisse-info@orange.fr>
2022-06-09 15:39:07 +0200

    fix(phpunit tests): exit error in intermediate tests for tools

--- tests/tests.sh
+++ tests/tests.sh
@@ -1,6 +1,7 @@
 phpunit --do-not-cache-result --stderr tests \
     && (
         for foldername in `ls -d tools/*/tests`
-            do phpunit --do-not-cache-result --stderr $foldername
+        do
+            phpunit --do-not-cache-result --stderr $foldername || exit 1
         done
     )
