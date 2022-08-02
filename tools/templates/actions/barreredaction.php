41d6ddb0ff9b1e7b14c80e66064fb5dfc9f39cbc
Jérémy Dufraisse <jeremy.dufraisse-info@orange.fr>
2022-08-02 19:11:55 +0200

    fix(barreredaction): display it when connected to display favorite button

--- tools/templates/actions/barreredaction.php
+++ tools/templates/actions/barreredaction.php
@@ -9,7 +9,8 @@ use YesWiki\Core\Service\FavoritesManager;
 if (!defined("WIKINI_VERSION")) {
     die("acc&egrave;s direct interdit");
 }
-if ($this->HasAccess("write") && $this->method != "revisions") {
+$user = $this->services->get(AuthController::class)->getLoggedUser();
+if ((!empty($user) || $this->HasAccess("write")) && $this->method != "revisions") {
     // on récupére la page et ses valeurs associées
     $page = $this->GetParameter('page');
     if (empty($page)) {
@@ -32,57 +33,58 @@ if ($this->HasAccess("write") && $this->method != "revisions") {
     // on peut ajouter des classes, la classe par défaut est .footer
     $barreredactionelements['class'] = ($this->GetParameter('class') ? 'footer '.$this->GetParameter('class') : 'footer');
 
-    // on ajoute le lien d'édition si l'action est autorisée
-    if ($this->HasAccess("write", $page) && !$this->services->get(SecurityController::class)->isWikiHibernated()) {
-        $barreredactionelements['linkedit'] = $this->href("edit", $page);
-    }
-
-    if ($time) {
-        // hack to hide E_STRICT error if no timezone set
-        date_default_timezone_set(@date_default_timezone_get());
-        $barreredactionelements['linkrevisions'] = $this->href("revisions", $page);
-        $barreredactionelements['time'] = date(_t('TEMPLATE_DATE_FORMAT'), strtotime($time));
-    }
-
-    // if this page exists
-    if ($content) {
-        $owner = $this->GetPageOwner($page);
-        // message
-        if ($this->UserIsOwner($page)) {
-            $barreredactionelements['owner'] = _t('TEMPLATE_OWNER')." : "._t('TEMPLATE_YOU');
-        } elseif ($owner) {
-            $barreredactionelements['owner'] = _t('TEMPLATE_OWNER')." : ".$owner;
-        } else {
-            $barreredactionelements['owner'] = _t('TEMPLATE_NO_OWNER');
+    if ($this->HasAccess("write")) {
+        // on ajoute le lien d'édition si l'action est autorisée
+        if ($this->HasAccess("write", $page) && !$this->services->get(SecurityController::class)->isWikiHibernated()) {
+            $barreredactionelements['linkedit'] = $this->href("edit", $page);
         }
 
-        // if current user is owner or admin
-        if ($this->UserIsOwner($page) || $this->UserIsAdmin()) {
-            $barreredactionelements['owner'] .= ' - '._t('TEMPLATE_PERMISSIONS');
-            if (!$this->services->get(SecurityController::class)->isWikiHibernated()) {
-                $barreredactionelements['linkacls'] = $this->href("acls", $page);
-                $barreredactionelements['linkdeletepage'] = $this->href("deletepage", $page);
+        if ($time) {
+            // hack to hide E_STRICT error if no timezone set
+            date_default_timezone_set(@date_default_timezone_get());
+            $barreredactionelements['linkrevisions'] = $this->href("revisions", $page);
+            $barreredactionelements['time'] = date(_t('TEMPLATE_DATE_FORMAT'), strtotime($time));
+        }
+
+        // if this page exists
+        if ($content) {
+            $owner = $this->GetPageOwner($page);
+            // message
+            if ($this->UserIsOwner($page)) {
+                $barreredactionelements['owner'] = _t('TEMPLATE_OWNER')." : "._t('TEMPLATE_YOU');
+            } elseif ($owner) {
+                $barreredactionelements['owner'] = _t('TEMPLATE_OWNER')." : ".$owner;
+            } else {
+                $barreredactionelements['owner'] = _t('TEMPLATE_NO_OWNER');
             }
-            $aclsService = $this->services->get(AclService::class);
-            $hasAccessComment = $aclsService->hasAccess('comment');
-            $barreredactionelements['wikigroups'] = $this->GetGroupsList();
-            if ($this->services->get(ParameterBagInterface::class)->get('comments_activated')) {
-                if ($hasAccessComment && $hasAccessComment !== 'comments-closed') {
-                    $barreredactionelements['linkclosecomments'] = $this->href("claim", $page, ['action' => 'closecomments'], false);
-                } else {
-                    $barreredactionelements['linkopencomments'] = $this->href("claim", $page, ['action' => 'opencomments'], false);
+
+            // if current user is owner or admin
+            if ($this->UserIsOwner($page) || $this->UserIsAdmin()) {
+                $barreredactionelements['owner'] .= ' - '._t('TEMPLATE_PERMISSIONS');
+                if (!$this->services->get(SecurityController::class)->isWikiHibernated()) {
+                    $barreredactionelements['linkacls'] = $this->href("acls", $page);
+                    $barreredactionelements['linkdeletepage'] = $this->href("deletepage", $page);
                 }
-            }
-        } elseif (!$owner && $this->GetUser()) {
-            $barreredactionelements['owner'] .= " - "._t('TEMPLATE_CLAIM');
-            if (!$this->services->get(SecurityController::class)->isWikiHibernated()) {
-                $barreredactionelements['linkacls'] = $this->href("claim", $page);
+                $aclsService = $this->services->get(AclService::class);
+                $hasAccessComment = $aclsService->hasAccess('comment');
+                $barreredactionelements['wikigroups'] = $this->GetGroupsList();
+                if ($this->services->get(ParameterBagInterface::class)->get('comments_activated')) {
+                    if ($hasAccessComment && $hasAccessComment !== 'comments-closed') {
+                        $barreredactionelements['linkclosecomments'] = $this->href("claim", $page, ['action' => 'closecomments'], false);
+                    } else {
+                        $barreredactionelements['linkopencomments'] = $this->href("claim", $page, ['action' => 'opencomments'], false);
+                    }
+                }
+            } elseif (!$owner && $this->GetUser()) {
+                $barreredactionelements['owner'] .= " - "._t('TEMPLATE_CLAIM');
+                if (!$this->services->get(SecurityController::class)->isWikiHibernated()) {
+                    $barreredactionelements['linkacls'] = $this->href("claim", $page);
+                }
             }
         }
     }
     $barreredactionelements['linkshare'] = $this->href("share", $page);
 
-    $user = $this->services->get(AuthController::class)->getLoggedUser();
     $favoritesManager = $this->services->get(FavoritesManager::class);
     if (!empty($user) && $favoritesManager->areFavoritesActivated()) {
         $barreredactionelements['currentuser'] = $user['name'];
