c1b38dd8a8b5ccb206070c93399da95cfea78438
mrflos <mrflos@lilo.org>
2022-08-09 13:41:38 +0300

    fix(htmlpurifier) : better typing and call the cleanfile method only runs on files that need it
    (cherry picked from commit a9e38a9923ceed057b6cc128f92594c60f9a1d5f)

--- includes/services/HtmlPurifierService.php
+++ includes/services/HtmlPurifierService.php
@@ -69,7 +69,7 @@ class HtmlPurifierService
      * @param string $content of svg
      * @return string $content
      */
-    public function sanitizeSVG(string $content): string
+    public function sanitizeSVG(string $content): bool|string
     {
         if (!$this->params->get('htmlPurifierActivated')) {
             return $dirtyContent;
@@ -85,17 +85,21 @@ class HtmlPurifierService
      * @param string $content of svg
      * @return mixed false if problem or int of filesize
      */
-    public function cleanFile(string $filename, string $extension): mixed
+    public function cleanFile(string $filename, string $extension): bool|int
     {
         if (file_exists($filename)) {
-            $content = file_get_contents($filename);
-            if ($extension === 'svg') {
-                return file_put_contents($filename, $this->sanitizeSVG($content));
-            } elseif ($extension === 'xml') {
-                return file_put_contents($filename, $this->cleanXSS($content));
-            } elseif ($extension === 'html' || $extension === 'htm' ) {
-                return file_put_contents($filename, $this->cleanHTML($content));
-            }
+	   if (in_array($extension, ['svg', 'xml', 'html', 'htm'])) {	
+		$content = file_get_contents($filename);
+                if ($extension === 'svg') {
+                    return file_put_contents($filename, $this->sanitizeSVG($content));
+                } elseif ($extension === 'xml') {
+                    return file_put_contents($filename, $this->cleanXSS($content));
+                } elseif ($extension === 'html' || $extension === 'htm' ) {
+                    return file_put_contents($filename, $this->cleanHTML($content));
+                }
+	   } else {
+	       return true; // the file type doesn't need to be cleaned
+	   }
         } else {
             return false; //TODO : maybe raise an explicit error in case of non-existing file
         }
