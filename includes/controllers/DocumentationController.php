6d0a65d81bb717c8540fc3885ad8ec3a2a2b85b5
Sebastian Castro <sebastian.castro@protonmail.com>
2022-09-12 15:12:36 +0200

    feat(doc): use dedicate ?doc url instead of using an action

---
+++ includes/controllers/DocumentationController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace YesWiki\Core\Controller;
+
+use YesWiki\Core\YesWikiController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DocumentationController extends YesWikiController
+{
+    /**
+     * @Route("/doc",options={"acl":{"public"}})
+     */
+    public function show()
+    {
+      return new Response($this->render('@core/documentation.twig'));
+    }
+}
