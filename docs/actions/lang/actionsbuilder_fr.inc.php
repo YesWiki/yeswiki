4f204782d8e7dfd966b2e9f43cb5a6ea953530c5
Jérémy Dufraisse <jeremy.dufraisse-info@orange.fr>
2022-09-15 11:43:20 +0200

    feat(adminbackups): create action in vue

--- docs/actions/lang/actionsbuilder_fr.inc.php
+++ docs/actions/lang/actionsbuilder_fr.inc.php
@@ -278,6 +278,7 @@ return [
     "AB_management_despam_hint_details" => "Ceci permet de rétablir les pages vandalisées ou de supprimer les nouvelles pages créées"
             ." par des spams. ATTENTION : la suppression des pages choisies sera DÉFINITITVE.",
     "AB_management_editconfig_label" => "Modifier des paramètres du fichier de configuration",
+    "AB_management_adminbackups_label" => "Gestion des sauvegardes",
     // advanced actions
     "AB_advanced_actions_label" => "Actions avancées",
     "AB_advanced_action_backlinks_label" => "Afficher les pages qui ont un lien vers la page courante",
