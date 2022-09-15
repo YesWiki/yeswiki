386efccc93b2d3f83b05046444667b5b1464c74f
Jérémy Dufraisse <jeremy.dufraisse-info@orange.fr>
2022-09-15 11:43:20 +0200

    feat(Archive): add sql file + associated tests

--- includes/services/ConsoleService.php
+++ includes/services/ConsoleService.php
@@ -4,6 +4,7 @@ namespace YesWiki\Core\Service;
 
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Process\ExecutableFinder;
 use Symfony\Component\Process\PhpExecutableFinder;
 use Symfony\Component\Process\Process;
 use YesWiki\Wiki;
@@ -14,12 +15,14 @@ class ConsoleService
 
     protected $wiki;
     protected $params;
+    protected $executableFinder;
     protected $phpBinaryFinder;
 
     public function __construct(Wiki $wiki, ParameterBagInterface $params)
     {
         $this->wiki = $wiki;
         $this->params = $params;
+        $this->executableFinder = new ExecutableFinder();
         $this->phpBinaryFinder = new PhpExecutableFinder();
     }
 
@@ -29,32 +32,17 @@ class ConsoleService
      * @param string $subfolder
      * @param bool $newConsole
      * @param int $timeoutInSec
-     * @return array ['command'=>['stdout|stderr' => $output]]
+     * @return null|Process
      */
     public function startConsoleAsync(string $command, array $args = [], string $subfolder = "", bool $newConsole = true, int $timeoutInSec = 60): ?Process
     {
-        if (empty($command)) {
-            return null;
-        }
-        if (!empty($subfolder) && !is_dir(basename($subfolder))) {
-            return null;
-        }
-        $folder = getcwd(). (empty($subfolder) ? '' : (DIRECTORY_SEPARATOR . basename($subfolder)));
         $phpBinaryPath = $this->phpBinaryFinder->find();
-        $params = [$phpBinaryPath,self::CONSOLE_BIN,$command];
+        $newCommand = $phpBinaryPath;
+        $newArgs = [self::CONSOLE_BIN,$command];
         foreach ($args as $arg) {
-            $params[] = $arg;
+            $newArgs[] = $arg;
         }
-        $process = new Process($params, $folder);
-        if ($timeoutInSec > 0) {
-            $process->setTimeout($timeoutInSec); // default 60s
-        }
-        // this option allows a subprocess to continue running after the main script exited
-        if ($newConsole) {
-            $process->setOptions(['create_new_console' => true]);
-        }
-        $process->start();
-        return $process;
+        return $this->startRawCommandAsync($newCommand, $newArgs, $subfolder, $newConsole, $timeoutInSec);
     }
 
     public function getProcessOut(Process $process): array
@@ -75,9 +63,9 @@ class ConsoleService
      * @param array $args
      * @param string $subfolder
      * @param int $timeoutInSec
-     * @return array ['stdout|stderr' => $output]
+     * @return null|array ['stdout|stderr' => $output]
      */
-    public function startConsoleSync(string $command, array $args = [], string $subfolder = "", int $timeoutInSec = 60): array
+    public function startConsoleSync(string $command, array $args = [], string $subfolder = "", int $timeoutInSec = 60): ?array
     {
         $process = $this->startConsoleAsync($command, $args, $subfolder, false, $timeoutInSec);
         if (!$process) {
@@ -85,5 +73,105 @@ class ConsoleService
         }
         $process->wait();
         return $this->getProcessOut($process);
+    }
+
+    /**
+     * @param string $command
+     * @param array $args
+     * @param string $subfolder
+     * @param bool $newConsole
+     * @param int $timeoutInSec
+     * @return null|Process
+     */
+    public function startRawCommandAsync(string $command, array $args = [], string $subfolder = "", bool $newConsole = true, int $timeoutInSec = 60): ?Process
+    {
+        if (empty($command)) {
+            return null;
+        }
+        if (!empty($subfolder) && !is_dir(basename($subfolder))) {
+            return null;
+        }
+        $folder = getcwd(). (empty($subfolder) ? '' : (DIRECTORY_SEPARATOR . basename($subfolder)));
+        $params = [$command];
+        foreach ($args as $arg) {
+            $params[] = $arg;
+        }
+        $process = new Process($params, $folder);
+        if ($timeoutInSec > 0) {
+            $process->setTimeout($timeoutInSec); // default 60s
+        }
+        // this option allows a subprocess to continue running after the main script exited
+        if ($newConsole) {
+            $process->setOptions(['create_new_console' => true]);
+        }
+        $process->start();
+        return $process;
+    }
+    
+    /**
+     * @param string $command
+     * @param array $args
+     * @param string $subfolder
+     * @param int $timeoutInSec
+     * @return null|array ['stdout|stderr' => $output]
+     */
+    public function startRawCommandSync(string $command, array $args = [], string $subfolder = "", int $timeoutInSec = 60): ?array
+    {
+        $process = $this->startRawCommandAsync($command, $args, $subfolder, false, $timeoutInSec);
+        if (!$process) {
+            return null;
+        }
+        $process->wait();
+        return $this->getProcessOut($process);
+    }
+
+    /**
+     * @param string $executableName
+     * @param array $args
+     * @param string $subfolder
+     * @param array $extraDirsWhereSearch
+     * @param bool $newConsole
+     * @param int $timeoutInSec
+     * @return null|Process
+     */
+    public function findAndStartExecutableAsync(string $executableName, array $args = [], string $subfolder = "", array $extraDirsWhereSearch=[], bool $newConsole = true, int $timeoutInSec = 60): ?Process
+    {
+        $executable = $this->findExecutable($executableName, $extraDirsWhereSearch);
+        if (empty($executable)) {
+            throw new Exception("Excutable \"$executableName\" not found !");
+        }
+        return $this->startRawCommandAsync($executable, $args, $subfolder, $newConsole, $timeoutInSec);
+    }
+
+    /**
+     * @param string $executableName
+     * @param array $args
+     * @param string $subfolder
+     * @param array $extraDirsWhereSearch
+     * @param int $timeoutInSec
+     * @return null|array ['command'=>['stdout|stderr' => $output]]
+     */
+    public function findAndStartExecutableSync(string $executableName, array $args = [], string $subfolder = "", array $extraDirsWhereSearch=[], int $timeoutInSec = 60): ?array
+    {
+        $process = $this->findAndStartExecutableAsync($executableName, $args, $subfolder, $extraDirsWhereSearch, false, $timeoutInSec);
+        if (!$process) {
+            return null;
+        }
+        $process->wait();
+        return $this->getProcessOut($process);
+    }
+
+    /**
+     * @param string $name
+     * @param array $extraDirs wherer search
+     * @return string
+     * @throws Exception
+     */
+    protected function findExecutable(string $name, array $extraDirs = []): string
+    {
+        if (empty($name)) {
+            throw new Exception("'name' should nt be empty !");
+        }
+        return $this->executableFinder->find($name, "", $extraDirs);
     }
 }
