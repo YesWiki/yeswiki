a523e4e16e0bb988edcaa94c4d4cfbc5ba6641f1
Jérémy Dufraisse <jeremy.dufraisse-info@orange.fr>
2022-09-15 11:43:21 +0200

    feat(Archive): start Archive from frontend

--- includes/controllers/ArchiveController.php
+++ includes/controllers/ArchiveController.php
@@ -84,8 +84,22 @@ class ArchiveController extends YesWikiController
                 );
                 break;
             case 'startArchive':
+                if (isset($_POST['params']) && !is_array($_POST['params'])) {
+                    return new ApiResponse(
+                        ['error' => "\$_POST['params'] should be set and be an array for action 'startArchive'"],
+                        Response::HTTP_BAD_REQUEST
+                    );
+                }
+                $params = (isset($_POST['params']) && is_array($_POST['params'])) ? $_POST['params'] : [];
+                $pid = $this->startArchiveAsync($params);
+                if (empty($pid)) {
+                    return new ApiResponse(
+                        ['error' => 'no process created when starting archive action'],
+                        Response::HTTP_INTERNAL_SERVER_ERROR
+                    );
+                }
                 return new ApiResponse(
-                    [],
+                    ['pid' => $pid],
                     Response::HTTP_OK
                 );
                 break;
@@ -123,5 +137,36 @@ class ArchiveController extends YesWikiController
                 );
                 break;
         }
+    }
+
+    /**
+     * start archive async via CLI
+     * @param array $params
+     *
+     * @param bool $savefiles
+     * @param bool $savedatabase
+     * @param array $extrafiles
+     * @param array $excludedfiles
+     * @return string PID
+     */
+    protected function startArchiveAsync(
+        array $params = []
+    ): string {
+        $savefiles = (isset($params['savefiles']) && in_array($params['savefiles'], [1,"1",true,'true'], true));
+        $savedatabase = (isset($params['savedatabase']) && in_array($params['savedatabase'], [1,"1",true,'true'], true));
+        $extrafiles = (isset($params['extrafiles']) && is_array($params['extrafiles'])) ? $params['extrafiles'] : [];
+        $excludedfiles = (isset($params['excludedfiles']) && is_array($params['excludedfiles'])) ? $params['excludedfiles'] : [];
+
+        $extrafiles = array_filter($extrafiles, 'is_string');
+        $excludedfiles = array_filter($excludedfiles, 'is_string');
+
+        $process = $this->archiveService->startArchiveAsync(
+            $savefiles,
+            $savedatabase,
+            $extrafiles,
+            $excludedfiles
+        );
+        $processId = empty($process) ? "" : $process->getPid();
+        return empty($processId) ? "" : $processId;
     }
 }
