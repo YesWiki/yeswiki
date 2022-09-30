40c8bd7d6b6ec4d4a99f658c7f8b229ab1e35861
Sebastian Castro <sebastian.castro@protonmail.com>
2022-09-30 21:27:31 +0200

    doc(hello-world): adds comments

--- tools/helloworld/services/GreetingService.php
+++ tools/helloworld/services/GreetingService.php
@@ -2,6 +2,17 @@
 
 namespace YesWiki\HelloWorld\Service;
 
+// How to register your service?
+// =============================
+//
+// You need to make sure following code is present in the tools/XXX/config.yaml
+// services:
+//   _defaults:
+//      autowire: true
+//      public: true
+//
+//   YesWiki\HelloWorld\Service\:
+//      resource: 'services/*'
 
 class GreetingService
 {
