fb43df01a1a45a532a51d7f310c604463b0ceb0c
Sebastian Castro <sebastian.castro@protonmail.com>
2022-09-30 09:05:42 +0200

    refactor(aceditor): small stuff

--- tools/aceditor/presentation/javascripts/aceditor.js
+++ tools/aceditor/presentation/javascripts/aceditor.js
@@ -1,92 +1,77 @@
 import { createAceditorToolbar } from "./aceditor-toolbar.js"
 import { setupAceditorKeyBindings } from './aceditor-key-bindings.js'
 
-/*
- Transform a textarea into an Ace editor, with toolbar
-*/
-;(function ($, window ) {
-  // Create the defaults once
-  var pluginName = 'aceditor',
-      defaults = {
-        savebtn: false,
-        syntax: 'yeswiki',
-        class: ""
-      };
-
-  // The actual plugin constructor
-  function Plugin( element, options ) {
-    this.element = element;
-    this.lang = wiki.lang;
-    this.wiki = wiki;
-
-    this.options = $.extend( {}, defaults, options)
-
-    this._defaults = defaults;
-    this._name = pluginName;
-
-    this.init();
-    return this.aceditor;
-  }
-
-  Plugin.prototype.init = function () {
-    // Place initialization logic here
-    // You already have access to the DOM element and the options via the instance,
-    // e.g., this.element and this.options
-    if ($(this.element).is('textarea')) {
-      var textarea = $(this.element);
-      var aceeditor = $('<div class="ace-editor-container ' + this.options.class +'"><pre class="ace-body"></pre></div>')
-
-      var aceditor = ace.edit(aceeditor.find('pre')[0], {
-        printMargin: false,
-        mode: "ace/mode/" + this.options.syntax,
-        showGutter: true,
-        wrap: 'free',
-        maxLines: Infinity,
-        minLines: $(this.element).attr('rows'),
-        showFoldWidgets:false,
-        fontSize: "18px",
-        useSoftTabs: false,
-        tabSize: 3,
-        fontFamily: 'monospace',
-        highlightActiveLine: true,
-      });
-      aceditor.getSession().setValue(textarea.val());
-      aceditor.getSession().on('change', function(){
-        textarea.val(aceditor.getSession().getValue());
-      });
-      aceditor.on('change', function(){
-        if (typeof showPopup !== "undefined") { showPopup = 1 };
-      });
-
-      textarea.data('aceditor', aceditor);
-
-      setupAceditorKeyBindings(aceeditor)
-
-      // Add buttonbar and aceeditor container over textarea
-
-      var toolbar = createAceditorToolbar(textarea, aceditor, this.options)
-      textarea.before(toolbar)
-      textarea.after(aceeditor);
-      textarea.hide();
-      textarea.addClass('textarea-aceditor')
+// Aceditor Plugin
+// Transform a textarea into an Ace editor, with toolbar
+$.fn['aceditor'] = function (options) {
+  // Lightweight plugin wrapper, preventing against multiple instantiations
+  return this.each(function () {
+    if (!$.data(this, 'plugin_acedior')) {
+      $.data(this, 'plugin_acedior', new Plugin(this, options))
     }
-    return aceditor;
+  })
+}
+
+// The actual plugin constructor
+function Plugin(element, options) {
+  this.element = element;
+  const defaults = {
+    savebtn: false,
+    syntax: 'yeswiki'
   };
+  this.options = $.extend({}, defaults, options)
 
-  // A really lightweight plugin wrapper around the constructor,
-  // preventing against multiple instantiations
-  $.fn[pluginName] = function ( options ) {
-    return this.each(function () {
-      if (!$.data(this, 'plugin_' + pluginName)) {
-        $.data(this, 'plugin_' + pluginName, new Plugin( this, options ));
-      }
+  return this.init()
+}
+
+Plugin.prototype.init = function () {
+  // Place initialization logic here
+  // You already have access to the DOM element and the options via the instance,
+  // e.g., this.element and this.options
+  if ($(this.element).is('textarea')) {
+    var textarea = $(this.element);
+
+    var $editorContainer = $('<div class="ace-editor-container"><pre class="ace-body"></pre></div>')
+    setupAceditorKeyBindings($editorContainer)
+
+    var aceditor = ace.edit($editorContainer.find('pre')[0], {
+      printMargin: false,
+      mode: "ace/mode/" + this.options.syntax,
+      showGutter: true,
+      wrap: 'free',
+      maxLines: Infinity,
+      minLines: $(this.element).attr('rows'),
+      showFoldWidgets:false,
+      fontSize: "18px",
+      useSoftTabs: false,
+      tabSize: 3,
+      fontFamily: 'monospace',
+      highlightActiveLine: true,
     });
-  }
 
-}(jQuery, window));
+    // Sync textarea and editor
+    aceditor.getSession().setValue(textarea.val());
+    aceditor.getSession().on('change', function(){
+      textarea.val(aceditor.getSession().getValue());
+    });
+
+    // Enable alert popup when leaving the page
+    aceditor.on('change', function(){
+      if (typeof showPopup !== "undefined") { showPopup = 1 };
+    });
+
+    // Setup DOM
+    var toolbar = createAceditorToolbar(textarea, aceditor, this.options)
+    textarea.data('aceditor', aceditor);
+    textarea.before(toolbar)
+    textarea.after($editorContainer);
+    textarea.hide().addClass('textarea-aceditor');
+  }
+  return aceditor;
+}
 
 // Edit handler of yeswiki
-$('#body').aceditor({savebtn : true, class: "big"});
+$('#body').aceditor({savebtn : true});
 
 // For comments and Bazar's textarea
 $('.wiki-textarea, .commentform textarea').aceditor();
