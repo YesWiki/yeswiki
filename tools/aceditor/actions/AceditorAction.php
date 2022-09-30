ff37d9795cd5be67e737394a4de7d346c9c94441
Sebastian Castro <sebastian.castro@protonmail.com>
2022-09-30 22:52:57 +0200

    refactor(aceditor): render toolbar with twig

--- tools/aceditor/actions/AceditorAction.php
+++ tools/aceditor/actions/AceditorAction.php
@@ -8,13 +8,17 @@ use YesWiki\Core\YesWikiAction;
 
 class AceditorAction extends YesWikiAction
 {
+    public function formatArguments($args)
+    {
+        return [
+            'saveButton' => $this->formatBoolean($args['saveButton'], false)
+        ];
+    }
+
     public function run()
     {
-        $this->wiki->AddJavascriptFile('tools/aceditor/presentation/javascripts/aceditor.js', false, true);
-        $this->wiki->AddCSSFile('tools/aceditor/presentation/styles/aceditor.css');
-
         $data = $this->getService(ActionsBuilderService::class)->getData();
 
-        return $this->render('@aceditor/actions-builder.twig', ['data' => $data]);
+        return $this->render('@aceditor/aceditor.twig', ['actionsBuilderData' => $data]);
     }
 }
