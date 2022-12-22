374778474088664390acc1227526ec1465d0b9bb
Jérémy Dufraisse <jeremy.dufraisse-info@orange.fr>
2022-12-22 11:37:47 +0100

    refactor(CommentService): filter comments o read acl in ::loadComments

--- actions/CommentsTableAction.php
+++ actions/CommentsTableAction.php
@@ -1,24 +1,17 @@
 <?php
 
 use YesWiki\Core\YesWikiAction;
-use YesWiki\Core\Service\AclService;
 use YesWiki\Core\Service\CommentService;
 
 class CommentsTableAction extends YesWikiAction
 {
-    protected $aclService;
     protected $commentsService;
 
     public function run()
     {
         // get Services
-        $this->aclService  = $this->getService(AclService::class);
         $this->commentsService  = $this->getService(CommentService::class);
         $coms = $this->commentsService->loadComments(''); # get all comments
-        // filter on read acl on parent page
-        $coms = array_filter($coms, function ($com) {
-            return !empty($com['comment_on']) && $this->aclService->hasAccess('read', $com['comment_on']);
-        });
         return $this->render('@core/comment-table.twig', [
             'comments' => $coms,
         ]) ;
