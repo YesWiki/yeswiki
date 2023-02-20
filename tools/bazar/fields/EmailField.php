4c2066bbdf9ea7bb39d5374e71e53a66a444313c
Jérémy Dufraisse <jeremy.dufraisse-info@orange.fr>
2023-02-20 14:56:09 +0100

    feat(EmailField): add acls to see email in templates

--- tools/bazar/fields/EmailField.php
+++ tools/bazar/fields/EmailField.php
@@ -4,17 +4,20 @@ namespace YesWiki\Bazar\Field;
 
 use Psr\Container\ContainerInterface;
 use YesWiki\Bazar\Controller\ApiController as BazarApiController;
+use YesWiki\Core\Service\AclService;
 
 /**
  * @Field({"champs_mail"})
  */
 class EmailField extends BazarField
 {
+    protected $seeEmailAcls;
     protected $sendMail;
-    public $showContactForm;
+    protected $showContactForm;
 
     // Field-specific
     protected const FIELD_SHOW_CONTACT_FORM = 6;
+    protected const FIELD_SEE_MAIL_ACLS = 4;
     protected const FIELD_SEND_EMAIL = 9;
 
     public function __construct(array $values, ContainerInterface $services)
@@ -25,6 +28,11 @@ class EmailField extends BazarField
         $this->sendMail = $values[self::FIELD_SEND_EMAIL] == 1;
         $this->showContactForm = $values[self::FIELD_SHOW_CONTACT_FORM] === 'form';
         $this->maxChars = $this->maxChars ?? 255;
+        $this->seeEmailAcls = (!empty($values[self::FIELD_SEE_MAIL_ACLS]) && is_string($values[self::FIELD_SEE_MAIL_ACLS]) && !empty(trim($values[self::FIELD_SEE_MAIL_ACLS])))
+            ? trim($values[self::FIELD_SEE_MAIL_ACLS])
+            : '@admins' ; // default
+        $this->seeEmailAcls = str_replace(',',"\n",$this->seeEmailAcls);
+        $this->maxChars = '';
     }
 
     public function formatValuesBeforeSave($entry)
@@ -63,10 +71,14 @@ class EmailField extends BazarField
 
     public function canRead($entry, ?string $userNameForRendering = null)
     {
+        $aclService = $this->getService(AclService::class);
         return parent::canRead($entry,$userNameForRendering) && (
             !$this->getShowContactForm() ||
             // cas des formulaires champs mails, qui ne doivent pas apparaitre en /raw
-            $this->canDisplayEmailForThisUrl()
+            (
+                $this->canDisplayEmailForThisUrl() &&
+                $aclService->check($this->getSeeEmailAcls(), $userNameForRendering, true)
+            )
         );
     }
 
@@ -92,6 +104,11 @@ class EmailField extends BazarField
         return $this->showContactForm ;
     }
 
+    public function getSeeEmailAcls(): string
+    {
+        return $this->seeEmailAcls ;
+    }
+
     // change return of this method to keep compatible with php 7.3 (mixed is not managed)
     #[\ReturnTypeWillChange]
     public function jsonSerialize()
@@ -100,7 +117,8 @@ class EmailField extends BazarField
             parent::jsonSerialize(),
             [
                 'sendMail' => $this->sendMail,
-                'showContactForm' => $this->getShowContactForm()
+                'showContactForm' => $this->getShowContactForm(),
+                'seeEmailAcls' => $this->getSeeEmailAcls()
             ]
         );
     }
