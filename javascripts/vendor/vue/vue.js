be1f54f8c18555a1fae79731a26f13672d517258
Jérémy Dufraisse <jeremy.dufraisse-info@orange.fr>
2023-03-09 15:28:21 +0100

    shore(yarn): upgrade

--- javascripts/vendor/vue/vue.js
+++ javascripts/vendor/vue/vue.js
@@ -1,5 +1,5 @@
 /*!
- * Vue.js v2.7.13
+ * Vue.js v2.7.14
  * (c) 2014-2022 Evan You
  * Released under the MIT License.
  */
@@ -881,77 +881,6 @@
       });
   });
 
-  var rawMap = new WeakMap();
-  function reactive(target) {
-      makeReactive(target, false);
-      return target;
-  }
-  /**
-   * Return a shallowly-reactive copy of the original object, where only the root
-   * level properties are reactive. It also does not auto-unwrap refs (even at the
-   * root level).
-   */
-  function shallowReactive(target) {
-      makeReactive(target, true);
-      def(target, "__v_isShallow" /* ReactiveFlags.IS_SHALLOW */, true);
-      return target;
-  }
-  function makeReactive(target, shallow) {
-      // if trying to observe a readonly proxy, return the readonly version.
-      if (!isReadonly(target)) {
-          {
-              if (isArray(target)) {
-                  warn$2("Avoid using Array as root value for ".concat(shallow ? "shallowReactive()" : "reactive()", " as it cannot be tracked in watch() or watchEffect(). Use ").concat(shallow ? "shallowRef()" : "ref()", " instead. This is a Vue-2-only limitation."));
-              }
-              var existingOb = target && target.__ob__;
-              if (existingOb && existingOb.shallow !== shallow) {
-                  warn$2("Target is already a ".concat(existingOb.shallow ? "" : "non-", "shallow reactive object, and cannot be converted to ").concat(shallow ? "" : "non-", "shallow."));
-              }
-          }
-          var ob = observe(target, shallow, isServerRendering() /* ssr mock reactivity */);
-          if (!ob) {
-              if (target == null || isPrimitive(target)) {
-                  warn$2("value cannot be made reactive: ".concat(String(target)));
-              }
-              if (isCollectionType(target)) {
-                  warn$2("Vue 2 does not support reactive collection types such as Map or Set.");
-              }
-          }
-      }
-  }
-  function isReactive(value) {
-      if (isReadonly(value)) {
-          return isReactive(value["__v_raw" /* ReactiveFlags.RAW */]);
-      }
-      return !!(value && value.__ob__);
-  }
-  function isShallow(value) {
-      return !!(value && value.__v_isShallow);
-  }
-  function isReadonly(value) {
-      return !!(value && value.__v_isReadonly);
-  }
-  function isProxy(value) {
-      return isReactive(value) || isReadonly(value);
-  }
-  function toRaw(observed) {
-      var raw = observed && observed["__v_raw" /* ReactiveFlags.RAW */];
-      return raw ? toRaw(raw) : observed;
-  }
-  function markRaw(value) {
-      if (isObject(value)) {
-          rawMap.set(value, true);
-      }
-      return value;
-  }
-  /**
-   * @internal
-   */
-  function isCollectionType(value) {
-      var type = toRawType(value);
-      return (type === 'Map' || type === 'WeakMap' || type === 'Set' || type === 'WeakSet');
-  }
-
   var arrayKeys = Object.getOwnPropertyNames(arrayMethods);
   var NO_INIITIAL_VALUE = {};
   /**
@@ -1041,7 +970,6 @@
           (isArray(value) || isPlainObject(value)) &&
           Object.isExtensible(value) &&
           !value.__v_skip /* ReactiveFlags.SKIP */ &&
-          !rawMap.has(value) &&
           !isRef(value) &&
           !(value instanceof VNode)) {
           return new Observer(value, shallow, ssrMockReactivity);
@@ -1214,6 +1142,77 @@
       }
   }
 
+  function reactive(target) {
+      makeReactive(target, false);
+      return target;
+  }
+  /**
+   * Return a shallowly-reactive copy of the original object, where only the root
+   * level properties are reactive. It also does not auto-unwrap refs (even at the
+   * root level).
+   */
+  function shallowReactive(target) {
+      makeReactive(target, true);
+      def(target, "__v_isShallow" /* ReactiveFlags.IS_SHALLOW */, true);
+      return target;
+  }
+  function makeReactive(target, shallow) {
+      // if trying to observe a readonly proxy, return the readonly version.
+      if (!isReadonly(target)) {
+          {
+              if (isArray(target)) {
+                  warn$2("Avoid using Array as root value for ".concat(shallow ? "shallowReactive()" : "reactive()", " as it cannot be tracked in watch() or watchEffect(). Use ").concat(shallow ? "shallowRef()" : "ref()", " instead. This is a Vue-2-only limitation."));
+              }
+              var existingOb = target && target.__ob__;
+              if (existingOb && existingOb.shallow !== shallow) {
+                  warn$2("Target is already a ".concat(existingOb.shallow ? "" : "non-", "shallow reactive object, and cannot be converted to ").concat(shallow ? "" : "non-", "shallow."));
+              }
+          }
+          var ob = observe(target, shallow, isServerRendering() /* ssr mock reactivity */);
+          if (!ob) {
+              if (target == null || isPrimitive(target)) {
+                  warn$2("value cannot be made reactive: ".concat(String(target)));
+              }
+              if (isCollectionType(target)) {
+                  warn$2("Vue 2 does not support reactive collection types such as Map or Set.");
+              }
+          }
+      }
+  }
+  function isReactive(value) {
+      if (isReadonly(value)) {
+          return isReactive(value["__v_raw" /* ReactiveFlags.RAW */]);
+      }
+      return !!(value && value.__ob__);
+  }
+  function isShallow(value) {
+      return !!(value && value.__v_isShallow);
+  }
+  function isReadonly(value) {
+      return !!(value && value.__v_isReadonly);
+  }
+  function isProxy(value) {
+      return isReactive(value) || isReadonly(value);
+  }
+  function toRaw(observed) {
+      var raw = observed && observed["__v_raw" /* ReactiveFlags.RAW */];
+      return raw ? toRaw(raw) : observed;
+  }
+  function markRaw(value) {
+      // non-extensible objects won't be observed anyway
+      if (Object.isExtensible(value)) {
+          def(value, "__v_skip" /* ReactiveFlags.SKIP */, true);
+      }
+      return value;
+  }
+  /**
+   * @internal
+   */
+  function isCollectionType(value) {
+      var type = toRawType(value);
+      return (type === 'Map' || type === 'WeakMap' || type === 'Set' || type === 'WeakSet');
+  }
+
   /**
    * @internal
    */
@@ -1349,8 +1348,8 @@
       return ref;
   }
 
-  var rawToReadonlyMap = new WeakMap();
-  var rawToShallowReadonlyMap = new WeakMap();
+  var rawToReadonlyFlag = "__v_rawToReadonly";
+  var rawToShallowReadonlyFlag = "__v_rawToShallowReadonly";
   function readonly(target) {
       return createReadonly(target, false);
   }
@@ -1369,18 +1368,21 @@
           }
           return target;
       }
+      if (!Object.isExtensible(target)) {
+          warn$2("Vue 2 does not support creating readonly proxy for non-extensible object.");
+      }
       // already a readonly object
       if (isReadonly(target)) {
           return target;
       }
       // already has a readonly proxy
-      var map = shallow ? rawToShallowReadonlyMap : rawToReadonlyMap;
-      var existingProxy = map.get(target);
+      var existingFlag = shallow ? rawToShallowReadonlyFlag : rawToReadonlyFlag;
+      var existingProxy = target[existingFlag];
       if (existingProxy) {
           return existingProxy;
       }
       var proxy = Object.create(Object.getPrototypeOf(target));
-      map.set(target, proxy);
+      def(target, existingFlag, proxy);
       def(proxy, "__v_isReadonly" /* ReactiveFlags.IS_READONLY */, true);
       def(proxy, "__v_raw" /* ReactiveFlags.RAW */, target);
       if (isRef(target)) {
@@ -3994,7 +3996,7 @@
   /**
    * Note: also update dist/vue.runtime.mjs when adding new exports to this file.
    */
-  var version = '2.7.13';
+  var version = '2.7.14';
   /**
    * @internal type is manually declared in <root>/types/v3-define-component.d.ts
    */
@@ -5196,7 +5198,8 @@
   /**
    * Helper that recursively merges two data objects together.
    */
-  function mergeData(to, from) {
+  function mergeData(to, from, recursive) {
+      if (recursive === void 0) { recursive = true; }
       if (!from)
           return to;
       var key, toVal, fromVal;
@@ -5210,7 +5213,7 @@
               continue;
           toVal = to[key];
           fromVal = from[key];
-          if (!hasOwn(to, key)) {
+          if (!recursive || !hasOwn(to, key)) {
               set(to, key, fromVal);
           }
           else if (toVal !== fromVal &&
@@ -5370,7 +5373,19 @@
                           extend(ret, childVal);
                       return ret;
                   };
-  strats.provide = mergeDataOrFn;
+  strats.provide = function (parentVal, childVal) {
+      if (!parentVal)
+          return childVal;
+      return function () {
+          var ret = Object.create(null);
+          mergeData(ret, isFunction(parentVal) ? parentVal.call(this) : parentVal);
+          if (childVal) {
+              mergeData(ret, isFunction(childVal) ? childVal.call(this) : childVal, false // non-recursive
+              );
+          }
+          return ret;
+      };
+  };
   /**
    * Default strategy.
    */
@@ -11124,7 +11139,7 @@
           !el.key) {
           state.warn("<".concat(el.tag, " v-for=\"").concat(alias, " in ").concat(exp, "\">: component lists rendered with ") +
               "v-for should have explicit keys. " +
-              "See https://vuejs.org/guide/list.html#key for more info.", el.rawAttrsMap['v-for'], true /* tip */);
+              "See https://v2.vuejs.org/v2/guide/list.html#key for more info.", el.rawAttrsMap['v-for'], true /* tip */);
       }
       el.forProcessed = true; // avoid recursion
       return ("".concat(altHelper || '_l', "((").concat(exp, "),") +
