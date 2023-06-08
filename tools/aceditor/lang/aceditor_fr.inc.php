86fdf2a12650e80120cd4fdeb761af967337f2ad
Marseault Laurent <laurent@marseault.info>
2023-06-08 15:19:18 +0200

    Update aceditor_fr.inc.php
    fote d'orthographe

--- tools/aceditor/lang/aceditor_fr.inc.php
+++ tools/aceditor/lang/aceditor_fr.inc.php
@@ -1,10 +1,10 @@
 <?php
 
 $GLOBALS['translations'] = array_merge($GLOBALS['translations'], array(
-    'ACTION_BUILDER_CHOOSE_FORM' => 'Choisissez un Formulaire',
+    'ACTION_BUILDER_CHOOSE_FORM' => 'Choisissez un formulaire',
     'ACTION_BUILDER_CHOOSE_TEMPLATE' => 'Sous quelle forme voulez-vous afficher les données?',
     'ACTION_BUILDER_CHOOSE_ACTION' => 'Choisissez une action',
-    'ACTION_BUILDER_WIKI_CODE_TITLE' => "Code à include dans la page",
+    'ACTION_BUILDER_WIKI_CODE_TITLE' => "Code à inclure dans la page",
     'ACTION_BUILDER_PARAMETERS' => 'Paramètres',
     'ACTION_BUILDER_TEMPLATE_CUSTOM' => 'Template custom',
     'ACTION_BUILDER_SEVERAL_FORMS_HINT' => 'Nb. : Il est possible d\'afficher plusieurs bases de donn&eacute;es d\'un coup en entrant leur id s&eacute;par&eacute; par une virgule [<a href="https://yeswiki.net/?ActionBazarliste">acc&eacute;der &agrave; la documentation</a>].',
