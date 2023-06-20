3b8118b8ab18ea5bd70e1d66bd86d47f74de45e0
Jérémy Dufraisse <jeremy.dufraisse-info@orange.fr>
2023-06-20 12:11:32 +0200

    fix(PopuEntryField): do not use lazy for images, because already lazy
    needed to catch error and ask dynamically refreshing the cache image

--- tools/bazar/presentation/javascripts/components/PopupEntryField.js
+++ tools/bazar/presentation/javascripts/components/PopupEntryField.js
@@ -66,7 +66,6 @@ export default {
     <EntryField v-else-if="renderViaEntryField" :entry="entry" :prop="prop" v-bind="$attrs"></EntryField>
     <h3 v-else-if="type == 'titre' && value" v-html="value" v-bind="$attrs"></h3>
     <img
-      loading="lazy" 
       v-else-if="type == 'image' && value" class="popup-visual" 
       v-bind="$attrs"
       :src="$root.urlImage(entry,prop,imagewidth,imageheight,imagemethod)"
