7f692287bef635ded05702dc5afb2dcf8582a76c
Jérémy Dufraisse <jeremy.dufraisse-info@orange.fr>
2023-06-23 12:41:40 +0200

    refactor(YesWiki:FormOpen): use _form-open.twig

--- includes/YesWiki.php
+++ includes/YesWiki.php
@@ -625,24 +625,7 @@ class Wiki
     // FORMS
     public function FormOpen($method = '', $tag = '', $formMethod = 'post', $class = '')
     {
-        if ($method=='edit' || $method=='editiframe') {
-            $result  = '<form id="ACEditor" name="ACEditor" enctype="multipart/form-data" action="'.$this->href($method, $tag).'" method="'.$formMethod.'"';
-            $result .= !empty($class) ? ' class="'.$class.'"' : '';
-            $result .= ">\n";
-            if (isset($this->config['password_for_editing']) and !empty($this->config['password_for_editing'])
-                and isset($_POST['password_for_editing'])) {
-                $result .= '<input type="hidden" name="password_for_editing" value="'.$_POST['password_for_editing'].'" />'."\n";
-            }
-        } else {
-            $result = '<form action="'.$this->href($method, $tag).'" method="'.$formMethod.'"';
-            $result .= !empty($class) ? ' class="'.$class.'"' : '';
-            $result .= ">\n";
-        }
-
-        if (!$this->config["rewrite_mode"]) {
-            $result .= '<input type="hidden" name="wiki" value="'.$this->MiniHref($method, $tag).'" />'."\n";
-        }
-        return $result;
+        return $this->render('@core/_form-open.twig',compact(['method','tag','formMethod','class']));
     }
 
     public function FormClose()
