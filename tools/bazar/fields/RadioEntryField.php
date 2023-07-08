c6d1a00e5192da534d556be7b4d5b3dfba0874e1
acheype <adrien.cheype@gmail.com>
2023-07-08 12:19:28 +0200

    fix(EntryField): better entries browsing in modals

--- tools/bazar/fields/RadioEntryField.php
+++ tools/bazar/fields/RadioEntryField.php
@@ -44,10 +44,12 @@ class RadioEntryField extends RadioField
             $entryUrl = $this->services->get(Wiki::class)->Href('', $value);
         }
 
+        $isIframe = !empty(testUrlInIframe());
         return $this->render('@bazar/fields/select_entry.twig', [
             'value' => $value,
             'label' => $this->getOptions()[$value],
-            'entryUrl' => $entryUrl
+            'entryUrl' => $entryUrl,
+            'isIframe' => $isIframe,
         ]);
     }
 
