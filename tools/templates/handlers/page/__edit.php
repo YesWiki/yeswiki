81f635a24fe85705d912b1d86920798e97b6127f
Jérémy Dufraisse <jeremy.dufraisse-info@orange.fr>
2023-10-16 22:51:05 +0200

    fix(__editHandler): prevent registering metadata before testing acls and security

--- tools/templates/handlers/page/__edit.php
+++ tools/templates/handlers/page/__edit.php
@@ -6,22 +6,6 @@ if (!defined("WIKINI_VERSION")) {
     die("acc&egrave;s direct interdit");
 }
 
-// Sauvegarde des metas
-if (isset($_GET["newpage"]) && $_GET["newpage"]==1 && isset($_GET["theme"]) && !isset($this->page['metadatas']['theme'])) {
-    $metadata = [
-        'theme' => $_GET["theme"],
-        'style' => $_GET["style"] ?? CSS_PAR_DEFAUT ,
-        'squelette' => $_GET["squelette"] ?? SQUELETTE_PAR_DEFAUT ,
-        'bgimg' => $_GET["bgimg"] ?? null
-    ];
-    foreach (ThemeManager::SPECIAL_METADATA as $metadataName) {
-        if (!empty($_GET[$metadataName])) {
-            $metadata[$metadataName] = $_GET[$metadataName];
-        }
-    }
-    $this->SaveMetaDatas($this->GetPageTag(), $metadata);
-}
-
 // Si une valeur de body est passee en paramétre GET (et pas POST) on l'ajoute en titre dans la nouvelle page vierge
 if (isset($_GET["body"]) && !isset($_POST["body"])) {
     $_POST["body"] = '======'.$_GET["body"].'======';
