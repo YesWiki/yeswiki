0312fe3dad5087a758d520f38149a0fdf76daaba
Florian <mrflos@gmail.com>
2023-12-20 16:37:37 +0000

    Translated using Weblate (English)
    Currently translated at 100.0% (9 of 9 strings)
    
    Translation: YesWiki/YesWiki extension Rss
    Translate-URL: https://hosted.weblate.org/projects/yeswiki/yeswiki-extension-rss/en/

--- tools/rss/lang/rss_en.inc.php
+++ tools/rss/lang/rss_en.inc.php
@@ -1,14 +1,14 @@
 <?php
 return [
     // actions/recentchangesrss.php
-    'RSS_CHANGE_OF' => '',
-    'RSS_HISTORY' => '',
-    'RSS_HIDDEN_CONTENT' => '',
-    'RSS_ON_DATE' => '',
+    'RSS_CHANGE_OF' => 'Modification of',
+    'RSS_HISTORY' => 'history',
+    'RSS_HIDDEN_CONTENT' => 'Hidden content',
+    'RSS_ON_DATE' => 'on',
     // libs/rssdiff.function.php
-    'RSS_COMPARISON_OF' => '',
-    'RSS_TO' => '',
-    'RSS_ADDS' => '',
-    'RSS_DELETIONS' => '',
-    'RSS_NO_DIFF' => '',
+    'RSS_COMPARISON_OF' => 'Comparison of',
+    'RSS_TO' => 'to',
+    'RSS_ADDS' => 'Addings',
+    'RSS_DELETIONS' => 'Deletions',
+    'RSS_NO_DIFF' => 'No difference',
 ];
