5058634d1a775a3be12a3f5d78d644908c20f345
Florian <mrflos@gmail.com>
2023-12-20 16:32:15 +0000

    Translated using Weblate (English)
    Currently translated at 100.0% (10 of 10 strings)
    
    Translation: YesWiki/YesWiki extension Helloword
    Translate-URL: https://hosted.weblate.org/projects/yeswiki/yeswiki-extension-helloword/en/

--- tools/helloworld/lang/helloworld_en.inc.php
+++ tools/helloworld/lang/helloworld_en.inc.php
@@ -1,13 +1,13 @@
 <?php
 return [
-    'HELLOWORLD_MY_MESSAGE' => '',
-    'HELLOWORD_NO_MSG_PARAM' => '',
-    'HELLOWORD_CALLBACK_MSG' => '',
-    'HELLOWORLD_CONFIG' => '',
-    'HELLOWORLD_URL_SHOW_XML' => '',
-    'HELLOWORLD_URL_SHOW_TAG' => '',
-    'HELLOWORLD_URL_EDIT_TAG' => '',
-    'HELLOWORLD_URL_EDIT_WITH_PARAMS' => '',
-    'HELLOWORLD_PAGE_NOT_ACCESSIBLE' => '',
-    'HELLOWORLD_FIELD_ALERT' => '',
+    'HELLOWORLD_MY_MESSAGE' => 'My message',
+    'HELLOWORD_NO_MSG_PARAM' => 'No \'message\' parameter defined in the {{greeting ...}} action',
+    'HELLOWORD_CALLBACK_MSG' => '(the action argument is changed in the before callback)',
+    'HELLOWORLD_CONFIG' => 'Config test',
+    'HELLOWORLD_URL_SHOW_XML' => 'See XML info of current page',
+    'HELLOWORLD_URL_SHOW_TAG' => 'Show %{tag}',
+    'HELLOWORLD_URL_EDIT_TAG' => 'Edit %{tag}',
+    'HELLOWORLD_URL_EDIT_WITH_PARAMS' => 'Edit current page with params',
+    'HELLOWORLD_PAGE_NOT_ACCESSIBLE' => 'Page not reachable',
+    'HELLOWORLD_FIELD_ALERT' => 'This will be saved in the entry',
 ];
