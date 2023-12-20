e21f0d28f251f3012bcac34ecc80fb156e71d01f
Florian <mrflos@gmail.com>
2023-12-20 16:29:42 +0000

    Translated using Weblate (English)
    Currently translated at 100.0% (2 of 2 strings)
    
    Translation: YesWiki/YesWiki extension Contact js
    Translate-URL: https://hosted.weblate.org/projects/yeswiki/yeswiki-extension-contact-js/en/

--- tools/contact/lang/contactjs_en.inc.php
+++ tools/contact/lang/contactjs_en.inc.php
@@ -1,6 +1,6 @@
 <?php
 return [
     // libs/contact.js
-    'CONTACT_REQUIRED_FIELD' => '',
-    'CONTACT_EMAIL_NOT_VALID' => '',
+    'CONTACT_REQUIRED_FIELD' => 'This field is mandatory.',
+    'CONTACT_EMAIL_NOT_VALID' => 'The given mail is not valid.',
 ];
