d601b2432199f7e07ca9263dacce9a4da8507f59
Jérémy Dufraisse <jeremy.dufraisse-info@orange.fr>
2024-02-15 12:30:44 +0100

    fix(recurrentEvent): update except on repetition change

--- tools/bazar/presentation/javascripts/recurrent-event.js
+++ tools/bazar/presentation/javascripts/recurrent-event.js
@@ -357,6 +357,8 @@ let appParams = {
         if (limitdate && 'value' in this.datePickerForLimit){
             this.datePickerForLimit.value = limitdate
             this.updateEndDateLimitTime()
+        } else {
+            this.updateAvailableExcept()
         }
         if (data?.isRecurrent !== '1'){
             this.recurrenceBaseId = (
@@ -412,6 +414,7 @@ let appParams = {
             if (repetition !== 'w' && this.days?.length > 1){
                 this.days = [this.days[0]]
             }
+            this.updateAvailableExceptUpdatingNbMax()
         },
         repetitionInternal(){
             this.setCurrentDayIfWeek()
