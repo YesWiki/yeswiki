6c28ff44b6907d3f6b9d2ba0aafcf1e5b93a3a1e
Sebastian Castro <sebastian.castro@protonmail.com>
2024-05-02 08:31:16 +0200

    move postInstall service as migration

--- tools/autoupdate/services/MigrationService.php
+++ tools/autoupdate/services/MigrationService.php
@@ -5,12 +5,13 @@ namespace YesWiki\AutoUpdate\Service;
 use Exception;
 use ReflectionClass;
 use ReflectionMethod;
+use YesWiki\Core\Service\ConfigurationService;
 use YesWiki\Core\Service\TripleStore;
 use YesWiki\Security\Controller\SecurityController;
 use YesWiki\Wiki;
 
 // This is a simple mecanism to perform migrations
-// Create a new private method in this class, it will be run after the wiki get updated
+// Create a new private method at the bottom of this class, it will be run after the wiki gets updated
 class MigrationService
 {
   public const TRIPLES_MIGRATION_ID = 'migration';
@@ -52,10 +53,34 @@ class MigrationService
     return $messages;
   }
 
-
   // All private methods below are considered as migrations
-  // Add new migration on the top
+  // Add new migration on the bottom
 
-  // private function newMigration() {}
   // private function oldMigration() {}
+  // private function newMigration() {}
+
+  private function cercopitequePostInstall()
+  {
+    $params = $this->wiki->services->getParameterBag();
+    $config = $this->wiki->services->get(ConfigurationService::class)->getConfiguration('wakka.config.php');
+    $config->load();
+
+    $releaseInConfig = $params->get('yeswiki_release');
+    if ($releaseInConfig == _t('AU_UNKNOW') || !preg_match("/^\d{1,4}[.-].*/", $releaseInConfig)) {
+      $config['yeswiki_release'] = YESWIKI_RELEASE;
+      $config->write();
+    }
+
+    // check favorite_theme
+    // If default theme was used, install new yeswikicerco extension to keep same look and feel
+    $favoriteThemefromFile = $config['favorite_theme'] ?? '';
+    if (empty($favoriteThemefromFile) || $favoriteThemefromFile == 'yeswiki') {
+      $this->wiki->services->get(AutoUpdateService::class)->upgrade('yeswikicerco');
+
+      $config['favorite_theme'] = 'yeswikicerco';
+      $config['favorite_style'] = $config['favorite_style'] ?? 'gray.css';
+      $config['favorite_squelette'] = $config['favorite_squelette'] ?? 'responsive-1col.tpl.html';
+      $config->write();
+    }
+  }
 }
