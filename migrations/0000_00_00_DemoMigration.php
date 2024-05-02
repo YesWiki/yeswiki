d5aa44f51cb482b6faec0bb92d8b10357c0b61b3
Sebastian Castro <sebastian.castro@protonmail.com>
2024-05-02 08:31:17 +0200

    move migrations into dedicate files (new mecanism)

--- migrations/0000_00_00_DemoMigration.php
+++ migrations/0000_00_00_DemoMigration.php
@@ -3,8 +3,8 @@ use YesWiki\Core\YesWikiMigration;
 
 class DemoMigration extends YesWikiMigration
 {
-  public function run()
-  {
-    // This is a demo migration which does nothing
-  }
+    public function run()
+    {
+
+    }
 }
---
+++ migrations/2024_04_25_UpdatePasswordSize.php
@@ -0,0 +1,15 @@
+<?php
+use YesWiki\Core\Service\PasswordHasherFactory;
+use YesWiki\Core\YesWikiMigration;
+
+class UpdatePasswordSize extends YesWikiMigration
+{
+    public function run()
+    {
+        // update user table to increase size of password
+        $passwordHasherFactory = $this->wiki->services->get(PasswordHasherFactory::class);
+        if (!$passwordHasherFactory->newModeIsActivated()) {
+            $passwordHasherFactory->activateNewMode();
+        }
+    }
+}
