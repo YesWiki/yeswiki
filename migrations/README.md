5f7dc5fe2542418062bb284f27078480dc9634be
Sebastian Castro <sebastian.castro@protonmail.com>
2024-05-02 08:31:17 +0200

    adds command to create a migration

--- migrations/README.md
+++ migrations/README.md
@@ -6,23 +6,4 @@ A migration is only run once
 
 ## Create a new migration
 
-Create a file in the `migrations` folder (can be created also inside a tool, i.e. `tools/bazar/migrations`)
-
-Choose a name for the migration (for example `TestMigration`) and prefix the file name with current date, i.e. `2024_04_25_TestMigration.php`. The date is there to ensure uniq migration name
-
-```
-// migrations/2024-04_25_TestMigration.php
-
-<?php
-use YesWiki\Core\YesWikiMigration;
-
-class TestMigration extends YesWikiMigration
-{
-    public function run()
-    {
-        // your code goes here
-        // $this->dbService is available
-        // $this->wiki is also available, so you can get other services if needed
-    }
-}
-```
+`php includes/commands/console generate:migration YourMigrationName`
---
+++ tools/autoupdate/commands/GenerateMigrationCommand.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace YesWiki\AutoUpdate\Commands;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use YesWiki\Wiki;
+
+class GenerateMigrationCommand extends Command
+{
+  protected $wiki;
+
+  public function __construct(Wiki &$wiki)
+  {
+    parent::__construct();
+    $this->wiki = $wiki;
+  }
+
+  protected function configure()
+  {
+    $this
+      ->setName('generate:migration')
+      ->setDescription('Create a new migration file')
+      ->addArgument('className', InputArgument::REQUIRED, 'The name of the migration class (CamelCase)');
+  }
+
+  protected function execute(InputInterface $input, OutputInterface $output)
+  {
+    $className = $input->getArgument('className');
+    $timestamp = date('YmdHis');
+    $migrationFileName = $timestamp . '_' . $className . '.php';
+    $migrationTemplate = "<?php\n\nuse YesWiki\\Core\\YesWikiMigration;\n\nclass $className extends YesWikiMigration\n{\n    public function run()\n    {\n\n    }\n}";
+
+    $filePath = 'migrations/' . $migrationFileName;
+    if (!file_exists($filePath)) {
+      file_put_contents($filePath, $migrationTemplate);
+      $output->writeln("Migration file created successfully: $filePath");
+      return Command::SUCCESS;
+    } else {
+      $output->writeln("Error: Migration file already exists: $filePath");
+      return Command::FAILURE;
+    }
+  }
+}
