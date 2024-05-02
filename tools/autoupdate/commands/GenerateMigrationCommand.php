e2aa15f4173e4dbf957f6e157b5e3f89ddf3f469
Sebastian Castro <sebastian.castro@protonmail.com>
2024-05-02 08:31:17 +0200

    generate-migration: add tool option

--- tools/autoupdate/commands/GenerateMigrationCommand.php
+++ tools/autoupdate/commands/GenerateMigrationCommand.php
@@ -5,6 +5,7 @@ namespace YesWiki\AutoUpdate\Commands;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use YesWiki\Wiki;
 
@@ -23,7 +24,8 @@ class GenerateMigrationCommand extends Command
     $this
       ->setName('generate:migration')
       ->setDescription('Create a new migration file')
-      ->addArgument('className', InputArgument::REQUIRED, 'The name of the migration class (CamelCase)');
+      ->addArgument('className', InputArgument::REQUIRED, 'The name of the migration class (CamelCase)')
+      ->addOption('tool', 't', InputOption::VALUE_REQUIRED, 'The name of the tool (otherwise migration created in root folder)');
   }
 
   protected function execute(InputInterface $input, OutputInterface $output)
@@ -33,7 +35,13 @@ class GenerateMigrationCommand extends Command
     $migrationFileName = $timestamp . '_' . $className . '.php';
     $migrationTemplate = "<?php\n\nuse YesWiki\\Core\\YesWikiMigration;\n\nclass $className extends YesWikiMigration\n{\n    public function run()\n    {\n\n    }\n}";
 
-    $filePath = 'includes/migrations/' . $migrationFileName;
+    $tool = $input->getOption('tool');
+    $folderPath = (!empty($tool) ? "tools/$tool/migrations/" : 'includes/migrations/');
+    if (!file_exists($folderPath)) {
+      mkdir($folderPath);
+    }
+    $filePath = $folderPath . $migrationFileName;
+
     if (!file_exists($filePath)) {
       file_put_contents($filePath, $migrationTemplate);
       $output->writeln("Migration file created successfully: $filePath");
