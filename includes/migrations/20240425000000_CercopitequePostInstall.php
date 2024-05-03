9f3035325b01c6ac0775e4e309c7b3b32268ecc8
Jérémy Dufraisse <jeremy.dufraisse-info@orange.fr>
2024-05-03 10:38:11 +0200

    feat(YesWikiMigration): add method getService

--- includes/migrations/20240425000000_CercopitequePostInstall.php
+++ includes/migrations/20240425000000_CercopitequePostInstall.php
@@ -1,4 +1,5 @@
 <?php
+
 use YesWiki\AutoUpdate\Service\AutoUpdateService;
 use YesWiki\Core\Service\ConfigurationService;
 use YesWiki\Core\YesWikiMigration;
@@ -9,14 +10,14 @@ class CercopitequePostInstall extends YesWikiMigration
     {
         if (isset($_GET['previous_version']) && $_GET['previous_version'] == 'cercopitheque') {
 
-            $config = $this->wiki->services->get(ConfigurationService::class)->getConfiguration('wakka.config.php');
+            $config = $this->getService(ConfigurationService::class)->getConfiguration('wakka.config.php');
             $config->load();
 
             // check favorite_theme
             // If default theme was used, install new yeswikicerco extension to keep same look and feel
             $favoriteThemefromFile = $config['favorite_theme'] ?? '';
             if (empty($favoriteThemefromFile) || $favoriteThemefromFile == 'yeswiki') {
-                $this->wiki->services->get(AutoUpdateService::class)->upgrade('yeswikicerco');
+                $this->getService(AutoUpdateService::class)->upgrade('yeswikicerco');
 
                 $config['favorite_theme'] = 'yeswikicerco';
                 $config['favorite_style'] = $config['favorite_style'] ?? 'gray.css';
