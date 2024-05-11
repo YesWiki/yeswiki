02ede780cbe351d2aeaf0308d35bfb78eb6cd336
ppom <yeswiki@ppom.me>
2024-05-11 12:00:00 +0200

    fix(a11y): replace all <div> buttons by real <button>
    warn: I couldn't find on my brower all places I modified, so I may have
    broken a few styles 😬

--- tools/bazar/presentation/javascripts/components/Panel.js
+++ tools/bazar/presentation/javascripts/components/Panel.js
@@ -35,11 +35,11 @@ export default {
   },
   template: `
     <div class="panel" :class="[panelClass, {collapsed: internalCollapsed}]">
-      <div class="panel-heading" :class="{collapsed: internalCollapsed}"
+      <button class="panel-heading" :class="{collapsed: internalCollapsed}"
            :data-toggle="collapsable ? 'collapse' : ''"
            @click="headerClicked()">
         <slot name="header"></slot>
-      </div>
+      </button>
       <div class="panel-body" v-show="!internalCollapsed" style="padding: 0">
         <slot name="body"></slot>
       </div>
