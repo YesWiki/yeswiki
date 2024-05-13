7bc465c026ed0465bf8cb7a8df5cf714ef142c0b
Florian Schmitt <mrflos@gmail.com>
2024-05-13 16:22:46 +0300

    feat(duplicate): works for entries and rename manager

--- includes/services/DuplicationManager.php
+++ includes/services/DuplicationManager.php
@@ -11,14 +11,14 @@ use YesWiki\Bazar\Service\ListManager;
 use YesWiki\Core\Service\PageManager;
 use YesWiki\Wiki;
 
-class ImportFilesManager
+class DuplicationManager
 {
     protected $uploadPath;
     protected $wiki;
 
 
     /**
-     * ImportManager constructor
+     * DuplicationManager constructor
      * @param Wiki $wiki the injected Wiki instance
      */
     public function __construct(Wiki $wiki)
@@ -47,44 +47,6 @@ class ImportFilesManager
         }
 
         return $this->uploadPath;
-    }
-
-    /**
-     * Download file url to local wiki using cURL
-     *
-     * @param string $from file url
-     * @param string $to local path
-     * @param boolean $overwrite overwrite existing file ? (default:false)
-     * @return void
-     */
-    private function cURLDownload($from, $to, $overwrite = false)
-    {
-        $output = '';
-        if (file_exists($to)) {
-            if ($overwrite) {
-                $output .= _t('FILE') . ' ' . $to . ' ' . _t('FILE_OVERWRITE') . '.';
-            } else {
-                $output .= _t('FILE') . ' ' . $to . ' ' . _t('FILE_NO_OVERWRITE') . '.';
-                return $output;
-            }
-        }
-
-        // Do cURL transfer
-        $fp = fopen($to, 'wb');
-        $ch = curl_init($from);
-        curl_setopt($ch, CURLOPT_FILE, $fp);
-        curl_setopt($ch, CURLOPT_HEADER, 0);
-        curl_setopt($ch, CURLOPT_FAILONERROR, true);
-        curl_exec($ch);
-        $err = curl_error($ch);
-        curl_close($ch);
-        fclose($fp);
-
-        if ($err) {
-            unlink($to);
-            throw new \Exception($output . _t('ERROR_DOWNLOADING') . ' ' . $from . ': ' . $err . "\n" . _t('REMOVING_CORRUPTED_FILE') . ' ' . $to);
-        }
-        return $output;
     }
 
     /**
@@ -162,7 +124,7 @@ class ImportFilesManager
             }
         }
         $fileUrlRegex = '#' . preg_quote(str_replace('?', '', $this->wiki->config['base_url']), '#') .
-            '(files/.*\.[a-zA-Z0-9]{1,16}\b([-a-zA-Z0-9!@:%_\+.~\#?&\/\/=]*))#Ui';
+            '(' . $this->uploadPath . '/.*\.[a-zA-Z0-9]{1,16}\b([-a-zA-Z0-9!@:%_\+.~\#?&\/\/=]*))#Ui';
         preg_match_all(
             $fileUrlRegex,
             $wikiText,
@@ -208,7 +170,7 @@ class ImportFilesManager
         } elseif (!$this->wiki->services->get(ListManager::class)->isList($tag)) { // page
             $wikiText = $this->wiki->services->get(PageManager::class)->getOne($tag)['body'];
             if ($fi = $this->findFilesInWikiText($tag, $wikiText)) {
-                $files[] = $fi;
+                $files = array_merge($files, $fi);
             }
         }
         return $files;
@@ -217,18 +179,24 @@ class ImportFilesManager
     public function duplicateFiles($fromTag, $toTag)
     {
         $files = $this->findFiles($fromTag);
+        $doneFiles = [];
         foreach ($files as $f) {
             $newPath = preg_replace(
-                '~files/' . preg_quote($fromTag, '~') . '_~Ui',
-                'files/' . $toTag . '_',
+                '~' . $this->uploadPath . '/' . preg_quote($fromTag, '~') . '_~Ui',
+                $this->uploadPath . '/' . $toTag . '_',
                 $f['path']
             );
             // if the file name has not changed, we add newPageTag_ as filename prefix
             if ($f['path'] == $newPath) {
-                $newPath = str_replace('files/', 'files/' . $toTag . '_', $newPath);
+                $newPath = str_replace($this->uploadPath . '/', $this->uploadPath . '/' . $toTag . '_', $newPath);
             }
             copy($f['path'], $newPath);
+            $doneFiles[] = [
+                'originalFile' => str_replace($this->uploadPath . '/', '', $f['path']),
+                'duplicatedFile' => str_replace($this->uploadPath . '/', '', $newPath),
+            ];
         }
+        return $doneFiles;
     }
 
     public function checkPostData($data)
@@ -242,6 +210,9 @@ class ImportFilesManager
         if ($data['type'] != 'page' && empty($data['pageTitle'])) {
             throw new \Exception(_t('EMPTY_PAGE_TITLE'));
         }
+        if (!$this->wiki->services->get(AclService::class)->hasAccess('write', $data['pageTag'])) {
+            throw new \Exception(_t('LOGIN_NOT_AUTORIZED_EDIT') . ' ' . $data['pageTag']);
+        }
         $page = $this->wiki->services->get(PageManager::class)->getOne($data['pageTag']);
         if ($page) {
             throw new \Exception($data['pageTag'] . ' ' . _t('ALREADY_EXISTING'));
@@ -252,6 +223,43 @@ class ImportFilesManager
         return $data;
     }
 
+    public function duplicateLocally($data)
+    {
+        switch ($data['type']) {
+            case 'list':
+                $list = $this->wiki->services->get(ListManager::class)->getOne($this->wiki->getPageTag());
+                $this->wiki->services->get(ListManager::class)->create($data['pageTitle'], $list['label'], $data['pageTag']);
+                break;
+
+            case 'entry':
+                $files = $this->duplicateFiles($this->wiki->getPageTag(), $data['pageTag']);
+                $entry = $this->wiki->services->get(EntryManager::class)->getOne($this->wiki->getPageTag());
+                dump($entry, $files);
+                $fields = $this->getUploadFieldsFromEntry($this->wiki->GetPageTag());
+                foreach ($fields as $f) {
+                }
+
+                exit;
+
+                $entry['id_fiche'] = $data['pageTag'];
+                $entry['bf_titre'] = $data['pageTitle'];
+                $entry['antispam'] = 1;
+                $this->wiki->services->get(EntryManager::class)->create($entry['id_typeannonce'], $entry);
+                break;
+
+            default:
+            case 'page':
+                $newBody = $this->wiki->page['body'];
+                $files = $this->duplicateFiles($this->wiki->getPageTag(), $data['pageTag']);
+                foreach ($files as $f) {
+                    $newBody = str_replace($f['originalFile'], $f['duplicatedFile'], $newBody);
+                }
+                $this->wiki->services->get(PageManager::class)->save($data['pageTag'], $newBody);
+                break;
+        }
+        // TODO: duplicate acls and metadatas
+    }
+
     public function humanFilesize($bytes, $decimals = 2)
     {
         $factor = floor((strlen($bytes) - 1) / 3);
@@ -259,79 +267,5 @@ class ImportFilesManager
             $sz = 'KMGT';
         }
         return sprintf("%.{$decimals}f", $bytes / pow(1024, $factor)) . @$sz[$factor - 1] . 'B';
-    }
-
-    /**
-     * Generate distant file url and download to local file path
-     *
-     * @param string $remoteUrl distant file url
-     * @param string $filename file name
-     * @param boolean $overwrite overwrite existing file ? (default:false)
-     * @return void
-     */
-    public function downloadDirectLinkAttachment($remoteUrl, $filename, $overwrite = false)
-    {
-        $remoteFileUrl = $remoteUrl . '/files/' . $filename;
-        $saveFileLoc = $this->getLocalFileUploadPath() . '/' . $filename;
-
-        return $this->cURLDownload($remoteFileUrl, $saveFileLoc, $overwrite);
-    }
-
-    /**
-     * Generate local path and download hidden attachments
-     * It downloads attachments linked with /download links
-     *
-     * @param string $remoteUrl distant url
-     * @param string $pageTag page tag
-     * @param string $lastPageUpdate last update time
-     * @param string $filename file name
-     * @param boolean $overwrite overwrite existing file ? (default:false)
-     * @return array all file attachments
-     */
-    public function downloadHiddenAttachment($remoteUrl, $pageTag, $lastPageUpdate, $filename, $overwrite = false)
-    {
-        if (!class_exists('attach')) {
-            require_once("tools/attach/libs/attach.lib.php");
-        }
-
-        $this->wiki->tag = $pageTag;
-        $this->wiki->page = array('tag' => $pageTag, 'time' => $lastPageUpdate);
-
-        $remoteFileUrl = $remoteUrl . '?' . $pageTag . '/download&file=' . $filename;
-        $att = new \attach($this->wiki);
-        $att->file = $filename;
-        $newFilename = $att->GetFullFilename(true);
-
-        $this->cURLDownload($remoteFileUrl, $newFilename, $overwrite);
-    }
-
-
-    /**
-     * All type of attachment related to a page or a bazar entry
-     *
-     * @param string $remoteUrl distant url
-     * @param array $wikiPage page or entry content as an array
-     * @param boolean $overwrite overwrite existing file ? (default:false)
-     * @return void
-     */
-    public function downloadAttachments($remoteUrl, &$wikiPage, $overwrite = false)
-    {
-        // Handle Pictures and file attachments
-        $attachments = $this->findDirectLinkAttachements($remoteUrl, $wikiPage, true);
-
-        if (count($attachments)) {
-            foreach ($attachments as $image) {
-                $this->downloadDirectLinkAttachment($remoteUrl, $image, $overwrite);
-            }
-        }
-
-        // Downloading hidden attachments
-        $attachments = $this->findHiddenAttachments($remoteUrl, $wikiPage, true);
-
-        if (!empty($attachments)) {
-            foreach ($attachments as $attachment) {
-                $this->downloadHiddenAttachment($remoteUrl, $wikiPage['id_fiche'], date("Y-m-d H:i:s"), $attachment, $overwrite);
-            }
-        }
     }
 }
