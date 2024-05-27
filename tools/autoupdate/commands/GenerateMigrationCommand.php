3389ad2a6e441d54438b692bccd2b87e5569ab1e
mrflos <mrflos@gmail.com>
2024-05-27 23:32:51 +0300

    feat(migration): prefix migration for extensions with extension name

--- tools/autoupdate/commands/GenerateMigrationCommand.php
+++ tools/autoupdate/commands/GenerateMigrationCommand.php
@@ -22,20 +22,20 @@ class GenerateMigrationCommand extends Command
     protected function configure()
     {
         $this
-          ->setName('generate:migration')
-          ->setDescription('Create a new migration file')
-          ->addArgument('className', InputArgument::REQUIRED, 'The name of the migration class (CamelCase)')
-          ->addOption('tool', 't', InputOption::VALUE_REQUIRED, 'The name of the tool (otherwise migration created in root folder)');
+            ->setName('generate:migration')
+            ->setDescription('Create a new migration file')
+            ->addArgument('className', InputArgument::REQUIRED, 'The name of the migration class (CamelCase)')
+            ->addOption('tool', 't', InputOption::VALUE_REQUIRED, 'The name of the tool (otherwise migration created in root folder)');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $className = $input->getArgument('className');
+        $tool = $input->getOption('tool');
+        $className = (!empty($tool) ? ucwords(strtolower($tool)) : '') . $input->getArgument('className');
         $timestamp = date('YmdHis');
         $migrationFileName = $timestamp . '_' . $className . '.php';
         $migrationTemplate = "<?php\n\nuse YesWiki\\Core\\YesWikiMigration;\n\nclass $className extends YesWikiMigration\n{\n    public function run()\n    {\n\n    }\n}";
 
-        $tool = $input->getOption('tool');
         $folderPath = (!empty($tool) ? "tools/$tool/migrations/" : 'includes/migrations/');
         if (!file_exists($folderPath)) {
             mkdir($folderPath);
