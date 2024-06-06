22b6a803b4dc95926dd1fd37b56096428c7ed4dc
தமிழ்நேரம் <anishprabu.t@gmail.com>
2024-06-06 06:14:21 +0200

    Added translation using Weblate (Tamil)

---
+++ tools/syndication/lang/syndication_ta.inc.php
@@ -0,0 +1,21 @@
+<?php
+return [
+    // actions/syndication.php
+    'SYNDICATION_ACTION_SYNDICATION' => '',
+    'SYNDICATION_PARAM_URL_REQUIRED' => '',
+    'SYNDICATION_WRITE_ACCESS_TO_CACHE_FOLDER' => '',
+    'SYNDICATION_CREATE_CACHE_FOLDER' => '',
+    'SYNDICATION_TEMPLATE_NOT_FOUND' => '',
+    'SYNDICATION_READ_MORE' => '',
+    // actions/twitter.php
+    'SYNDICATION_ACTION_TWITTER' => '',
+    'SYNDICATION_PARAM_USER_REQUIRED' => '',
+    // actions/twittersearch.php
+    'SYNDICATION_ACTION_TWITTERSEARCH' => '',
+    'SYNDICATION_PARAM_QUERY_REQUIRED' => '',
+    'SYNDICATION_TEMPLATE_DOESNT_EXISTS' => '',
+    'SYNDICATION_USE_OF_DEFAULT_TEMPLATE' => '',
+    // templates/accordeon.tpl.html
+    'SYNDICATION_SOURCE' => '',
+    'SYNDICATION_SEE_ALL_CONTENT' => '',
+];
