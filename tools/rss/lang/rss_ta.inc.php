45249300e80f94c99223443b20cb07cf26665020
தமிழ்நேரம் <anishprabu.t@gmail.com>
2024-06-06 07:14:31 +0200

    Added translation using Weblate (Tamil)

---
+++ tools/rss/lang/rss_ta.inc.php
@@ -0,0 +1,14 @@
+<?php
+return [
+    // actions/recentchangesrss.php
+    'RSS_CHANGE_OF' => '',
+    'RSS_HISTORY' => '',
+    'RSS_HIDDEN_CONTENT' => '',
+    'RSS_ON_DATE' => '',
+    // libs/rssdiff.function.php
+    'RSS_COMPARISON_OF' => '',
+    'RSS_TO' => '',
+    'RSS_ADDS' => '',
+    'RSS_DELETIONS' => '',
+    'RSS_NO_DIFF' => '',
+];
