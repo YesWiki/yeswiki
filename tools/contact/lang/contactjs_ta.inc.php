384d108a4928fa4d5a4a336fe9e84b3e4c0e0ad0
தமிழ்நேரம் <anishprabu.t@gmail.com>
2024-06-07 01:48:13 +0000

    Translated using Weblate (Tamil)
    Currently translated at 100.0% (2 of 2 strings)
    
    Translation: YesWiki/YesWiki extension Contact js
    Translate-URL: https://hosted.weblate.org/projects/yeswiki/yeswiki-extension-contact-js/ta/

--- tools/contact/lang/contactjs_ta.inc.php
+++ tools/contact/lang/contactjs_ta.inc.php
@@ -1,6 +1,6 @@
 <?php
 return [
     // libs/contact.js
-    'CONTACT_REQUIRED_FIELD' => '',
-    'CONTACT_EMAIL_NOT_VALID' => '',
+    'CONTACT_REQUIRED_FIELD' => 'இந்தப் புலத்தின் நுழைவு கட்டாயமாகும்.',
+    'CONTACT_EMAIL_NOT_VALID' => 'உள்ளிட்ட மின்னஞ்சல் செல்லுபடியாகாது.',
 ];
