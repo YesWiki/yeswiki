c1d8f9a523b678eef9f20f351808ebf3f289809e
Sebastian Castro <sebastian.castro@protonmail.com>
2024-06-21 05:50:39 +0000

    Adds both parent and parents to each node

--- tools/bazar/presentation/javascripts/utils.js
+++ tools/bazar/presentation/javascripts/utils.js
@@ -2,27 +2,14 @@ export function flattenTree(tree) {
   const flatList = []
 
   // Recursive function to traverse the tree
-  function traverse(node, parentChain) {
-    // Create a new object for the current node with a parentValues field
-    const nodeWithParentOptions = {
-      ...node,
-      parentValues: [...parentChain] // Clone the parentChain array
-    }
-
-    // Add the current node to the flat list
-    flatList.push(nodeWithParentOptions)
-
-    // If the node has children, traverse them
-    if (node.children && node.children.length > 0) {
-      node.children.forEach((child) => {
-        // Traverse each child, passing the updated parent chain
-        traverse(child, [...parentChain, node.id])
-      })
-    }
+  function traverse(node) {
+    flatList.push(node)
+    node.children.forEach((childNode) => {
+      traverse(childNode)
+    })
   }
 
-  // Start the traversal for each root node in the tree
-  tree.forEach((root) => traverse(root, []))
+  tree.forEach((rootNode) => traverse(rootNode))
 
   return flatList
 }
