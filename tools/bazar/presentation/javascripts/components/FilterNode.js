7c4c3be581937a8ddede4c997e8ad950c7a5567c
Sebastian Castro <sebastian.castro@protonmail.com>
2024-07-02 18:40:08 +0000

    simplify tree logic by pre calculating all parents and descendants

--- tools/bazar/presentation/javascripts/components/FilterNode.js
+++ tools/bazar/presentation/javascripts/components/FilterNode.js
@@ -1,32 +1,41 @@
 export default {
   props: ['node'],
-  data() {
-    return { mounted: false }
-  },
   computed: {
     // A parent node should be display if some of it's children has nb > 0
     displayNode() {
-      this.mounted // force recalculate this cmputed prop when mounted
-      if (this.node.nb > 0) {
-        return true
-      }
-      if (this.$refs.children) {
-        return this.$refs.children.some((childComponent) => childComponent.displayNode)
-      }
-      return false
+      return [this.node, ...this.node.descendants].some((node) => node.nb > 0)
+    },
+    someDescendantChecked() {
+      return this.node.descendants.some((node) => node.checked)
     }
   },
-  mounted() {
-    this.mounted = true
+  methods: {
+    onChecked() {
+      // uncheck all parents & descendants
+      // (this logic is not easy to understand, play with the UI to see how it works)
+
+      // Why unchecking parents?
+      // Given this tree France: [ Paris, Bordeaux], Spain: [ Madrid, Barcelone ]
+      // Given those entries entryCapitaleFR: Paris|France, entryLyon: France
+      // If France is checked, I want both entry to be display
+      // If Paris is checked, I want only entryCapitaleFR to be displayed.
+      // So when clicking Paris, I need to uncheck France (otherwise both will be displayed)
+
+      // Why unchecking descendants?
+      // If Paris is checked, and I check France. All entries from France will be displayed
+      // then no need to have Paris checked, it's misleading
+      this.node.descendants.forEach((node) => { node.checked = false })
+      this.node.parents.forEach((node) => { node.checked = false })
+    }
   },
   template: `
     <div class="node-container" v-show="displayNode">
-      <div class="checkbox">
+      <div :class="['checkbox', {checked: node.checked, 'some-descendant-checked': someDescendantChecked}]">
         <label>
           <input class="filter-checkbox" type="checkbox"
-                v-model="node.checked">
+                 v-model="node.checked" @change="onChecked">
           <span>
-            <span v-html="node.label"></span>
+            <span class="node-label" v-html="node.label"></span>
             <span class="nb" v-if="node.nb">({{ node.nb }})</span>
           </span>
         </label>
