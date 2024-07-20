6ae58550614ece0f82425acd056b2895f367af17
Florian <mrflos@gmail.com>
2024-07-20 05:43:48 +0000

    Translated using Weblate (Spanish)
    Currently translated at 30.3% (84 of 277 strings)
    
    Translation: YesWiki/YesWiki extension Template
    Translate-URL: https://hosted.weblate.org/projects/yeswiki/yeswiki-extension-template/es/

--- tools/templates/lang/templates_es.inc.php
+++ tools/templates/lang/templates_es.inc.php
@@ -1,9 +1,7 @@
 <?php
-
 return [
     'TEMPLATE_ACTION' => 'Acción',
     // 'TEMPLATE_FILE_NOT_FOUND' => 'Template non trouvé',
-
     // actions/button.php
     'TEMPLATE_ACTION_BUTTON' => 'Acción {{button ...}}',
     'TEMPLATE_LINK_PARAMETER_REQUIRED' => 'parámetro "link" obligatorio',
@@ -23,7 +21,6 @@ return [
     'TEMPLATE_BG_IMAGE' => 'Imagen de fondo',
     'TEMPLATE_ERROR_NO_DATA' => 'ERROR : no hay nada que añadir en los metadatos.',
     'TEMPLATE_ERROR_NO_ACCESS' => 'ERROR : no tienes los derechos de acceso.',
-
     // barre de redaction
     'TEMPLATE_VIEW_PAGE' => 'Ver la página',
     'TEMPLATE_EDIT' => 'Editar',
@@ -47,24 +44,19 @@ return [
     // 'TEMPLATE_CLASSIC_SLIDESHOW' => 'Diaporama classique',
     'TEMPLATE_SEE_SHARING_OPTIONS' => 'Compartir la página',
     'TEMPLATE_SHARE' => 'Compartir',
-
     // action/diaporama
     // 'DIAPORAMA_PAGE_PARAM_MISSING' => 'Action diaporama : param&ecirc;tre "page" obligatoire.',
     // 'DIAPORAMA_TEMPLATE_PARAM_ERROR' => 'Action diaporama : le param&ecirc;tre "template" pointe sur un fichier inexistant ou illisible. Le template par d&eacute;faut sera utilis&eacute;.',
-
     // formatage des dates
     'TEMPLATE_DATE_FORMAT' => 'd M Y',
-
     // recherche
     'TEMPLATE_SEARCH_INPUT_TITLE' => 'Buscar en YesWiki [alt-shift-C]',
     'TEMPLATE_SEARCH_BUTTON_TITLE' => 'Buscar las páginas con este texto.',
     'TEMPLATE_SEARCH_PLACEHOLDER' => 'Buscar...',
     'TEMPLATE_SEARCH' => 'Buscar',
-
     // handler widget
     'TEMPLATE_WIDGET_TITLE' => 'Widget : integrar el contenido de esta página en otra parte',
     'TEMPLATE_WIDGET_COPY_PASTE' => 'Copiar-pegar el codigo HTML más arriba para integrar le contenido tal como aparece más abajo.',
-
     // handler share
     'TEMPLATE_SHARE_INCLUDE_CODE' => 'Codigo de integración de contenido en una página HTML',
     'TEMPLATE_SHARE_MUST_READ' => 'Para leer : ',
@@ -76,12 +68,10 @@ return [
     'TEMPLATE_SHARE_MAIL' => 'Enviar el contenido de esta página por mail',
     'TEMPLATE_ADD_SHARE_BUTTON' => 'Añadir en la parte superior derecha un botón para compartir la página',
     'TEMPLATE_ADD_EDIT_BAR' => 'Añadir la barra de edición abajo de la página',
-
     // handler diaporama
     'TEMPLATE_NO_ACCESS_TO_PAGE' => 'Nos tienes los derechos de acceso a esta página.',
     'TEMPLATE_PAGE_DOESNT_EXIST' => 'No existe esta página',
     'PAGE_CANNOT_BE_SLIDESHOW' => 'La página no se puede cortar en diapositives (no hay títulos de nivel 2)',
-
     // handler edit
     'TEMPLATE_CUSTOM_PAGE' => 'Preferencias de la página',
     'TEMPLATE_PAGE_PREFERENCES' => 'Parámetros de la página',
@@ -94,43 +84,33 @@ return [
     'FOR_2_OR_3_COLUMN_THEMES' => 'Para los temas con 2 o 3 columnas',
     'VERTICAL_MENU_PAGE' => 'el menú vertical',
     'RIGHT_COLUMN_PAGE' => 'la columna de derecha',
-
     // actions/yeswikiversion.php
     'RUNNING_WITH' => 'Disfruta con',
-
     // 'TEMPLATE_ACTION_SECTION' => 'Action {{section ...}}',
     // 'TEMPLATE_ELEM_SECTION_NOT_CLOSED' => 'l\'action {{section ...}} doit &ecirc;tre ferm&eacute;e par une action {{end elem="section"}}',
-
     'TEMPLATE_NO_THEME_FILES' => 'Archivo de los temas que faltan',
     'TEMPLATE_DEFAULT_THEME_USED' => 'El tema por defecto est donc utilisé',
-
     // actions/end.php
     'TEMPLATE_ACTION_END' => 'Acción {{end ...}}',
     'TEMPLATE_ELEM_PARAMETER_REQUIRED' => 'parámetro "elem" obligatorio',
-
     // actions/col.php
     'TEMPLATE_ACTION_COL' => 'Acción {{col ...}}',
     'TEMPLATE_SIZE_PARAMETER_REQUIRED' => 'parámetro "size" obligatorio',
     'TEMPLATE_SIZE_PARAMETER_MUST_BE_INTEGER_FROM_1_TO_12' => 'el parámetro "size" tiene que ser un número entero entre 1 y 12',
     'TEMPLATE_ELEM_COL_NOT_CLOSED' => 'la acción {{col ...}} tiene que ser cerrada por una acción {{end elem="col"}}',
-
     // actions/grid.php
     'TEMPLATE_ACTION_GRID' => 'Acción {{grid ...}}',
     'TEMPLATE_ELEM_GRID_NOT_CLOSED' => 'la acción {{grid ...}} tiene que ser cerrada por una acción {{end elem="grid"}}',
-
     // action/panel.php
     // 'TEMPLATE_ACTION_PANEL' => 'Action {{panel ...}}',
     // 'TEMPLATE_ELEM_PANEL_NOT_CLOSED' => 'l\'action {{panel ...}} doit &ecirc;tre ferm&eacute;e par une action {{end elem="panel"}}',
     // 'TEMPLATE_TITLE_PARAMETER_REQUIRED' => "Param&egrave;tre title obligatoire",
-
     // action/accordion.php
     // 'TEMPLATE_ACTION_ACCORDION' => 'Action {{accordion ...}}',
     // 'TEMPLATE_ELEM_ACORDION_NOT_CLOSED' => 'l\'action {{accordion ...}} doit &ecirc;tre ferm&eacute;e par une action {{end elem="accordion"}}',
-
     // actions/buttondropdown.php
     'TEMPLATE_ACTION_BUTTONDROPDOWN' => 'Acción {{buttondropdown ...}}',
     'TEMPLATE_ELEM_BUTTONDROPDOWN_NOT_CLOSED' => 'la acción {{buttondropdown ...}} tiene que ser cerrada por una acción {{end elem="buttondropdown"}}',
-
     // actions/adminpages.php
     'TEMPLATE_ACTION_FOR_ADMINS_ONLY' => 'acción reservada a los administradores',
     'TEMPLATE_CONFIRM_DELETE_PAGE' => 'Estas seguro de querer suprimir definitivamente esta página ?',
@@ -139,205 +119,6 @@ return [
     'TEMPLATE_OWNER' => 'Proprietario',
     'TEMPLATE_ACTIONS' => 'Acciones',
     'TEMPLATE_MODIFY' => 'Modificar',
-
     // actions/setwikidefaultheme.php
     'TEMPLATE_FORCE_TEMPLATE' => 'Fuerza para todo el wiki.',
-
-    // themeselector.tpl.html
-    // 'TEMPLATE_PRIMARY_COLOR' => 'Couleur primaire',
-    // 'TEMPLATE_SECONDARY_COLOR_1' => 'Couleur secondaire 1',
-    // 'TEMPLATE_SECONDARY_COLOR_2' => 'Couleur secondaire 2',
-    // 'TEMPLATE_NEUTRAL_COLOR' => 'Couleur de texte',
-    // 'TEMPLATE_SOFT_COLOR' => 'Couleur neutre',
-    // 'TEMPLATE_LIGHT_COLOR' => 'Couleur claire',
-    // 'TEMPLATE_MAIN_TEXT_SIZE' => 'Taille de texte',
-    // 'TEMPLATE_MAIN_TEXT_FONT' => 'Police des textes',
-    // 'TEMPLATE_MAIN_TITLE_FONT' => 'Police des titres',
-    // 'TEMPLATE_CHOOSE_FONT' => 'Choisir une police',
-    // 'TEMPLATE_SEARCH_POINTS' => 'Rechercher...',
-    // 'TEMPLATE_DELETE_CSS_PRESET' => 'Voulez-vous supprimer le preset personnalisé',
-    // 'TEMPLATE_ADD_CSS_PRESET_API_HINT' => 'Sauvegarde un fichier preset personnalisé',
-    // 'TEMPLATE_DELETE_CSS_PRESET_API_HINT' => 'Supprime un fichier preset personnalisé',
-    // 'TEMPLATE_PRESET_FILENAME' => 'Nom du preset',
-    // 'TEMPLATE_THEME_NOT_SAVE' => 'Thème non sauvegardé',
-    // 'TEMPLATE_FILE_NOT_ADDED' => ' non ajouté !',
-    // 'TEMPLATE_FILE_NOT_DELETED' => ' non supprimé !',
-    // 'TEMPLATE_FILE_ALREADY_EXISTING' => "Le fichier est déjà existant ! Changez de nom de preset ou connectez-vous en admin !",
-    // 'TEMPLATE_PRESET_ERROR' => "Impossible d'appliquer ce preset, il y a une erreur !",
-    // 'TEMPLATE_PRESETS' => 'Configurations graphiques',
-    // 'TEMPLATE_CREATE_PRESET' => 'Créer une nouvelle configuration graphique',
-    // 'TEMPLATE_CUSTOMIZE_PRESET' => 'Configuration graphique',
-
-    // actions-builder
-    // 'AB_template_group_label' => 'Mise en forme',
-    // 'AB_template_action_label_label' => 'Etiquette',
-    // 'AB_template_action_label_example' => 'Texte de votre étiquette à changer par la suite',
-    // 'AB_template_actions_class' => 'Classe',
-    // 'AB_template_actions_default' => 'Défaut',
-    // 'AB_template_actions_color' => 'Couleur',
-    // 'AB_template_actions_primary' => 'Primaire',
-    // 'AB_template_actions_secondary_1' => 'Secondaire-1',
-    // 'AB_template_actions_secondary_2' => 'Secondaire-2',
-    // 'AB_template_actions_success' => 'Succès',
-    // 'AB_template_actions_info' => 'Info',
-    // 'AB_template_actions_warning' => 'Attention',
-    // 'AB_template_actions_danger' => 'Danger',
-    // 'AB_template_action_accordion_label' => 'Afficher des encadrés en accordéon',
-    // 'AB_template_action_accordion_example' => "{{panel title=\"Titre 1\"}}\nTexte du panneau 1 à changer par la suite\n{{end elem=\"panel\"}}\n"
-    // ."{{panel title=\"Titre 2\"}}\nTexte du panneau 2 à changer par la suite\n{{end elem=\"panel\"}}\n",
-    // 'AB_template_action_ariane_label' => 'Fil d\'ariane',
-    // 'AB_template_action_col_label' => 'Colonne',
-    // 'AB_template_action_col_example' => 'Texte de votre colonne à changer par la suite',
-    // 'AB_template_col_size_label' => 'Largeur de la colonne',
-    // 'AB_template_action_grid_label' => 'Afficher plusieurs colonnes',
-    // 'AB_template_action_grid_example' => "{{col size=\"3\"}}\nTexte de la colonne 1 à changer par la suite\n{{end elem=\"col\"}}\n"
-    // ."{{col size=\"3\"}}\nTexte de la colonne 2 à changer par la suite\n{{end elem=\"col\"}}\n"
-    // ."{{col size=\"3\"}}\nTexte de la colonne 3 à changer par la suite\n{{end elem=\"col\"}}\n"
-    // ."{{col size=\"3\"}}\nTexte de la colonne 4 à changer par la suite\n{{end elem=\"col\"}}\n",
-    // 'AB_templates_nav_label' => 'Onglets',
-    // 'AB_templates_nav_description' => 'Générer un menu',
-    // 'AB_templates_nav_hint' => 'LeNomDeVotrePage doit être le nom de la page dans laquelle vous mettrez cette action. Pensez à coller le code obtenu dans chacune des pages des onglets.',
-    // 'AB_templates_nav_links_label' => 'Liens vers vos pages wiki',
-    // 'AB_templates_nav_links_default' => 'LeNomDeVotrePage, LaSecondePage, LaTroisiemePage', // no special chars !!
-    // 'AB_templates_nav_links_hint' => 'Nom des pages wiki séparées par des virgules',
-    // 'AB_templates_nav_titles_label' => 'Intitulés de vos pages',
-    // 'AB_templates_nav_titles_default' => 'Première page, Seconde page, Troisième page',
-    // 'AB_templates_nav_titles_hint' => 'Textes de chaque onglet séparés par des virgules',
-    // 'AB_templates_nav_class_label' => 'Affichage',
-    // 'AB_templates_nav_class_tabs' => 'Horizontal souligné',
-    // 'AB_templates_nav_class_pills' => 'Horizontal sobre',
-    // 'AB_templates_nav_class_justified' => 'Horizontal justifié',
-    // 'AB_templates_nav_class_vertical' => 'Vertical',
-    // 'AB_templates_nav_hide_if_no_access_label' => 'Masquer si l\'utilisateur n\'a pas accès à la page liée',
-    // 'AB_templates_panel_label' => 'Encadré',
-    // 'AB_templates_panel_wrappedcontentexample' => 'Texte de votre encadré à modifier par la suite',
-    // 'AB_templates_panel_title_label' => 'Titre',
-    // 'AB_templates_panel_title_default' => 'Titre de mon encadré',
-    // 'AB_templates_panel_type_default' => 'Simple encadré',
-    // 'AB_templates_panel_type_collapsible' => 'Accordéon ouvert',
-    // 'AB_templates_panel_type_collapsed' => 'Accordéon fermé',
-    // 'AB_templates_section_label' => 'Section',
-    // 'AB_templates_section_wrappedcontentexample' => '=====Titre===== Texte de votre section à remplacer par la suite',
-    // 'AB_templates_section_bgcolor_label' => 'Couleur de fond',
-    // 'AB_templates_section_height_label' => 'Hauteur (en pixels)',
-    // 'AB_templates_section_pattern_label' => 'Texture',
-    // 'AB_templates_section_pattern_solid' => 'Uni',
-    // 'AB_templates_section_pattern_border_solid' => 'Bordure',
-    // 'AB_templates_section_pattern_border_dashed' => 'Bordure tirets',
-    // 'AB_templates_section_pattern_border_dotted' => 'Bordure pointillés',
-    // 'AB_templates_section_pattern_cross' => 'Croix alignés',
-    // 'AB_templates_section_pattern_cross_not_aligned' => 'Croix décalées',
-    // 'AB_templates_section_pattern_points' => 'Points alignés',
-    // 'AB_templates_section_pattern_points_not_aligned' => 'Points décalées',
-    // 'AB_templates_section_pattern_zigzag' => 'Zigzag',
-    // 'AB_templates_section_pattern_diag' => 'Diagonales',
-    // 'AB_templates_section_pattern_reverse' => 'Inverser les couleurs de la texture',
-    // 'AB_templates_section_file_label' => 'Image de fond',
-    // 'AB_templates_section_file_hint' => 'Entrez le nom.jpg d\'une image déjà uploadé dans cette page, ou le nom d\'une nouvelle image.jpg pour faire apparaitre son boutton d\'upload',
-    // 'AB_templates_section_textcolor_label' => 'Tonalité du texte',
-    // 'AB_templates_section_textcolor_white' => 'Claire',
-    // 'AB_templates_section_textcolor_black' => 'Foncée',
-    // 'AB_templates_section_fullwidth_label' => 'Prendre toute la largeur disponible',
-    // 'AB_templates_section_shape_label' => 'Forme',
-    // 'AB_templates_section_shape_rect' => 'Rectangle',
-    // 'AB_templates_section_shape_rounded' => 'Arrondi',
-    // 'AB_templates_section_shape_circ' => 'Cercle',
-    // 'AB_templates_section_shape_blob1' => 'Blob 1',
-    // 'AB_templates_section_shape_blob2' => 'Blob 2',
-    // 'AB_templates_section_shape_blob3' => 'Blob 3',
-    // 'AB_templates_section_shape_blob4' => 'Blob 4',
-    // 'AB_templates_section_shape_blob5' => 'Blob 5',
-    // 'AB_templates_section_textalign_label' => 'Centrage du texte',
-    // 'AB_templates_section_textalign_left' => 'Calé à gauche',
-    // 'AB_templates_section_textalign_right' => 'Calé à droite',
-    // 'AB_templates_section_textalign_center' => 'Centré',
-    // 'AB_templates_section_textalign_justify' => 'Justifié',
-    // 'AB_templates_section_image_label' => 'Comportement de l\'image',
-    // 'AB_templates_section_image_cover' => 'Couvre section avec l\'image',
-    // 'AB_templates_section_image_fixed' => 'L\'image est bloquée lors du scroll (parallax)',
-    // 'AB_templates_section_image_center' => 'L\'image reste centré',
-    // 'AB_templates_section_image_parallax' => 'L\'image est légèrement grossie',
-    // 'AB_templates_section_height_label' => 'Hauteur préfigurée de la section',
-    // 'AB_templates_section_height_one_quarter' => 'un quart de la hauteur',
-    // 'AB_templates_section_height_one_third' => 'un tiers',
-    // 'AB_templates_section_height_half' => 'une moitié',
-    // 'AB_templates_section_height_two_third' => 'deux tiers',
-    // 'AB_templates_section_height_three_quarter' => 'trois quarts',
-    // 'AB_templates_section_height_full' => '100% hauteur',
-    // 'AB_templates_section_animation_label' => 'Animation',
-    // 'AB_templates_section_animation_hint' => 'Beaucoup plus d\'effets disponibles sur https://animate.style/',
-    // 'AB_templates_section_animation_bounce' => 'Bonds',
-    // 'AB_templates_section_animation_flash' => 'Flash',
-    // 'AB_templates_section_animation_pulse' => 'Pulse',
-    // 'AB_templates_section_animation_rubberband' => 'Elastique',
-    // 'AB_templates_section_animation_shakex' => 'Gauche à droite',
-    // 'AB_templates_section_animation_shakey' => 'Bas en haut',
-    // 'AB_templates_section_animation_headshaked' => 'Secoué',
-    // 'AB_templates_section_animation_swing' => 'Swing',
-    // 'AB_templates_section_animation_tada' => 'Tada',
-    // 'AB_templates_section_animation_wobble' => 'Essuie glace',
-    // 'AB_templates_section_animation_jello' => 'Danse',
-    // 'AB_templates_section_animation_heartbat' => 'Battemments',
-    // 'AB_templates_section_visible_label' => 'Visible par',
-    // 'AB_templates_section_visible_everyone' => 'Tout le monde',
-    // 'AB_templates_section_visible_connected_user' => 'Utilisateur connecté',
-    // 'AB_templates_section_visible_owner' => 'Propriétaire de la page',
-    // 'AB_templates_section_visible_admins' => 'Admins seulement',
-    // 'AB_templates_section_visible_no_container' => 'Ne pas mettre de conteneur',
-    // 'AB_templates_tabs_label' => 'Onglets dans la page',
-    // 'AB_templates_tabs_description' => 'Générer des onglets',
-    // 'AB_templates_tabs_hint' => 'Chaque contenu d\'onglet se trouve dans une action <code>{{tab}}Contenu de l\'onglet.{{end elem="tab"}}</code>',
-    // 'AB_templates_tabs_wrappedcontentexample' => "{{tab}}\n".
-    //     "Contenu de l'onglet 1\n".
-    //     "{{end elem=\"tab\"}}\n".
-    //     "{{tab}}\n".
-    //     "Contenu de l'onglet 2\n".
-    //     "{{end elem=\"tab\"}}\n".
-    //     "{{tab}}\n".
-    //     "Contenu de l'onglet 3\n".
-    //     "{{end elem=\"tab\"}}\n",
-    // 'AB_templates_tabs_titles_label' => 'Titres des onglets',
-    // 'AB_templates_tabs_titles_hint' => 'Séparer chaque titre par une virgule',
-    // 'AB_templates_tabs_titles_default' => 'Onglet 1, Onglet 2, Onglet 3',
-    // 'AB_templates_tabs_btnsize_label' => 'Taille de bouton',
-    // 'AB_templates_tabs_btnsize_default' => 'Normale',
-    // 'AB_templates_tabs_btnsize_small' => 'Petite',
-    // 'AB_templates_tabs_bottom_nav_label' => 'Navigation en bas des onglets',
-    // 'AB_templates_tabs_bottom_nav_yes' => 'Oui',
-    // 'AB_templates_tabs_bottom_nav_no' => 'Non',
-    // 'AB_templates_tabs_counter_on_bottom_nav_label' => 'Numéroter les Boutons de navigation',
-    // 'AB_templates_tabs_counter_on_bottom_nav_yes' => 'Oui',
-    // 'AB_templates_tabs_counter_on_bottom_nav_no' => 'Non',
-    // 'AB_templates_tabs_btncolor_label' => 'Couleur des boutons',
-    // 'AB_templates_tabs_btncolor_primary' => 'Primaire',
-    // 'AB_templates_tabs_btncolor_secondary_1' => 'Secondaire 1',
-    // 'AB_templates_tabs_btncolor_secondary_2' => 'Secondaire 2',
-    // 'AB_templates_tabs_selectedtab_label' => 'Onglet sélectionné',
-
-    // gererdroits
-    // 'ACLS_SELECT_PAGES_FILTER' => 'Filtrer :',
-    // 'ACLS_SELECT_PAGES_FILTER_ON_PAGES' => 'les pages uniquement',
-    // 'ACLS_SELECT_PAGES_FILTER_ON_SPECIALPAGES' => 'les pages spéciales uniquement',
-    // 'ACLS_SELECT_PAGES_FILTER_ON_LISTS' => 'les listes uniquement',
-    // 'ACLS_SELECT_PAGES_FILTER_FORM' => 'les fiches du formulaire : {name} ({id})',
-
-    // actions/gererthemes.php
-    // 'GERERTHEMES_ACTIONS' => 'Actions',
-    // 'GERERTHEMES_HINT' => 'Cochez les pages que vous souhaitez modifier et choisissez une action en bas de page',
-    // 'GERERTHEMES_INIT_THEME_FOR_SELECTED_PAGES' => 'Réinitialiser les pages selectionnées (elles utiliseront le thème par défault)',
-    // 'GERERTHEMES_MODIFY_THEME_FOR_SELECTED_PAGES' => 'Modifier les valeurs pour les pages sélectionnées',
-    // 'GERERTHEMES_PAGE' => 'Page',
-
-    // actions/progressbar.php
-    // 'PROGRESSBAR_REQUIRED_VAL_PARAM' => 'param&egrave;tre "val" obligatoire.',
-    // 'PROGRESSBAR_ERROR_VAL_PARAM' => 'le param&egrave;tre "val" doit étre un chiffre entre 0 et 100.',
-
-    // templates/setdefaulttheme.tpl.html
-    // 'SETDEFAULTTHEME_HNIT' => 'Le thème par défault est utilisé pour les pages qui n\'ont aucun thème défini (les lignes vides dans le tableau ci dessus)',
-
-    // for edit config
-    // 'EDIT_CONFIG_HINT_META_KEYWORDS' => 'Mots clés pour le référencement (séparés par des virgules, pas plus de 20-30)',
-    // 'EDIT_CONFIG_HINT_META_DESCRIPTION' => 'Description du site en une phrase, pour le référencement (Attention : ne pas mettre de "." (point))',
-    // 'EDIT_CONFIG_HINT_META[ROBOTS]' => 'Empêcher les robots à indexer le wiki (Mettre \'noindex,nofollow,max-image-preview:none,noarchive,noimageindex\')',
-    // 'EDIT_CONFIG_GROUP_TEMPLATES' => 'Balises meta pour l\'indexation web', // idéalement 'Mise en forme' mais templates est pour le moment uniquement utilisé pour meta
 ];
