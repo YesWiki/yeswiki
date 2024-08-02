cf3f42ef6a58b5c16b9f53133f36cfcb33a3e328
LibreTranslate <noreply-mt-libretranslate@weblate.org>
2024-08-02 07:57:27 +0000

    Translated using Weblate (Portuguese)
    Currently translated at 85.7% (12 of 14 strings)
    
    Translation: YesWiki/YesWiki extension Syndication
    Translate-URL: https://hosted.weblate.org/projects/yeswiki/yeswiki-extension-syndication/pt/

--- tools/syndication/lang/syndication_pt.inc.php
+++ tools/syndication/lang/syndication_pt.inc.php
@@ -1,5 +1,4 @@
 <?php
-
 return [
     // actions/syndication.php
     'SYNDICATION_ACTION_SYNDICATION' => 'Ação {{syndication ...}}',
@@ -8,18 +7,14 @@ return [
     'SYNDICATION_CREATE_CACHE_FOLDER' => 'devemos criar um diretório "cache" no diretório principal',
     'SYNDICATION_TEMPLATE_NOT_FOUND' => 'não existe, o arquivo template padrão é usado.',
     'SYNDICATION_READ_MORE' => 'Leia mais',
-
     // actions/twitter.php
     'SYNDICATION_ACTION_TWITTER' => 'Ação {{twitter ...}}',
     'SYNDICATION_PARAM_USER_REQUIRED' => 'parâmetro "usuário" faltando, é obrigatório para especificar o usuário do twitter selecionado',
-
     // actions/twittersearch.php
     'SYNDICATION_ACTION_TWITTERSEARCH' => 'Ação {{twittersearch ...}}',
     'SYNDICATION_PARAM_QUERY_REQUIRED' => 'parâmetro "query" faltando, é obrigatório para especificar sua pesquisa',
     'SYNDICATION_TEMPLATE_DOESNT_EXISTS' => 'O arquivo template',
     'SYNDICATION_USE_OF_DEFAULT_TEMPLATE' => 'não existe, o arquivo template padrão é usado.',
-
-    // templates/accordeon.tpl.html
-    // 'SYNDICATION_SOURCE' => 'source',
-    // 'SYNDICATION_SEE_ALL_CONTENT' => 'Accéder à l\'intégralité du contenu',
+    'SYNDICATION_SOURCE' => 'fonte',
+    'SYNDICATION_SEE_ALL_CONTENT' => 'Acesso a todo o conteúdo',
 ];
