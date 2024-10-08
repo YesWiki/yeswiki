cfe955fa639434e9489218e4b98c508928ec5438
oiseauroch <contact@oiseauroch.fr>
2024-10-08 12:01:35 +0200

    add test for groupcontroller and fix functions issues

--- includes/controllers/GroupController.php
+++ includes/controllers/GroupController.php
@@ -43,6 +43,10 @@ class GroupController extends YesWikiController
      */
     public function groupExists(string $name): bool
     {
+        if (str_starts_with($name, "@"))
+        {
+            $name = substr($name, 1);
+        }
         return $this->groupManager->groupExists($name);
     }
 
@@ -111,37 +115,22 @@ class GroupController extends YesWikiController
         if ($this->groupManager->groupExists($name)) {
             $this->groupManager->delete($name);
         } else {
-            throw new GroupNameDoesNotExistException(_t(GROUP_NAME_DOES_NOT_EXIST));
+            throw new GroupNameDoesNotExistException(_t('GROUP_NAME_DOES_NOT_EXIST'));
         }
     }
 
     /**
     *  add users and/or groups to group
-    * @param string $group_name
+    * @param string $groupName
     * @param array $members users and/or groups to add
     * @throws UserDoesNotExistException
     * @throws GroupDoesNotExistException
     * @throws InvalidInputException
     * @return void
     */
-    public function add(string $group_name, array $members): void
+    public function add(string $groupName, array $members): void
     {
-        if(!$this->groupManager->groupExists($group_name)) {
-            throw new GroupNameDoesNotExistException(_t('GROUP_NAME_DOES_NOT_EXIST'));
-        }
-        foreach($members as $member) {
-            switch ($this->checkMemberValidity($group_name, $member)) {
-                case 0:
-                    break;
-                case 1:
-                    throw new UserNameDoesNotExistException(_t('USER_NAME_DOES_NOT_EXIST'));
-                case 2:
-                    throw new GroupNameDoesNotExistException(_t('GROUP_NAME_DOES_NOT_EXIST'));
-                case 3:
-                    throw new InvalidInputException(_t('ERROR_RECURSIVE_GROUP'));
-            }
-        }
-        $this->groupManager->add($group_name, $members);
+        $this->addOrUpdate($groupName, $members, true);
     }
 
     /**
@@ -149,25 +138,25 @@ class GroupController extends YesWikiController
      *  - if $member is a user, check if the user exists
      *  - if $member is a group, check if the group exists
      *  - if $member is a group, check if the group doesn't define itself recursively
-     *  @param string $group_name
-     *  @param string $member
+     *  @param string $groupName (without !/@)
+     *  @param string $member (with !/@ if present)
      *  @return : int
      *      - 0 if ok
      *      - 1 if user doesn't exist
      *      - 2 if group doesn't exist
      *      - 3 if group is recursive
      */
-    private function checkMemberValidity(string $group_name, string $member): int
+    private function checkMemberValidity(string $groupName, string $member): int
     {
         if(str_starts_with($member, "!")) {
-            $$member = substr($member, 1);
+            $member = substr($member, 1);
         }
         if(str_starts_with($member, "@")) {
 
-            if(!$this->groupManager->groupExists($member)) {
+            if(!$this->groupExists($member)) {
                 return 2;
             }
-            if(!this->CheckGroupRecursive($member, $group_name)) {
+            if($this->CheckGroupRecursive(substr($member, 1), strtolower(trim($groupName)))) {
                 return 3;
             }
         } else {
@@ -180,23 +169,22 @@ class GroupController extends YesWikiController
 
 
     /**
-     * Checks if a new group acl is not defined recursively
+     * Checks if a new group acl is defined recursively
      * (this method expects that groups that are already defined are not themselves defined recursively...)
      *
-     * @param string $group_name
+     * @param string $groupName
      *            The name of the group to test against origin
      * @param string $origin group name to save test recursivity
      * @return bool True if the new acl defines the group recursively
      */
-    private function CheckGroupRecursive($group_name, $origin, $checked = array()): bool
+    private function CheckGroupRecursive($groupName, $origin, $checked = array()): bool
     {
-        $group_name = strtolower(trim($group_name));
-        if ($group_name === $origin) {
+        $groupName = strtolower(trim($groupName));
+        if ($groupName === $origin) {
             return true;
         }
-        $members = this->get_members($group_name);
-        $recursive_members = str_replace(["\r\n", "\r"], "\n", $recursive_members);
-        foreach (explode("\n", $recursive_members) as $line) {
+        $recursive_members = $this->getMembers($groupName);
+        foreach ($recursive_members as $line) {
             $line = trim($line);
             if (!$line) {
                 continue;
@@ -218,54 +206,68 @@ class GroupController extends YesWikiController
                 }
             }
         }
-        $checked[] = $group_name;
+        $checked[] = $groupName;
         return false;
     }
 
     /**
      *  remove  users  and/or groups from group
-     * @param array $names users and/or groups to add
-     * @return bool
-     * @throws UserDoesNotExistException
+     * @param array $members users and/or groups to add
+     * @return void
      * @throws GroupDoesNotExistException
      */
-    public function remove(array $names): void
+    public function remove(string $groupName, array $members): void
     {
-        $this->groupManager->removeMembers($names);
+        if(!$this->groupManager->groupExists($groupName)) {
+            throw new GroupNameDoesNotExistException(_t('GROUP_NAME_DOES_NOT_EXIST'));
+        }
+        $this->groupManager->removeMembers($groupName, $members);
     }
-
+    
     /**
-     *  replace current members with new one
+     *  add or replace current members with new one
      * @param string $groupName
-     * @param array $names new members List
+     * @param array $members new members List
+     * @param string $action
      * @return bool
      * @throws UserDoesNotExistException
      * @throws GroupDoesNotExistException
      * @throws InvalidInputException
      */
-    public function update(string $groupName, array $names)
-    {
-        if(!$this->groupManager->groupExists($groupName)) {
+    private function addOrUpdate(string $groupName, array $members, bool $add):void {
+         if(!$this->groupManager->groupExists($groupName)) {
             throw new GroupNameDoesNotExistException(_t('GROUP_NAME_DOES_NOT_EXIST'));
         }
-        foreach ($names as $name) {
-            if(str_starts_with($name, "!")) {
-                $name = substr($name, 1);
-            }
-            if(str_starts_with($name, "@")) {
-
-                if(!$this->groupManager->groupExists($name)) {
-                    throw new GroupNameDoesNotExistException(_t('GROUP_NAME_DOES_NOT_EXIST'));
-                }
-                if(!this->CheckGroupRecursive($name, $groupName)) {
-                    throw new InvalidInputException(_t('RECURSIVE_GROUP_ERROR')); //FIXME
-                }
-            } else {
-                if(!$this->userManager->userExist($name)) {
+        foreach($members as $member) {
+            switch ($this->checkMemberValidity($groupName, $member)) {
+                case 0:
+                    break;
+                case 1:
                     throw new UserNameDoesNotExistException(_t('USER_NAME_DOES_NOT_EXIST'));
-                }
+                case 2:
+                    throw new GroupNameDoesNotExistException("included "._t('GROUP_NAME_DOES_NOT_EXIST'));
+                case 3:
+                    throw new InvalidInputException(_t('ERROR_RECURSIVE_GROUP'));
             }
         }
-        $this->groupManager->update($groupName, $name);
+        if ($add) {
+            $this->groupManager->addMembers($groupName, $members);
+        } else {
+            $this->groupManager->update($groupName, $members);
+        }
+    }
+
+    /**
+     *  replace current members with new one
+     * @param string $groupName
+     * @param array $members new members List
+     * @return void
+     * @throws UserDoesNotExistException
+     * @throws GroupDoesNotExistException
+     * @throws InvalidInputException
+     */
+    public function update(string $groupName, array $members):void 
+    {
+        this->addOrUpdate($groupName, $members, false);
     }
 }
--- tests/includes/controllers/GroupControllerTest.php
+++ tests/includes/controllers/GroupControllerTest.php
@@ -78,19 +78,164 @@ class GroupControllerTest extends YesWikiTestCase
                 $groupcontroller->create($groupname, $members);
             }
     }
+
+    
+    /**
+     * 
+     * @depends testGroupControllerExisting
+     * @depends testCreate
+     * @covers GroupController::getMembers
+     * @dataProvider dataProviderTestCreate
+     */
+    public function testGetMembers(string $groupname, int $result_type, array $members, GroupController $groupcontroller) {
+            if ($result_type == 0 ) {
+               $groupcontroller->create($groupname, $members);
+               $groupcontroller->getMembers($groupname);
+               $this->assertEquals($groupcontroller->getMembers($groupname), $members);
+            } else if ($result_type == 1) {
+                $this->expectException(GroupNameDoesNotExistException::class);
+                $groupcontroller->getMembers($groupname);
+            } else if ($result_type == 2) {
+               $groupcontroller->getMembers($groupname);
+               $this->assertEquals($groupcontroller->getMembers($groupname), $members);
+            } else {
+                $this->expectException(GroupNameDoesNotExistException::class);
+                $groupcontroller->getMembers($groupname);
+            }
+    }
+
+    /**
+     * 
+     * @depends testGroupControllerExisting
+     * @depends testCreate
+     * @covers GroupController::delete
+     * @dataProvider dataProviderTestCreate
+     */
+    public function testDelete(string $groupname, int $result_type, array $members, GroupController $groupcontroller) {
+            if ($result_type == 0 ) {
+               $groupcontroller->create($groupname, $members);
+               $groupcontroller->delete($groupname);
+               $this->assertFalse($groupcontroller->groupExists($groupname));
+            } else if ($result_type == 1) {
+                $this->expectException(GroupNameDoesNotExistException::class);
+                $groupcontroller->delete($groupname);
+            } else if ($result_type == 2) {
+               $groupcontroller->create($groupname, $members);
+               $groupcontroller->delete($groupname);
+               $this->assertFalse($groupcontroller->groupExists($groupname));
+            } else {
+                $this->expectException(GroupNameDoesNotExistException::class);
+                $groupcontroller->delete($groupname);
+            }
+    }
+    
+    
+    
+    public function dataProviderTestAdd()
+    {
+        $wiki = $this->getWiki();
+        $valid_group_name = $wiki->generateRandomString(10, self::CHARS_FOR_GROUP);
+        $new_valid_group = $wiki->generateRandomString(10, self::CHARS_FOR_GROUP);
+        $third_valid_group = $wiki->generateRandomString(10, self::CHARS_FOR_GROUP);
+        $fourth_valid_group = $wiki->generateRandomString(10, self::CHARS_FOR_GROUP);
+        $not_existing_group = $wiki->generateRandomString(10, self::CHARS_FOR_GROUP);
+
+        $user_name = $wiki->generateRandomString(10);
+        $user_name_1 = $wiki->generateRandomString(10);
+        
+        $userController = $wiki->services->get(UserController::class);
+        $userController->create(['name' => $user_name, 'email' => $valid_group_name.'@example.com', 'password' => $user_name]); 
+        $userController->create(['name' => $user_name_1, 'email' => $new_valid_group.'@example.com', 'password' => $user_name]); 
+        
+        $groupController = $wiki->services->get(GroupController::class);
+        $groupController->create($valid_group_name, [$user_name_1]);
+        $groupController->create($new_valid_group, [$user_name_1]);
+        $groupController->create($third_valid_group, [$user_name_1,'@'.$valid_group_name]);
+        $groupController->create($fourth_valid_group, [$user_name_1,'@'.$third_valid_group]);
+        
+        // groupname, error type, members
+        return [
+            'valid scenario' => [$valid_group_name,  0, [$user_name]],
+            'valid group add' => [$valid_group_name, 0, ['@'.$new_valid_group]],
+            'user does not exist' => [$valid_group_name, 1, [$new_valid_group]],
+            'group does not exist' => [$not_existing_group, 2 , ['@'.$valid_group_name]],
+            'included group does not exist' => [$valid_group_name, 2 , ['@'.$not_existing_group]],
+            'recursive group' => [$valid_group_name,3, ['@'.$fourth_valid_group, $user_name]],
+            ];
+    }
     
     
     /**
      * 
      * @depends testGroupControllerExisting
-     * @covers GroupController::getMembers
+     * @depends testCreate
+     * @covers GroupController::add
+     * @covers GroupController::update
+     * @dataProvider dataProviderTestAdd
      */
-    public function getMembersFromNotExistingGroup(GroupController $groupcontroller) {
+    public function testAdd(string $groupname, int $result_type, array $members, GroupController $groupcontroller) {
+            if ($result_type == 0 ) {
+               $groupcontroller->add($groupname, $members);
+               foreach ($members as $member) {
+                $this->assertContains($member, $groupcontroller->getMembers($groupname));
+               }
+            } else if ($result_type == 1) {
+                $this->expectException(UserNameDoesNotExistException::class);
+                $groupcontroller->add($groupname, $members);
+            } else if ($result_type == 2) {
+                $this->expectException(GroupNameDoesNotExistException::class);
+                $groupcontroller->add($groupname, $members);
+            } else {
+                $this->expectException(InvalidInputException::class);
+                $groupcontroller->add($groupname, $members);
+            }
+    }
+    
+     public function dataProviderTestRemoveMembers()
+    {
         $wiki = $this->getWiki();
-        $group_name = $wiki->generateRandomString(10);
+        $valid_group_name = $wiki->generateRandomString(10, self::CHARS_FOR_GROUP);
+        $new_valid_group = $wiki->generateRandomString(10, self::CHARS_FOR_GROUP);
+        $not_existing_group = $wiki->generateRandomString(10, self::CHARS_FOR_GROUP);
+
+        $user_name = $wiki->generateRandomString(10);
+        $user_name_1 = $wiki->generateRandomString(10);
         
-        $this->expectException(GroupNameDoesNotExistException::class);
-        $groupcontroller->getMembers($$group_name);
+        $userController = $wiki->services->get(UserController::class);
+        $userController->create(['name' => $user_name, 'email' => $valid_group_name.'@example.com', 'password' => $user_name]); 
+        $userController->create(['name' => $user_name_1, 'email' => $new_valid_group.'@example.com', 'password' => $user_name]); 
+        
+        $groupController = $wiki->services->get(GroupController::class);
+        $groupController->create($new_valid_group, [$user_name_1, $user_name]);
+        $groupController->create($valid_group_name, [$user_name_1, $user_name, '@'.$new_valid_group]);
+
+        // groupname, error type, members
+        return [
+            'remove one user' => [$new_valid_group,  0, [$user_name]],
+            'remove one user and one group' => [$valid_group_name, 0, ['@'.$new_valid_group, $user_name_1]],
+            'group does not exist' => [$not_existing_group, 2 , ['@'.$valid_group_name]],
+            'remove not existing user' => [$valid_group_name,0, ['@'.$new_valid_group, $user_name]],
+            ];
+    }
+    
+    /**
+     * 
+     * @depends testGroupControllerExisting
+     * @depends testCreate
+     * @covers GroupController::removeMembers
+     * @dataProvider dataProviderTestRemoveMembers
+     */
+    public function testRemoveMembers(string $groupname, int $result_type, array $members, GroupController $groupcontroller)
+    {
+         if ($result_type == 0 ) {
+               $groupcontroller->remove($groupname, $members);
+               foreach ($members as $member) {
+                $this->assertNotContains($member, $groupcontroller->getMembers($groupname));
+               }
+            } else {
+                $this->expectException(GroupNameDoesNotExistException::class);
+                $groupcontroller->remove($groupname, $members);
+        }
     }
 }
 
