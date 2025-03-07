da8d427ff09f445a88b78e1d5b24add293d40f54
Florian Schmitt <mrflos@gmail.com>
2025-03-07 20:41:10 +0300

    chore(dep): update simple simplepie for rss

--- tools/syndication/vendor/composer/installed.php
+++ tools/syndication/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => 'dev-doryphore-dev',
-        'version' => 'dev-doryphore-dev',
-        'reference' => 'abb312bb165f15ca0e8caadfee47cfb249477cdd',
+        'pretty_version' => 'dev-rss',
+        'version' => 'dev-rss',
+        'reference' => 'eaa8698aa3f4866a05440301c2c04b087f8f009a',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,18 +11,18 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => 'dev-doryphore-dev',
-            'version' => 'dev-doryphore-dev',
-            'reference' => 'abb312bb165f15ca0e8caadfee47cfb249477cdd',
+            'pretty_version' => 'dev-rss',
+            'version' => 'dev-rss',
+            'reference' => 'eaa8698aa3f4866a05440301c2c04b087f8f009a',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
             'dev_requirement' => false,
         ),
         'simplepie/simplepie' => array(
-            'pretty_version' => '1.8.0',
-            'version' => '1.8.0.0',
-            'reference' => '65b095d87bc00898d8fa7737bdbcda93a3fbcc55',
+            'pretty_version' => '1.8.1',
+            'version' => '1.8.1.0',
+            'reference' => 'a567b8ab9b6145a23e6a9ec2b6b74f56d52f7ad1',
             'type' => 'library',
             'install_path' => __DIR__ . '/../simplepie/simplepie',
             'aliases' => array(),
