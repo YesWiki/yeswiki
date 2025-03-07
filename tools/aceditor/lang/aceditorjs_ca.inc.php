25b7c54e5eba4bbe866cd5eb6a77cc372c0874a0
Hosted Weblate <hosted@weblate.org>
2025-03-07 21:57:01 +0100

    Translated using Weblate
    Co-authored-by: Hosted Weblate <hosted@weblate.org>
    Co-authored-by: rahma rais <rahma@marsnet.org>
    Translate-URL: https://hosted.weblate.org/projects/yeswiki/yeswiki-extension-aceditor-js/ca/
    Translate-URL: https://hosted.weblate.org/projects/yeswiki/yeswiki-extension-attach/ca/
    Translate-URL: https://hosted.weblate.org/projects/yeswiki/yeswiki-extension-bazar/ca/
    Translation: YesWiki/YesWiki extension Aceditor js
    Translation: YesWiki/YesWiki extension Attach
    Translation: YesWiki/YesWiki extension bazar

--- tools/aceditor/lang/aceditorjs_ca.inc.php
+++ tools/aceditor/lang/aceditorjs_ca.inc.php
@@ -1,13 +1,12 @@
 <?php
-
 return [
     'ACTION_BUILDER_COPY' => 'Còpia',
-    'ACTION_BUILDER_PREVIEW' => 'Descripció general (no es pot clicar)',
+    'ACTION_BUILDER_PREVIEW' => 'Vista prèvia (no es pot fer clic)',
     'ACTION_BUILDER_ONLINEDOC' => 'Documentació en línia',
     'ACTION_BUILDER_UPDATE_CODE' => 'Actualitza el codi',
-    'ACTION_BUILDER_INSERT_CODE' => 'Inserir a la pàgina',
-    'ACTION_BUILDER_OWNER' => 'Titular del fitxer',
-    'ACTION_BUILDER_MODIFICATION_DATE' => 'Data de modificació',
+    'ACTION_BUILDER_INSERT_CODE' => 'Insereix a la pàgina',
+    'ACTION_BUILDER_OWNER' => 'Propietari del llistat',
+    'ACTION_BUILDER_MODIFICATION_DATE' => 'Data modificada',
     'ACTION_BUILDER_CREATION_DATE' => 'Data de creació',
-    'ACTION_BUILDER_FORM_ID' => 'Formularis',
+    'ACTION_BUILDER_FORM_ID' => 'Formulari',
 ];
