d0f2dd9fd589befa9ae7f5c08681fac6b3ab585b
Hosted Weblate <hosted@weblate.org>
2025-03-07 13:50:39 +0100

    Translated using Weblate
    Co-authored-by: Hosted Weblate <hosted@weblate.org>
    Co-authored-by: Marsnet Rv <rv@marslibre.net>
    Translate-URL: https://hosted.weblate.org/projects/yeswiki/yeswiki-extension-rss/es/
    Translation: YesWiki/YesWiki extension Rss

--- tools/rss/lang/rss_es.inc.php
+++ tools/rss/lang/rss_es.inc.php
@@ -1,12 +1,12 @@
 <?php
 return [
-    'RSS_CHANGE_OF' => '',
-    'RSS_HISTORY' => '',
-    'RSS_HIDDEN_CONTENT' => '',
-    'RSS_ON_DATE' => '',
-    'RSS_COMPARISON_OF' => '',
-    'RSS_TO' => '',
-    'RSS_ADDS' => '',
-    'RSS_DELETIONS' => '',
-    'RSS_NO_DIFF' => '',
+    'RSS_CHANGE_OF' => 'Modificación de',
+    'RSS_HISTORY' => 'histórico',
+    'RSS_HIDDEN_CONTENT' => 'contenido oculto',
+    'RSS_ON_DATE' => 'el',
+    'RSS_COMPARISON_OF' => 'Comparación de',
+    'RSS_TO' => 'a',
+    'RSS_ADDS' => 'Añadidos',
+    'RSS_DELETIONS' => 'supresiones',
+    'RSS_NO_DIFF' => 'Ninguna diferencia',
 ];
