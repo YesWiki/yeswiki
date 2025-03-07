34cd1c33d46c4dea6c4790d3c421378b895ad24d
Hosted Weblate <hosted@weblate.org>
2025-03-07 14:28:46 +0100

    Translated using Weblate
    Co-authored-by: Hosted Weblate <hosted@weblate.org>
    Co-authored-by: Marsnet Rv <rv@marslibre.net>
    Translate-URL: https://hosted.weblate.org/projects/yeswiki/yeswiki-extension-helloword/es/
    Translate-URL: https://hosted.weblate.org/projects/yeswiki/yeswiki-extension-security/es/
    Translate-URL: https://hosted.weblate.org/projects/yeswiki/yeswiki-extension-syndication/es/
    Translation: YesWiki/YesWiki extension Helloword
    Translation: YesWiki/YesWiki extension Security
    Translation: YesWiki/YesWiki extension Syndication

--- tools/helloworld/lang/helloworld_es.inc.php
+++ tools/helloworld/lang/helloworld_es.inc.php
@@ -1,13 +1,13 @@
 <?php
 return [
-    'HELLOWORLD_MY_MESSAGE' => '',
-    'HELLOWORD_NO_MSG_PARAM' => '',
-    'HELLOWORD_CALLBACK_MSG' => '',
-    'HELLOWORLD_CONFIG' => '',
-    'HELLOWORLD_URL_SHOW_XML' => '',
-    'HELLOWORLD_URL_SHOW_TAG' => '',
-    'HELLOWORLD_URL_EDIT_TAG' => '',
-    'HELLOWORLD_URL_EDIT_WITH_PARAMS' => '',
-    'HELLOWORLD_PAGE_NOT_ACCESSIBLE' => '',
-    'HELLOWORLD_FIELD_ALERT' => '',
+    'HELLOWORLD_MY_MESSAGE' => 'Mi mensaje',
+    'HELLOWORD_NO_MSG_PARAM' => 'No hay parámetro \'mensaje\' definido en la acción {{greeting ...}}',
+    'HELLOWORD_CALLBACK_MSG' => '(el argumento de la acción se cambia en el callback anterior)',
+    'HELLOWORLD_CONFIG' => 'Prueba de la config',
+    'HELLOWORLD_URL_SHOW_XML' => 'Ver información XML de la página actual',
+    'HELLOWORLD_URL_SHOW_TAG' => 'Mostrar %{tag}',
+    'HELLOWORLD_URL_EDIT_TAG' => 'Editar %{tag}',
+    'HELLOWORLD_URL_EDIT_WITH_PARAMS' => 'Editar la página actual con params',
+    'HELLOWORLD_PAGE_NOT_ACCESSIBLE' => 'Página no accesible',
+    'HELLOWORLD_FIELD_ALERT' => 'Esto se guardará en la entrada',
 ];
--- tools/syndication/lang/syndication_es.inc.php
+++ tools/syndication/lang/syndication_es.inc.php
@@ -2,8 +2,8 @@
 return [
     'SYNDICATION_ACTION_SYNDICATION' => 'Acción {{syndication ...}}',
     'SYNDICATION_PARAM_URL_REQUIRED' => 'hay que entrar el parámetro "url" para sindicar un flujo RSS',
-    'SYNDICATION_WRITE_ACCESS_TO_CACHE_FOLDER' => 'el repertorio "cache" no tiene los derechos de accesso para escribir',
-    'SYNDICATION_CREATE_CACHE_FOLDER' => 'hay que crear un repertorio "cache" en el repertorio principal',
+    'SYNDICATION_WRITE_ACCESS_TO_CACHE_FOLDER' => 'el directorio "cache" no tiene permisos de escritura',
+    'SYNDICATION_CREATE_CACHE_FOLDER' => 'hay que crear un directorio "cache" en el directorio principal',
     'SYNDICATION_TEMPLATE_NOT_FOUND' => 'no existe, se usa el archivo patrón por defecto.',
     'SYNDICATION_READ_MORE' => 'Leer más',
     'SYNDICATION_ACTION_TWITTER' => 'Acción {{twitter ...}}',
@@ -11,7 +11,7 @@ return [
     'SYNDICATION_ACTION_TWITTERSEARCH' => 'Acción {{twittersearch ...}}',
     'SYNDICATION_PARAM_QUERY_REQUIRED' => 'falta el parámetro "query". Es obligatorio para precisar tu búsqueda',
     'SYNDICATION_TEMPLATE_DOESNT_EXISTS' => 'El archivo template',
-    'SYNDICATION_USE_OF_DEFAULT_TEMPLATE' => 'no existe, se usa el patrón por defecto.',
+    'SYNDICATION_USE_OF_DEFAULT_TEMPLATE' => 'no existe, se usa el patrón por defecto',
     'SYNDICATION_SOURCE' => 'fuente',
     'SYNDICATION_SEE_ALL_CONTENT' => 'Acceder a todo el contenido',
 ];
