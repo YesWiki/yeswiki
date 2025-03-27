cb409c594ace5ba7c77522d55b6759ca6d404e30
oiseauroch <contact@oiseauroch.fr>
2025-03-27 20:31:54 +0100

    add frontend support for multi lang lists

--- tools/bazar/presentation/javascripts/list-form.js
+++ tools/bazar/presentation/javascripts/list-form.js
@@ -1,60 +1,149 @@
-import ListNode from './list-node.js'
+import ListNode from './list-node.js';
 
 new Vue({
   el: '.list-form',
   components: { 'list-node': ListNode },
   data: {
     title: '',
-    rootNode: { id: '@root@', vueRef: '@root@', children: [] },
+    rootNode: {
+      id: '@root@',
+      vueRef: '@root@',
+      children: [],
+    },
     allIds: [],
-    nodeCreated: 0
+    nodeCreated: 0,
+    selected_language: '0',
+    extra_langs: {},
+    current_title: '',
+    extra_langs_list: [],
+    extra_langs_titles: {},
   },
   mounted() {
-    const list = JSON.parse(this.$el.dataset.list)
-    this.title = list.title
-    const nodes = list.nodes || []
+    const list = JSON.parse(this.$el.dataset.list);
+    this.title = list.title;
+    const nodes = list.nodes || [];
+    const extra_langs = list.extra_lang || [];
+    this.extra_langs_list = ['eu', 'de', 'en'];
     // vueRef is used to give a unique and fixed ID to each node
-    nodes.forEach((node) => this.addVueRefProp(node))
-    this.rootNode.children = nodes
+    for (let lang in extra_langs) {
+      this.extra_langs_titles[lang] = extra_langs[lang].title ?? '';
+      extra_langs[lang]['children'] = extra_langs[lang]['nodes'];
+      delete extra_langs[lang]['nodes'];
+    }
+    this.extra_langs = extra_langs;
+    nodes.forEach((node) => this.addVueRefProp(node));
+    nodes.forEach((node) => {
+      for (let lang in this.extra_langs) {
+        if (
+          this.extra_langs[lang].children &&
+          this.extra_langs[lang].children[node.id]
+        )
+          this.MergeTranslations(
+            node,
+            lang,
+            this.extra_langs[lang].children[node.id],
+          );
+      }
+    });
+    this.rootNode.children = nodes;
   },
   computed: {
     jsonNodes() {
-      const data = this.rootNode.children.map((child) => this.removeVueRefProps({ ...child }))
-      return JSON.stringify(data)
-    }
+      const data = this.rootNode.children.map((child) =>
+        this.removeVueRefProps({ ...child }),
+      );
+
+      return JSON.stringify(data);
+    },
+    jsonExtraLangs() {
+      let extra_lang = {};
+      for (let lang of this.extra_langs_list) {
+        extra_lang[lang] = {};
+        if (this.extra_langs_titles[lang]) {
+          extra_lang[lang].title = this.extra_langs_titles[lang];
+        }
+        extra_lang[lang].nodes = this.getExtraLang(this.rootNode, lang);
+      }
+
+      return JSON.stringify(extra_lang);
+    },
   },
   methods: {
+    getExtraLang(child, lang) {
+      let node = {};
+      let empty = true;
+      if (child[lang]) {
+        empty = false;
+        node.label = child[lang];
+      }
+      if (child.children && child.children.length > 0) {
+        for (let c of child.children) {
+          let subnode = this.getExtraLang(c, lang);
+          if (subnode != null) {
+            node[c.id] = subnode;
+            empty = false;
+          }
+        }
+      }
+      if (!empty) {
+        return node;
+      } else {
+        return null;
+      }
+    },
     onSubmit(event) {
       // check for id presence and uniquness
-      this.allIds = []
-      this.collectIds(this.rootNode)
+      this.allIds = [];
+      this.collectIds(this.rootNode);
       if (this.allIds.some((id) => !id)) {
-        toastMessage(_t('LIST_ERROR_MISSING_IDS'), 4000, 'alert alert-danger')
-        event.preventDefault()
+        toastMessage(_t('LIST_ERROR_MISSING_IDS'), 4000, 'alert alert-danger');
+        event.preventDefault();
       }
-      const duplicatesIds = this.allIds.filter((item, index) => this.allIds.indexOf(item) !== index)
+      const duplicatesIds = this.allIds.filter(
+        (item, index) => this.allIds.indexOf(item) !== index,
+      );
       if (duplicatesIds.length > 0) {
         toastMessage(
           _t('LIST_ERROR_DUPLICATES_IDS') + duplicatesIds.join(', '),
           8000,
-          'alert alert-danger'
-        )
-        event.preventDefault()
+          'alert alert-danger',
+        );
+        event.preventDefault();
       }
     },
     collectIds(node) {
-      this.allIds.push(node.id)
-      node.children.forEach((childNode) => this.collectIds(childNode))
+      this.allIds.push(node.id);
+      node.children.forEach((childNode) => this.collectIds(childNode));
+    },
+    onLangChange(event) {
+      if (this.selected_language != '0') {
+        if (!this.extra_langs[this.selected_language]) {
+          this.extra_langs[this.selected_language] = {};
+        }
+        this.current_title = this.extra_langs_titles[this.selected_language];
+      }
     },
     addVueRefProp(node) {
-      node.vueRef = node.id
-      node.children ||= []
-      node.children.forEach((childNode) => this.addVueRefProp(childNode))
+      node.vueRef = node.id;
+      node.children ||= [];
+      node.children.forEach((childNode) => this.addVueRefProp(childNode));
     },
     removeVueRefProps(node) {
-      delete node.vueRef
-      node.children = node.children.map((child) => this.removeVueRefProps({ ...child }))
-      return node
-    }
-  }
-})
+      delete node.vueRef;
+      node.children = node.children.map((child) =>
+        this.removeVueRefProps({ ...child }),
+      );
+      return node;
+    },
+    MergeTranslations(node, lang, translations) {
+      if (translations.label) {
+        node[lang] = translations.label;
+      }
+      for (let child of node.children) {
+        if (translations.children && translations.children[child.id]) {
+          this.MergeTranslations(child, lang, translations.children[child.id]);
+        }
+      }
+    },
+  },
+});
--- tools/bazar/presentation/javascripts/list-node.js
+++ tools/bazar/presentation/javascripts/list-node.js
@@ -1,12 +1,12 @@
 export default {
   name: 'list-node',
-  props: ['node', 'depth', 'index'],
+  props: ['node', 'depth', 'index', 'selected_language', 'extra_langs_list'],
   emits: ['delete'],
   data() {
     return {
       expanded: false,
-      newNodeLabel: ''
-    }
+      newNodeLabel: '',
+    };
   },
   methods: {
     addChildNode() {
@@ -15,65 +15,72 @@ export default {
         label: this.newNodeLabel,
         id: this.slugify(this.newNodeLabel),
         children: [],
-        vueRef: Date.now()
-      })
-      this.newNodeLabel = ''
-      this.expanded = true
+        vueRef: Date.now(),
+      });
+      this.newNodeLabel = '';
+      this.expanded = true;
     },
     deleteChildNode(nodeToDelete) {
-      this.node.children = this.node.children.filter((node) => node !== nodeToDelete)
+      this.node.children = this.node.children.filter(
+        (node) => node !== nodeToDelete,
+      );
     },
     slugify(val) {
-      return val.normalize('NFD').replace(/[\u0300-\u036f]/g, '')
+      return val
+        .normalize('NFD')
+        .replace(/[\u0300-\u036f]/g, '')
         .replace(/[^a-z^A-Z^_^0-9^{^}]/g, '_') // "test !" => "test__"
         .replace(/_+/g, '_') // "te__st" => "te_st"
         .replace(/^_+|_+$/g, '') // "___test__" => "test"
-        .toLowerCase()
-    }
+        .toLowerCase();
+    },
   },
   template: `
     <div class="list-node-container" :data-depth="depth" :data-index="index">
       <div v-if="depth > 0" class="list-node input-prepend input-append input-group">
         <!-- Chevron to expand children -->
-        <button type="button" @click="expanded = !expanded" 
+        <button type="button" @click="expanded = !expanded"
                 class="btn btn-icon btn-primary input-group-addon btn-expand" >
           <i :class="expanded ? 'fa fa-chevron-up' : 'fa fa-chevron-down'"></i>
         </button>
         <!-- Label -->
-        <input type="text" v-model="node.label" placeholder="${_t('LIST_TEXT')}" 
-              class="form-control" @keydown.enter.prevent />
+        <input type="text" v-model="node.label" placeholder="${_t('LIST_TEXT')}"
+              class="form-control main_lang_input" @keydown.enter.prevent :disabled="selected_language != 0"/>
+        <!-- translation label -->
+
+              <input v-for="lang in extra_langs_list" v-model="node[lang]" type="text" class="form-control extra_lang_input" placeholder="${_t('BAZ_TRANSLATION')}" v-if="selected_language === lang"/>
         <!-- Id -->
         <div class="input-group-addon" >
           ${_t('LIST_KEY')} :
         </div>
-        <input type="text" v-model="node.id" placeholder="${_t('LIST_KEY')}" 
+        <input type="text" v-model="node.id" placeholder="${_t('LIST_KEY')}"
               class="form-control input-id" :class="{ error: !node.id }"
-              @keydown.enter.prevent />
+              @keydown.enter.prevent :disabled="selected_language != 0" />
         <!-- Delete Icon -->
-        <button type="button" @click="$emit('delete', node)" 
+        <button type="button" @click="$emit('delete', node)" :disabled="selected_language != 0"
                 class="btn btn-danger btn-icon input-group-addon">
           <i class="fa fa-trash icon-trash"></i>
         </button>
       </div>
-      <div v-show="depth == 0 || expanded" class="list-node-children" 
+      <div v-show="depth == 0 || expanded" class="list-node-children"
            :class="{root: depth == 0, first: depth == 0 && node.children.length == 0}">
-        <draggable v-model="node.children" group="nodes">
-          <list-node v-for="(childNode, index) in node.children" :key="childNode.vueRef" 
-                     :node="childNode" @delete="deleteChildNode" :depth="depth + 1" 
+        <draggable v-model="node.children" group="nodes" :disabled="selected_language != 0">
+          <list-node v-for="(childNode, index) in node.children" :extra_langs_list="extra_langs_list" :selected_language="selected_language" :key="childNode.vueRef"
+                     :node="childNode" @delete="deleteChildNode" :depth="depth + 1"
                      :index="index"></list-node>
         </draggable>
         <div class="list-new-node input-group input-prepend">
-          <button type="button" @click="addChildNode" 
-                  class="btn btn-neutral btn-icon input-group-addon btn-add-child" >
+          <button type="button" @click="addChildNode"
+                  class="btn btn-neutral btn-icon input-group-addon btn-add-child" :disabled="selected_language != 0">
             <i class="fa fa-plus"></i>
           </button>
-          <input type="text" v-model="newNodeLabel" 
-                 :placeholder="depth > 0 ? '${_t('LIST_ADD_CHILD_NODE')}' : '${_t('LIST_ADD_NODE')}'" 
-                 class="form-control" @keydown.enter.prevent="addChildNode" />
-          
+          <input type="text" v-model="newNodeLabel"
+                 :placeholder="depth > 0 ? '${_t('LIST_ADD_CHILD_NODE')}' : '${_t('LIST_ADD_NODE')}'"
+                 class="form-control" @keydown.enter.prevent="addChildNode" :disabled="selected_language != 0"/>
+
         </div>
-        
+
       </div>
     </div>
-  `
-}
+  `,
+};
