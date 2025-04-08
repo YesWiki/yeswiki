d6e9268c8be5f5414af0e1c16390dbc513cf56c2
Sylvain Lehman <sylvain@lehmann.dev>
2025-04-08 16:42:11 +0200

    feat(core): add playwright tests

---
+++ tests/e2e/reset.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+rm -f  /var/www/html/wakka.config.test.php
+echo "DROP DATABASE IF EXISTS yeswiki_test; CREATE DATABASE yeswiki_test;" |  mysql -h yeswiki-db -u root -proot
+curl --silent --fail --show-error \
+          -F "config[default_language]=fr" \
+          -F "config[wakka_name]=MyTestWiki" \
+          -F "config[root_page]=PagePrincipale" \
+          -F "config[base_url]=http://yeswiki-web/?" \
+          -F "config[mysql_host]=yeswiki-db" \
+          -F "config[mysql_database]=yeswiki_test" \
+          -F "config[mysql_user]=root" \
+          -F "config[mysql_password]=root" \
+          -F "config[table_prefix]=yeswiki_" \
+          -F "config[allow_raw_html]=1" \
+          -F "config[archive][privatePath]=./private/archives" \
+          -F "admin_name=ActionTest" \
+          -F "admin_password=ActionTestPassword" \
+          -F "admin_password_conf=ActionTestPassword" \
+          -F "admin_email=test@example.com" \
+          -F "submit=Continue" \
+          "http://yeswiki-web/?PagePrincipale&installAction=install"
