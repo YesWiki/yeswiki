56cccf631a966a0f4eb2aecc9f40357f656e32e6
Sylvain Lehman <sylvain@lehmann.dev>
2025-05-06 11:10:16 +0200

    refacto(core): move playwright dependencies to dockerfile

--- docker/entrypoint_test.sh
+++ docker/entrypoint_test.sh
@@ -6,7 +6,6 @@ corepack enable
 yarn install
 
 export PLAYWRIGHT_BROWSERS_PATH=0
-yarn run playwright install-deps
 yarn run playwright install
 yarn run playwright test --ui --ui-port=8083 --ui-host 0.0.0.0 &
 
