c6563ba6144b2cdf4f5dd84332b646588c5b0b83
Sylvain Lehman <sylvain@lehmann.dev>
2025-05-07 17:32:04 +0200

    feature(core): add functional test helper to remove page

--- tests/e2e/helpers/page.ts
+++ tests/e2e/helpers/page.ts
@@ -1,5 +1,6 @@
 import {Page} from "@playwright/test";
 import {replaceEditorTextCallback, replaceEditorTextNewContent} from "./editor";
+import {errorShouldBe} from "./alert";
 
 export const createPageWithContent = async (page: Page, tag: string, content: string) => {
     await page.goto(`/?${tag}`);
@@ -8,4 +9,11 @@ export const createPageWithContent = async (page: Page, tag: string, content: st
     await replaceEditorTextNewContent(page, content);
     await page.getByRole('button', { name: 'Sauver' }).first().click();
     await page.waitForLoadState();
+}
+
+export const removePage = async (page: Page, tag: string) => {
+    await page.goto(`/?${tag}`);
+    await page.getByRole('link', { name: 'Supprimer' }).click();
+    await page.locator('#YesWikiModal.modal.in').getByRole('button', { name: 'Supprimer' }).click();
+    await errorShouldBe(page, `La page ${tag} a définitivement été supprimée`)
 }
