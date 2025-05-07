469e5de01ef94e47ab3310ec18376a8e424abced
Sylvain Lehman <sylvain@lehmann.dev>
2025-05-07 12:33:33 +0200

    feat(core): add functional test helpers to replace all page content

---
+++ tests/e2e/helpers/page.ts
@@ -0,0 +1,10 @@
+import {Page} from "@playwright/test";
+import {replaceEditorText} from "./editor";
+
+export const createPageWithContent = async (page: Page, tag: string, content: string) => {
+    await page.goto(`/?${tag}`);
+
+    await page.getByRole('link', { name: 'créer' }).click();
+    await replaceEditorText(page, (value, additionalProperties) => additionalProperties.content, {content: content});
+    await page.getByRole('button', { name: 'Sauver' }).first().click();
+}
