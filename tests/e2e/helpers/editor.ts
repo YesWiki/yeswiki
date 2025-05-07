d5734fab221715611976b466aa00fb18688c79b0
Sylvain Lehman <sylvain@lehmann.dev>
2025-05-07 12:33:33 +0200

    feat(core): add functional test helpers to replace all page content

--- tests/e2e/helpers/editor.ts
+++ tests/e2e/helpers/editor.ts
@@ -5,14 +5,15 @@ import {Page} from "@playwright/test";
  * The callback function does not have access to the page context.
  * So only use the value passed as argument and the browser api.
  */
-export const replaceEditorText = async (page: Page, callback: Function) => {
+export const replaceEditorText = async (page: Page, callback: Function, additionalProperties: object = null) => {
     await page.waitForLoadState();
 
-    await page.evaluate(({callbackStr}) => {
+    await page.evaluate(({callbackStr, additionalPropertiesStr}) => {
+        const additionalProperties = JSON.parse(additionalPropertiesStr);
         const callback = new Function('return ' + callbackStr)();
         const editor = window['aceditor-body'].editor;
         const value = editor.getValue();
-        const newValue = callback(value);
+        const newValue = callback(value, additionalProperties);
         editor.setValue(newValue);
-    }, {callbackStr: callback.toString()});
+    }, {callbackStr: callback.toString(), additionalPropertiesStr: JSON.stringify(additionalProperties)});
 }
