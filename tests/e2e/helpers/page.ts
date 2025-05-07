c6d45c6bef2e7475f3d48bf8de96b41f15387384
Sylvain Lehman <sylvain@lehmann.dev>
2025-05-07 15:34:09 +0200

    feat(core): improve reliability of functional test createPageWithContent helper

--- tests/e2e/helpers/page.ts
+++ tests/e2e/helpers/page.ts
@@ -7,4 +7,5 @@ export const createPageWithContent = async (page: Page, tag: string, content: st
     await page.getByRole('link', { name: 'créer' }).click();
     await replaceEditorText(page, (value, additionalProperties) => additionalProperties.content, {content: content});
     await page.getByRole('button', { name: 'Sauver' }).first().click();
+    await page.waitForLoadState();
 }
