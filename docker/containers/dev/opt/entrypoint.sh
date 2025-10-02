163364c0f227092f36d62a5001dde54b645c4203
sylvain lehmann <sylvain@lehmann.dev>
2025-10-02 16:27:55 +0200

    test phpunit

--- docker/containers/dev/opt/entrypoint.sh
+++ docker/containers/dev/opt/entrypoint.sh
@@ -4,5 +4,6 @@ set -xe
 
 /opt/install.sh
 
+cat /etc/nginx/nginx.conf.template  | sed "s|_NGINX_WEB_ROOT|${NGINX_WEB_ROOT}|g" > /etc/nginx/nginx.conf
 /usr/sbin/nginx -g 'daemon on; master_process on;'
 php-fpm
