4789dba14bd81449d0f5be19b30ed68c21285ce0
sylvain lehmann <sylvain@lehmann.dev>
2025-10-02 19:21:04 +0200

    test playwright

--- tests/e2e/tests/controllers/CiController/updateConfig.spec.ts
+++ tests/e2e/tests/controllers/CiController/updateConfig.spec.ts
@@ -5,6 +5,7 @@ const TARGET = '?api/ci/update_config'
 test('Access should no be granted to anonymous', async({ page }) => {
   const res = await page.request.post(TARGET, { data: {} })
   expect(res.status()).toBe(401)
+  expect(await res.body()).toBe(401)
 })
 
 // TODO: restore test
