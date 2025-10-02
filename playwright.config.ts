e11216481d532a5737ce91d3b6ae9a8574611a3d
sylvain lehmann <sylvain@lehmann.dev>
2025-10-02 18:17:01 +0200

    test playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
     /\/tools\/.*\/tests\/e2e\/.*\.spec.ts/
   ],
   /* Run tests in files in parallel */
-  fullyParallel: false,
+  fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
