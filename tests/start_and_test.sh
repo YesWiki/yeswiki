9c5a2a2ffa2cd463c3684c2113495bad0f39a1c7
sylvain lehmann <sylvain@lehmann.dev>
2025-10-02 20:00:53 +0200

    fix tests

--- tests/start_and_test.sh
+++ tests/start_and_test.sh
@@ -9,6 +9,6 @@ set -e
 
 /opt/entrypoint.sh &
 while ! echo . | curl --silent --fail localhost:8085 > /dev/null; do sleep 1; done # Wait for server initialized
-while ! echo . | nc yeswiki-db 3306; do sleep 1; done # Wait for server initialized
+while ! echo . | nc -W 1 yeswiki-db 3306; do sleep 1; done # Wait for server initialized
 
 ./tests/tests.sh
