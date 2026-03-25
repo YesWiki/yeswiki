8978f4d1d1a81210b9c3b63f4e68d42cd7ae803b
Florian Schmitt <mrflos@gmail.com>
2026-03-25 11:22:40 +0300

    feat(bazar): make filters work on geometries (non-dynamic)

--- tools/bazar/presentation/javascripts/leaflet-draw.helper.js
+++ tools/bazar/presentation/javascripts/leaflet-draw.helper.js
@@ -1,6 +1,6 @@
-export function drawGeometries(drawnItems, features, popup = '') {
+export function drawGeometries(drawnItems, features, popup = '', id = 'unknown') {
   if (features && features.length > 0) {
-    features.forEach(function (feature) {
+    features.forEach(function(feature) {
       if (feature.properties && feature.properties.type === 'circle') {
         var latlng = L.latLng(
           feature.geometry.coordinates[1],
@@ -13,10 +13,10 @@ export function drawGeometries(drawnItems, features, popup = '') {
         delete circleOptions.radius
         if (circleOptions.icon) delete circleOptions.icon
         if (circleOptions.title) delete circleOptions.title
-        circleOptions.className = 'bazar-entry ' + (feature.properties.className || '');
+        circleOptions.className = 'bazar-entry-geometry ' + (feature.properties.className || '');
         var circle = L.circle(latlng, { radius: radius, ...circleOptions })
         if (popup && popup.length > 0) {
-          circle.bindPopup(function (layer) {
+          circle.bindPopup(function(layer) {
             return popup;
           });
         }
@@ -24,9 +24,9 @@ export function drawGeometries(drawnItems, features, popup = '') {
         circle.on('add', function() {
           var pathElement = this.getElement();
           if (pathElement) {
-			pathElement.setAttribute ('stroke', 'blue');
-			pathElement.setAttribute ('stroke-opacity', '1');
-            pathElement.setAttribute('data-id', feature.properties.id || 'unknown');
+            pathElement.setAttribute('stroke', 'blue');
+            pathElement.setAttribute('stroke-opacity', '1');
+            pathElement.setAttribute('data-id', id);
             pathElement.setAttribute('data-type', 'circle');
           }
         });
@@ -35,43 +35,42 @@ export function drawGeometries(drawnItems, features, popup = '') {
 
         drawnItems.addLayer(circle);
       } else {
-        L.geoJSON (feature, {  
-			style:/* function (feature) {
+        L.geoJSON(feature, {
+          style:/* function (feature) {
 			    return */{
-		      color: 'blue',
-		      className: 'bazar-entry ' + (feature.properties.className || '')
-		    }//;
-		  ,
-		  pointToLayer: function (feature, latlng) {
-			var customIcon = L.Icon.Default.extend({
-			  options: {
-				className: 'bazar-entry ' + (feature.properties.className || '')
-			  }
-			});
+            color: 'blue',
+            className: 'bazar-entry-geometry ' + (feature.properties.className || '')
+          }//;
+          ,
+          pointToLayer: function(feature, latlng) {
+            var customIcon = L.Icon.Default.extend({
+              options: {
+                className: 'bazar-entry-geometry ' + (feature.properties.className || '')
+              }
+            });
 
-			return L.marker(latlng, { icon: new customIcon() });
-		  },
-		  onEachFeature: function (f, layer) {
-				    if (popup && popup.length > 0) {
-				      layer.bindPopup(function (l) {
-				        return popup;
-				      });
-				    }
-			if (layer.getElement) {
-			   layer.on('add', function() {
-				  var elem = this.getElement();
-				  if (elem) {
-				     elem.setAttribute('data-id', f.properties.id || '');
-				     elem.setAttribute('data-type', f.geometry.type);
-				  }
-			   });
-			}
-
-			drawnItems.addLayer(layer);
-		  }
-		});
+            return L.marker(latlng, { icon: new customIcon() });
+          },
+          onEachFeature: function(f, layer) {
+            if (popup && popup.length > 0) {
+              layer.bindPopup(function(l) {
+                return popup;
+              });
+            }
+            if (layer.getElement) {
+              layer.on('add', function() {
+                var elem = this.getElement();
+                if (elem) {
+                  elem.setAttribute('data-id', id);
+                  elem.setAttribute('data-type', f.geometry.type);
+                }
+              });
+            }
+            drawnItems.addLayer(layer);
+          }
+        });
       }
     })
-    }
-    return drawnItems
+  }
+  return drawnItems
 }
