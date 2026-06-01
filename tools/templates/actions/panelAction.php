2970b36aa4614026f4d2f2813b58576bfe1f8cca
oiseauroch <tobias@paquerette.eu>
2026-06-01 17:10:01 +0200

    fix regression panel action

--- tools/templates/actions/panelAction.php
+++ tools/templates/actions/panelAction.php
@@ -12,43 +12,37 @@ class PanelAction extends YesWikiAction
        $title = $this->arguments['title'] ?? '';
 
        // classe css pour la couleur du panel ou autre
-       $class = $this->arguments['class'] ?? '';
+       $class = $this->arguments['class'] ?? 'panel-default';
 
        // collapsed: initial state is collapsed, and the panel is collapsible
        // collapsible: initial state is displayed, and the panel is collapsible
        // empty: initial state is displayed, and the panel is not collapsible
        $type = $this->arguments['type'] ?? '';
-       // data attributes
-       //$data = $this->getService(YesWiki\Templates\Service\Utils::class)->getDataParameter();
        $pagetag = $this->wiki->GetPageTag();
 
         if ($this->check_end_elem('panel')) {
             $headingID = uniqid('heading');
             $collapseID = uniqid('collapse');
+
+            $collapsed = ($type == 'collapsed');
+            $collapsible = ($type == 'collapsible') ||$collapsed ;
+
             if (isset($GLOBALS['check_' . $pagetag]['accordion_uniqueID'])) {
                 $accordionID = $GLOBALS['check_' . $pagetag]['accordion_uniqueID'];
-                $collapsible = ($type == 'collapsible');
                 if ($collapsible && !isset($GLOBALS['check_' . $pagetag]['accordion_collapsible'])) {
                     $collapsed = false;
                     $GLOBALS['check_' . $pagetag]['accordion_collapsible'] = true;
-                } else {
-                    $collapsed = true;
                 }
+                $collapsed = true;
                 $collapsible = true;
             } else {
                 $accordionID = '';
-            }
 
-            $data = '';
-            if (is_array($data)) {
-                foreach ($data as $key => $value) {
-                    $data .= ' data-' . $key . '="' . $value . '"';
-                }
             }
 
             $headerTagName = $collapsible ? 'button' : 'div';
             $result = '<!-- start of panel -->'
-                . "<div class=\"panel $class\" $data>
+                . "<div class=\"panel $class\">
               <$headerTagName class=\"panel-heading" . ($collapsed ? ' collapsed' : '') . '"';
             if ($collapsible) {
                 $result .= " id=\"$headingID\"" . ' data-toggle="collapse"' . (!empty($accordionID) ? " data-parent=\"#$accordionID\"" : '')
