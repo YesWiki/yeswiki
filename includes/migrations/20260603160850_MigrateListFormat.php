41c45318706a263642b51401461d4c474563a1a5
oiseauroch <tobias@paquerette.eu>
2026-06-13 19:16:19 +0200

    migrate list format

---
+++ includes/migrations/20260603160850_MigrateListFormat.php
@@ -0,0 +1,87 @@
+<?php
+
+use YesWiki\Core\Service\DbService;
+use YesWiki\Core\YesWikiMigration;
+use YesWiki\Bazar\Service\ListManager;
+use YesWiki\Core\Service\TripleStore;
+
+class MigrateListFormat extends YesWikiMigration
+{
+    public function run()
+    {
+        $tripleStore = $this->getService(TripleStore::class);
+        $listManager = $this->getService(ListManager::class);
+        $dbService = $this->getService(DbService::class);
+
+        $table_page_name = trim($this->dbService->prefixTable('pages'));
+
+        $lists = $dbService->loadAll('Select * from '.$table_page_name.' where tag in (SELECT resource FROM '.$this->dbService->prefixTable('triples').' WHERE value = \'liste\')');
+
+        foreach($lists as $list) {
+            $id = $list['id'];
+            $list = json_decode($list['body'], true);
+            $nodes = [];
+            if (!isset($list['nodes'])) {
+                $list = $listManager->convertDataStructure($list);
+            }
+            foreach ($list['nodes'] as $index => $node) {
+                $children = [];
+                if (isset($node['children'])) {
+                    foreach ($node['children'] as $i => $child) {
+                        $children[$i] = [
+                            'id' => $child['id'],
+                            'label' => $child['label'],
+                            'children' => [],
+                        ];
+                    }
+                }
+                $nodes[$index] = [
+                    'id' => $node['id'],
+                    'label' => $node['label'],
+                    'children' => $children,
+                ];
+            }
+            $new_lists[$id] = [
+                'id' => $id,
+                'title' => $list['title'],
+                'nodes' => $nodes,
+            ];
+        }
+
+        $temp_table_query = 'create temporary table lists (id int, body longtext)';
+        $insert_query = 'insert into lists (id, body) values ';
+
+        foreach ($new_lists as $key => $value) {
+            $json = json_encode($value, JSON_FORCE_OBJECT);
+            $json = str_replace('\\', '\\\\', $json);
+            $json = str_replace("'", "\'", $json);
+            $insert_query .= '( '.$key.",'".$json."'),";
+        }
+        $insert_query = rtrim($insert_query, ',');
+
+        $update_query = 'update lists, '.
+            $table_page_name.
+            ' set '.
+            $table_page_name.'.body=lists.body where lists.id='.$table_page_name.'.id';
+
+        $query = $temp_table_query.';'.$insert_query.';'.$update_query;
+        $link = $dbService->getLink();
+        mysqli_multi_query($link, $query);
+        do {
+            /* store the result set in PHP */
+            if ($result = mysqli_store_result($link)) {
+                while ($row = mysqli_fetch_row($result)) {
+                    printf("%s\n", $row[0]);
+                }
+                /* Affichage d'une séparation */
+                if (mysqli_more_results($link)) {
+                    printf("-----------------\n");
+                }
+            }
+            /* print divider */
+            if (mysqli_more_results($link)) {
+                printf("-----------------\n");
+            }
+        } while (mysqli_next_result($link));
+    }
+}
--- tools/bazar/controllers/ListController.php
+++ tools/bazar/controllers/ListController.php
@@ -47,7 +47,8 @@ class ListController extends YesWikiController
         $lists = $this->listManager->getAll();
 
         foreach ($lists as $key => $list) {
-            $lists[$key]['canEdit'] = !$this->securityController->isWikiHibernated() && $this->wiki->HasAccess('write', $key);
+
+            $lists[$key]['canEdit'] = !$this->securityController->isWikiHibernated() && $this->aclService->hasAccess('write', $key);
             $lists[$key]['canDelete'] = !$this->securityController->isWikiHibernated() && ($this->wiki->UserIsAdmin() || $this->wiki->UserIsOwner($key));
             // Small trick : create a fake SelectListField so we can reuse the code to compute the options
             $field = $this->fieldFactory->create(['liste', $list['id'], '', '', '', '', '', '', '', '', '', '', '', '', '', '']);
