f0a8bd5813fdc155884d2d70d81fc6e4c1ddadc1
oiseauroch <tobias@paquerette.eu>
2026-07-13 12:06:11 +0200

    Improve translate handler

--- tools/bazar/handlers/TranslateHandler.php
+++ tools/bazar/handlers/TranslateHandler.php
@@ -31,87 +31,110 @@ class TranslateHandler extends YesWikiHandler
         'bf_longitude',
         'bf_latitude',
         'geolocation',
+        'lang'
     ];
+
+    private function save($entryManager) : string{
+        $post = $this->getRequest()->request;
+        if ($post->get('entry') != null && $post->get('extra_lang') != null && $post->get('antispam') != null) {
+            $entry = json_decode($post->get('entry'), true);
+            $entry['extra_lang'] = json_decode($post->get('extra_lang'), true);
+            $entry['antispam'] = $post->get('antispam');
+            $entryManager->update($entry['id_fiche'], $entry);
+
+            return $this->wiki->redirect($this->wiki->Href(testUrlInIframe(), '', [
+            'vue' => 'consulter',
+            'action' => 'voir_fiche',
+            'id_fiche' => $entry['id_fiche'],
+            'message' => 'modif_ok',
+            'lang' => $post->get('lang') ?? 'default',
+            ], false));
+        }
+    }
+
+    private function display_form($tag): string {
+        $pageManager = $this->getService(PageManager::class);
+        $formManager = $this->getService(FormManager::class);
+
+        $page = $pageManager->getOne($tag);
+        $page = json_decode($page['body'], true);
+        $page_lang = $page['lang'] ?? $this->wiki->config['default_language'];
+        $langs = $this->wiki->config['supported_langs'];
+        $extra_langs = array_filter($langs, function($el) use ($page_lang) {
+            return $el != $page_lang;
+        });
+
+
+        $field_names = [];
+        $ignorefields = [];
+            $form = $formManager->getOne($page['id_typeannonce']);
+            dump($form);
+            foreach ($form['fields'] as $field) {
+                if (!in_array($field['type'], self::IGNORE_FIELDS_TYPES)) {
+                    $type = 'text';
+                    if ($field['type'] == 'textarea') {
+                        $type = 'textarea';
+                    } else if ($field['type'] == 'text' && isset($field['subtype'])) {
+                        $type = $field['subtype'];
+                    }
+                    $field_names[] = [
+                    'name' => $field['name'],
+                    'label' => $field['label'],
+                    'type' => $type,
+                    ];
+
+                    foreach($extra_langs as $lang) {
+                        $page['extra_lang'][$lang]['name'] ??= '';
+                    }
+                }
+            }
+        $output = $this->render('@bazar/entries/translate.twig', [
+        'isEntry' => true,
+        'entry' => $page,
+        'langs' => $langs,
+        'baseLang' => $page_lang,
+        'fieldsNames' => $field_names,
+        ]);
+        return $output;
+    }
 
     public function run() :String
     {
-        if (!$this->wiki->HasAccess('write') || !$this->wiki->page) {
-            return null;
-        }
-        if (isset($this->wiki->config['supported_langs'])) {
-            $langs = $this->wiki->config['supported_langs'];
-
-            $pageManager = $this->getService(PageManager::class);
-            $entryManager = $this->getService(EntryManager::class);
-            $formManager = $this->getService(FormManager::class);
-
-            $tag = $this->wiki->GetPageTag();
-            $this->arguments = [];
-
-
-            if ($this->getRequest()->getMethod() === 'POST') {
-                $post = $this->getRequest()->request;
-                if ($post->get('entry') != null && $post->get('extra_lang') != null && $post->get('antispam') != null) {
-                    $entry = json_decode($post->get('entry'), true);
-                    $entry['extra_lang'] = json_decode($post->get('extra_lang'), true);
-                    $entry['antispam'] = $post->get('antispam');
-                    $entryManager->update($entry['id_fiche'], $entry);
-
-                    return $this->wiki->redirect($this->wiki->Href(testUrlInIframe(), '', [
-                    'vue' => 'consulter',
-                    'action' => 'voir_fiche',
-                    'id_fiche' => $entry['id_fiche'],
-                    'message' => 'modif_ok',
-                    'lang' => $post->get('lang') ?? 'default',
-                    ], false));
-                }
-            }
-
-            $page = $pageManager->getOne($tag);
-            $page = json_decode($page['body'], true);
-            $isEntry = $entryManager->isEntry($tag);
-            $field_names = [];
-            $ignorefields = [];
-            $page_lang = $page['lang'] ?? $this->wiki->config['default_language'];
-            $extra_langs = array_filter($langs, function($el) use ($page_lang) {
-                return $el != $page_lang;
-            });
-            if ($isEntry) {
-                $form = $formManager->getOne($page['id_typeannonce']);
-                foreach ($form['prepared'] as $field) {
-                    if (!in_array($field->getType(), self::IGNORE_FIELDS_TYPES)) {
-                        $field_names[] = [
-                        'name' => ($field->getType() == 'image' ? 'image' : '') . $field->getName(),
-                        'label' => $field->getLabel(),
-                        ];
-
-                        foreach($extra_langs as $lang) {
-                            $page['extra_lang'][$lang][$field->getName()] ??= '';
-                        }
-                    }
-                }
-
-
-        dump($page);
         $output = $this->wiki->Header();
-        $output .= $this->render('@bazar/entries/translate.twig', [
-            'isEntry' => $isEntry,
-            'entry' => $page,
-            'langs' => $langs,
-            'baseLang' => $page_lang,
-            'fieldsNames' => $field_names,
-        ]);
-        $output .= $this->wiki->Footer();
-        return $output;
-    }
-        } else {
-            $output = $this->wiki->Header();
+
+        if (!$this->wiki->HasAccess('write') || !$this->wiki->page) {
             $output .= $this->render('@templates/alert-message.twig', [
                             'type' => 'danger',
-                            'message' => _t('TRANSLATE_MISSING_EXTRA_LANG'),
+                            'message' => _t('NOT_ALLOWED'),
                         ]);
-            $output .= $this->wiki->Footer();
-            return $output;
+        } else {
+            if (isset($this->wiki->config['supported_langs'])) {
+
+                $tag = $this->wiki->GetPageTag();
+                $this->arguments = [];
+                $entryManager = $this->getService(EntryManager::class);
+                $isEntry = $entryManager->isEntry($tag);
+
+                if ($isEntry) {
+                    if ($this->getRequest()->getMethod() === 'POST') {
+                        $output .= $this->save($entryManager);
+                    } else {
+                        $output .= $this->display_form($tag);
+                    }
+                } else {
+                    $output .= $this->render('@templates/alert-message.twig', [
+                                    'type' => 'danger',
+                                    'message' => _t('TRANSLATE_ACTION_ONLY_ALLOWED_FOR_ENTRY'),
+                                ]);
+                }
+            } else {
+                $output .= $this->render('@templates/alert-message.twig', [
+                                'type' => 'danger',
+                                'message' => _t('TRANSLATE_MISSING_EXTRA_LANG'),
+                            ]);
+            }
         }
+        $output .= $this->wiki->Footer();
+        return $output;
     }
 }
