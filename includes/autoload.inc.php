0041b5f6af4f8f2af17966a15821196a373825bb
Florian Schmitt <mrflos@gmail.com>
2026-09-17 16:57:00 +0300

    fix(autoload): better error handling

--- includes/autoload.inc.php
+++ includes/autoload.inc.php
@@ -1,69 +1,70 @@
 <?php
 
 spl_autoload_register(function ($className) {
-    // Autoload services
-    if (preg_match('/^YesWiki\\\\([^\\\\]+)(?:\\\\([^\\\\]+))?(?:\\\\([^\\\\]+))?$/', $className, $matches)) {
-        if (empty($matches[2])) {
-            // not currently managed
-        } elseif (empty($matches[3])) {
-            switch ($matches[1]) {
-                case 'Core':
-                    if (file_exists('includes/' . $matches[2] . '.php')) {
-                        require 'includes/' . $matches[2] . '.php';
-                    }
-                    break;
-                default:
-                    // actions or handlers, directly managed by Performer
-                    break;
-            }
-        } else {
-            // basePath
-            switch ($matches[1]) {
-                case 'Core':
-                    $basePath = 'includes';
-                    break;
-                case 'Custom':
-                    $basePath = 'custom';
-                    break;
-                default:
-                    $extension = strtolower($matches[1]);
+    if (!preg_match('/^YesWiki\\\\([^\\\\]+)(?:\\\\([^\\\\]+))?(?:\\\\([^\\\\]+))?$/', $className, $matches)) {
+        return;
+    }
 
-                    if (is_dir("custom/tools/{$extension}")) {
-                        $basePath = "custom/tools/{$extension}";
-                    } else {
-                        $basePath = "tools/{$extension}";
-                    }
-                    break;
-            }
-            // Autoload services
-            switch ($matches[2]) {
-                case 'Service':
-                    require "$basePath/services/{$matches[3]}.php";
-                    break;
-                case 'Controller':
-                    require "$basePath/controllers/{$matches[3]}.php";
-                    break;
-                case 'Field':
-                    if ($matches[1] != 'Core') {
-                        require "$basePath/fields/{$matches[3]}.php";
-                    }
-                    break;
-                case 'Commands':
-                    require "$basePath/commands/{$matches[3]}.php";
-                    break;
-                case 'Entity':
-                    require "$basePath/entities/{$matches[3]}.php";
-                    break;
-                case 'Exception':
-                    require "$basePath/exceptions/{$matches[3]}.php";
-                    break;
-                case 'Trait':
-                    require "$basePath/traits/{$matches[3]}.php";
-                    break;
-                default:
-                    // do nothing
-                    break;
-            }
+    if (empty($matches[2])) {
+        return;
+    }
+
+    if (empty($matches[3])) {
+        if ($matches[1] === 'Core' && file_exists('includes/' . $matches[2] . '.php')) {
+            require_once 'includes/' . $matches[2] . '.php';
         }
+
+        return;
+    }
+
+    switch ($matches[1]) {
+        case 'Core':
+            $basePath = 'includes';
+            break;
+        case 'Custom':
+            $basePath = 'custom';
+            break;
+        default:
+            $extension = strtolower($matches[1]);
+            $basePath = is_dir("custom/tools/{$extension}")
+                ? "custom/tools/{$extension}"
+                : "tools/{$extension}";
+            break;
+    }
+
+    switch ($matches[2]) {
+        case 'Service':
+            $folder = 'services';
+            break;
+        case 'Controller':
+            $folder = 'controllers';
+            break;
+        case 'Field':
+            $folder = $matches[1] === 'Core' ? null : 'fields';
+            break;
+        case 'Commands':
+            $folder = 'commands';
+            break;
+        case 'Entity':
+            $folder = 'entities';
+            break;
+        case 'Exception':
+            $folder = 'exceptions';
+            break;
+        case 'Trait':
+            $folder = 'traits';
+            break;
+        default:
+            $folder = null;
+            break;
+    }
+
+    if ($folder === null) {
+        return;
+    }
+
+    $file = "{$basePath}/{$folder}/{$matches[3]}.php";
+    if (file_exists($file)) {
+        require_once $file;
     }
 });
